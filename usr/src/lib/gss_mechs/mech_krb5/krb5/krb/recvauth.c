140a57817bfdee4cb40ab049b2ce10f89707dcb0
gtb <none@none>
2005-07-14 18:46:15 -0700

    6284864  krb5_recvauth() may free memory twice under certain conditions

--- usr/src/lib/gss_mechs/mech_krb5/krb5/krb/recvauth.c
+++ usr/src/lib/gss_mechs/mech_krb5/krb5/krb/recvauth.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -83,7 +83,6 @@ recvauth_common(krb5_context context,
 	    if ((retval = krb5_read_message(context, fd, &inbuf)))
 		return(retval);
 	    if (strcmp(inbuf.data, sendauth_version)) {
-		krb5_xfree(inbuf.data);
 		problem = KRB5_SENDAUTH_BADAUTHVERS;
 	    }
 	    krb5_xfree(inbuf.data);
@@ -97,7 +96,6 @@ recvauth_common(krb5_context context,
 	if ((retval = krb5_read_message(context, fd, &inbuf)))
 		return(retval);
 	if (appl_version && strcmp(inbuf.data, appl_version)) {
-		krb5_xfree(inbuf.data);
 		if (!problem)
 			problem = KRB5_SENDAUTH_BADAPPLVERS;
 	}
