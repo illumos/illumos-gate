87ef939cfddf8a846f5cec412f277812e7e03c24
cth <none@none>
2005-11-01 09:53:03 -0800

    4994268 device configuration failure can corrupt parent's devinfo reference count

--- usr/src/uts/common/os/modctl.c
+++ usr/src/uts/common/os/modctl.c
@@ -2820,7 +2820,7 @@ detach_driver(char *name)
 		return (0);
 
 	error = ndi_devi_unconfig_driver(ddi_root_node(),
-	    NDI_DETACH_DRIVER, major);
+	    NDI_DETACH_DRIVER | NDI_UNCONFIG, major);
 	return (error == NDI_SUCCESS ? 0 : -1);
 }
 
