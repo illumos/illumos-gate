f5164d870a2752310a5b738f4e194974e1fbf6fa
cwb <none@none>
2005-11-15 13:32:57 -0800

    6350363 anon_array_try_enter needs to initialise sobj->sync_cv

--- usr/src/uts/common/vm/vm_anon.c
+++ usr/src/uts/common/vm/vm_anon.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -3218,6 +3217,7 @@ anon_array_try_enter(struct anon_map *amp, ulong_t an_idx,
 	an_idx = P2ALIGN(an_idx, page_get_pagecnt(amp->a_szc));
 	hash = ANON_ARRAY_HASH(amp, an_idx);
 	sobj->sync_mutex = mtx = &anon_array_lock[hash].pad_mutex;
+	sobj->sync_cv = &anon_array_cv[hash];
 	if (!mutex_tryenter(mtx)) {
 		return (EWOULDBLOCK);
 	}
