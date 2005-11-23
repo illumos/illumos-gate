55381082fdea0647bb5d44ceeed7d5af386f30d2
muffin <none@none>
2005-11-23 12:27:47 -0800

    6210677 dirname and basename are slower than they should be
    --HG--
    rename : usr/src/cmd/basename/basename.sh => deleted_files/usr/src/cmd/basename/basename.sh
    rename : usr/src/cmd/dirname/dirname.sh => deleted_files/usr/src/cmd/dirname/dirname.sh

--- usr/src/cmd/basename/basename.c
+++ usr/src/cmd/basename/basename.c
@@ -20,7 +20,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -30,9 +30,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-static void output(char *);
-static void usage(void);
+#ifndef	XPG4
+#include <unistd.h>
+#include <regex.h>
+#include <libintl.h>
+#endif
 
 int
 main(int argc, char **argv)
@@ -40,71 +42,130 @@ main(int argc, char **argv)
 	char	*p;
 	char	*string;
 	char	*suffix;
+#ifndef	XPG4
+	int	r;
+	char	suf_buf[256];
+	char	*suf_pat;
+	size_t	suf_len;
+	regex_t	reg;
+	regmatch_t	pmatch[2];
+#endif
 
-	(void) setlocale(LC_ALL, "");
+	/*
+	 * For better performance, defer the setlocale()/textdomain()
+	 * calls until they get really required.
+	 */
 #if !defined(TEXT_DOMAIN)
 #define	TEXT_DOMAIN "SYS_TEST"
 #endif
-	(void) textdomain(TEXT_DOMAIN);
+	if (argc == 1) {
+		(void) puts(".");
+		return (0);
+	}
 
-	if (argc == 1)
-		output(".");
-
+#ifdef	XPG4
 	if (strcmp(argv[1], "--") == 0) {
 		argv++;
 		argc--;
+		if (argc == 1) {
+			(void) puts(".");
+			return (0);
+		}
 	}
-
-	if (argc == 1)
-		output(".");
-
-	if (argc > 3)
-		usage();
+#endif
+	if (argc > 3) {
+		(void) setlocale(LC_ALL, "");
+		(void) textdomain(TEXT_DOMAIN);
+		(void) fputs(gettext("Usage: basename string [ suffix ]\n"),
+		    stderr);
+		return (1);
+	}
 
 	string = argv[1];
 	suffix = (argc == 2) ? NULL : argv[2];
 
-	if (*string == '\0')
-		output(".");
+	if (*string == '\0') {
+		(void) puts(".");
+		return (0);
+	}
 
 	/* remove trailing slashes */
-	p = string + strlen(string) -1;
-	while ((p >= string) && (*p == '/'))
+	p = string + strlen(string) - 1;
+	while (p >= string && *p == '/')
 		*p-- = '\0';
 
-	if (*string == '\0')
-		output("/");
+	if (*string == '\0') {
+		(void) puts("/");
+		return (0);
+	}
 
 	/* skip to one past last slash */
 	if ((p = strrchr(string, '/')) != NULL)
 		string = p + 1;
 
+	if (suffix == NULL) {
+		(void) puts(string);
+		return (0);
+	}
+
+#ifdef	XPG4
 	/*
 	 * if a suffix is present and is not the same as the remaining
 	 * string and is identical to the last characters in the remaining
 	 * string, remove those characters from the string.
 	 */
-	if (suffix != NULL)
-		if (strcmp(string, suffix) != NULL) {
-			p = string + strlen(string) - strlen(suffix);
-			if (strcmp(p, suffix) == NULL)
-				*p = '\0';
-		}
-
-	output(string);
+	if (strcmp(string, suffix) != 0) {
+		p = string + strlen(string) - strlen(suffix);
+		if (strcmp(p, suffix) == 0)
+			*p = '\0';
+	}
+	(void) puts(string);
 	return (0);
-}
+#else
+	(void) setlocale(LC_ALL, "");
+	(void) textdomain(TEXT_DOMAIN);
 
-static void
-output(char *string)
-{
-	(void) printf("%s\n", string);
-	exit(0);
-}
+	suf_len = 6 + strlen(suffix) + 1 + 1; /* \(.*\)suffix$ */
+	if (suf_len > sizeof (suf_buf)) {
+		suf_pat = malloc(suf_len);
+		if (suf_pat == NULL) {
+			(void) fputs("malloc failed\n", stderr);
+			return (1);
+		}
+	} else {
+		suf_pat = suf_buf;
+	}
+	(void) strcpy(suf_pat, "\\(.*\\)");
+	(void) strcpy(suf_pat + 6, suffix);
+	*(suf_pat + suf_len - 1 - 1) = '$';
+	*(suf_pat + suf_len - 1) = '\0';
 
-static void usage(void)
-{
-	(void) fprintf(stderr,
-	    gettext("Usage: basename string [ suffix ]\n"));
-	exit(1);
+	r = regcomp(&reg, suf_pat, 0);
+	if (r != 0) {
+		(void) fprintf(stderr,
+		    "Internal error: regcomp failed for \"%s\"\n",
+		    suf_pat);
+		return (1);
+	}
+	r = regexec(&reg, string, 2, pmatch, 0);
+	if (r == 0) {
+		if (pmatch[0].rm_so == (regoff_t)-1 ||
+		    pmatch[1].rm_so == (regoff_t)-1 ||
+		    pmatch[1].rm_so != 0) {
+			(void) fprintf(stderr, "Internal error: regexec did "
+			    "not set sub-expression for:\n");
+			(void) fprintf(stderr, "path: \"%s\"\n", string);
+			(void) fprintf(stderr, "pattern: \"%s\"", suf_pat);
+			return (1);
+		}
+		if (pmatch[1].rm_so == pmatch[1].rm_eo) {
+			/* a null string matched */
+			(void) printf("%s\n", string);
+			return (0);
+		}
+		string[pmatch[1].rm_eo] = '\0';
+	}
+	(void) puts(string);
+	return (0);
+#endif
 }
