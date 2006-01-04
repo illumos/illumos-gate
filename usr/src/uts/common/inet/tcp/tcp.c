adccf2f268a15d5b03cd4d65d7ad9d2392ded017
kcpoon <none@none>
2006-01-03 19:58:41 -0800

    6367677 TCP needs to inherit the tcp_init_cwnd from listener

--- usr/src/uts/common/inet/tcp/tcp.c
+++ usr/src/uts/common/inet/tcp/tcp.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,15 +18,16 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 /* Copyright (c) 1990 Mentat Inc. */
 
 #pragma ident	"%Z%%M%	%I%	%E% SMI"
 
-const char tcp_version[] = "@(#)tcp.c	1.490	05/11/29 SMI";
+const char tcp_version[] = "%Z%%M%	%I%	%E% SMI";
 
 #include <sys/types.h>
 #include <sys/stream.h>
@@ -5357,6 +5357,13 @@ tcp_conn_request(void *arg, mblk_t *mp, void *arg2)
 	 * listener's receive window later in this function.
 	 */
 	eager->tcp_rwnd = 0;
+
+	/*
+	 * Inherit listener's tcp_init_cwnd.  Need to do this before
+	 * calling tcp_process_options() where tcp_mss_set() is called
+	 * to set the initial cwnd.
+	 */
+	eager->tcp_init_cwnd = tcp->tcp_init_cwnd;
 
 	/*
 	 * Zones: tcp_adapt_ire() and tcp_send_data() both need the
