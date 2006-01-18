427e818f6e9cab5f220fbbca949a8705b783afe3
ja97890 <none@none>
2006-01-18 09:19:06 -0800

    5020738 possible null-dereference path in icmp

--- usr/src/uts/common/inet/ip/icmp.c
+++ usr/src/uts/common/inet/ip/icmp.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 /* Copyright (c) 1990 Mentat Inc. */
@@ -3961,7 +3960,7 @@ icmp_wput(queue_t *q, mblk_t *mp)
 		if (!(mp1 = allocb(ip_hdr_length + icmp_wroff_extra,
 		    BPRI_LO))) {
 			BUMP_MIB(&rawip_mib, rawipOutErrors);
-			icmp_ud_err(q, mp1, ENOMEM);
+			icmp_ud_err(q, mp, ENOMEM);
 			return;
 		}
 		mp1->b_cont = mp->b_cont;
@@ -4534,7 +4533,7 @@ no_options:
 	 */
 	if (ip_len > IP_MAXPACKET) {
 		BUMP_MIB(&rawip_mib, rawipOutErrors);
-		icmp_ud_err(q, mp1, EMSGSIZE);
+		icmp_ud_err(q, mp, EMSGSIZE);
 		return;
 	}
 	if (icmp->icmp_proto == IPPROTO_ICMPV6 || icmp->icmp_raw_checksum) {
