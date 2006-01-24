b5c1d037485d15edb481098032579461ef8116cf
eota <none@none>
2006-01-23 18:35:29 -0800

    6368823 extra backslashes in DDI_REP_GET and DDI_REP_PUT

--- usr/src/uts/sparc/v9/ml/ddi_v9_asm.s
+++ usr/src/uts/sparc/v9/ml/ddi_v9_asm.s
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -788,7 +787,7 @@ i_ddi_caut_get(size_t getsz, void *addr, void *value)
 	st/**/s	%g4, [%o1];			\
 	brnz,pt	%o3, 2b;			\
 	add	%o1, n, %o1;			\
-1:						\
+1:
 
 	.align 32
 	ENTRY(ddi_rep_get8)
@@ -897,7 +896,7 @@ i_ddi_caut_get(size_t getsz, void *addr, void *value)
 	st/**/s	%g4, [%o2];			\
 	brnz,pt	%o3, 2b;			\
 	add	%o2, %o4, %o2;			\
-1:						\
+1:
 
 	.align 32
 	ENTRY(ddi_rep_put8)
