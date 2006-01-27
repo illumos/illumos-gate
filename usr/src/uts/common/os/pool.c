22942fab9d00d3b12e746d7d1cd7840ab492b8ed
gm149974 <none@none>
2006-01-27 06:11:59 -0800

    6331878 pooladm -c reports invalid configuration when hostname begins with a number

--- usr/src/uts/common/os/pool.c
+++ usr/src/uts/common/os/pool.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -290,7 +289,7 @@ pool_enable(void)
 		return (ret);
 	(void) nvlist_alloc(&pool_sys_prop, NV_UNIQUE_NAME, KM_SLEEP);
 	(void) nvlist_add_string(pool_sys_prop, "system.name",
-	    utsname.nodename);
+	    "default");
 	(void) nvlist_add_string(pool_sys_prop, "system.comment", "");
 	(void) nvlist_add_int64(pool_sys_prop, "system.version", 1);
 	(void) nvlist_add_byte(pool_sys_prop, "system.bind-default", 1);
