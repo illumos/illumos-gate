2f523b64504a53ab2ee3baf2cc417a936a07a13f
jroberts <none@none>
2006-02-01 15:48:19 -0800

    6371490 uducu on Seattle with S10U2 results in fault.memory.page instead of fault.cpu.*.l2cachedata

--- usr/src/cmd/fm/modules/sun4u/cpumem-diagnosis/cmd_memerr_arch.c
+++ usr/src/cmd/fm/modules/sun4u/cpumem-diagnosis/cmd_memerr_arch.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,8 +18,9 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -62,7 +62,8 @@ cmd_mem_synd_check(fmd_hdl_t *hdl, uint64_t afar, uint8_t afar_status,
     uint16_t synd, uint8_t synd_status, cmd_cpu_t *cpu)
 {
 	if (synd == CH_POISON_SYND_FROM_XXU_WRITE ||
-	    (cpu->cpu_type == CPU_ULTRASPARC_IIIi &&
+	    ((cpu->cpu_type == CPU_ULTRASPARC_IIIi ||
+	    cpu->cpu_type == CPU_ULTRASPARC_IIIiplus) &&
 	    synd == CH_POISON_SYND_FROM_XXU_WRMERGE)) {
 		fmd_hdl_debug(hdl,
 		    "discarding UE due to magic syndrome %x\n", synd);
