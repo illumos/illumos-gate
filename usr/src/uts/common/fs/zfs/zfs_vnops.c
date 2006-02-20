736b9155b17c80a2b71c813ccfe4524852cc7f6f
marks <none@none>
2006-02-20 12:56:05 -0800

    6371285 panic when nfs lookup operation attempted on plain file

--- usr/src/uts/common/fs/zfs/zfs_vnops.c
+++ usr/src/uts/common/fs/zfs/zfs_vnops.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -948,6 +947,9 @@ zfs_lookup(vnode_t *dvp, char *nm, vnode_t **vpp, struct pathname *pnp,
 		ZFS_EXIT(zfsvfs);
 		return (error);
 	}
+
+	if (dvp->v_type != VDIR)
+		return (ENOTDIR);
 
 	/*
 	 * Check accessibility of directory.
