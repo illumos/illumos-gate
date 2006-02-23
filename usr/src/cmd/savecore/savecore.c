7950274e5799b56695a76f974ac361411ec2e515
eschrock <none@none>
2006-02-22 18:19:30 -0800

    5004701 svc-savecore should be quieter about swap devices with no dump support

--- usr/src/cmd/savecore/savecore.c
+++ usr/src/cmd/savecore/savecore.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,8 +19,8 @@
  * CDDL HEADER END
  */
 /*
- * Copyright (c) 1998-2002 by Sun Microsystems, Inc.
- * All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
  */
 
 #pragma ident	"%Z%%M%	%I%	%E% SMI"
@@ -450,8 +449,18 @@ main(int argc, char *argv[])
 
 	if (dumpfile == NULL) {
 		dumpfile = Zalloc(MAXPATHLEN);
-		if (ioctl(dumpfd, DIOCGETDEV, dumpfile) == -1)
-			logprint(LOG_ERR, 1, 1, "no dump device configured");
+		if (ioctl(dumpfd, DIOCGETDEV, dumpfile) == -1) {
+			/*
+			 * If this isn't an interactive session, we are running
+			 * as part of the boot process.  If this is the case,
+			 * don't complain about the lack of dump device.
+			 */
+			if (isatty(STDOUT_FILENO))
+				logprint(LOG_ERR, 1, 1,
+				    "no dump device configured");
+			else
+				return (1);
+		}
 	}
 
 	if (mflag)
