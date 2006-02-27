fb03efaa231f4871ed01ca035b3241fecceb09eb
dp <none@none>
2006-02-27 13:59:31 -0800

    6390074 zonecfg 'set zonename' has rough edges

--- usr/src/cmd/zonecfg/zonecfg.c
+++ usr/src/cmd/zonecfg/zonecfg.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -21,7 +20,7 @@
  */
 
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -2667,6 +2666,11 @@ set_func(cmd_t *cmd)
 	 */
 	if (res_type == RT_ZONENAME && got_handle == FALSE &&
 	    !state_atleast(ZONE_STATE_CONFIGURED)) {
+		if ((err = zonecfg_validate_zonename(prop_id)) != Z_OK) {
+			zone_perror(prop_id, err, TRUE);
+			usage(FALSE, HELP_SYNTAX);
+			return;
+		}
 		(void) strlcpy(zone, prop_id, sizeof (zone));
 		return;
 	}
@@ -2682,6 +2686,7 @@ set_func(cmd_t *cmd)
 			 * reporting a problem about the *new* zonename.
 			 */
 			zone_perror(prop_id, err, TRUE);
+			usage(FALSE, HELP_SYNTAX);
 		} else {
 			need_to_commit = TRUE;
 			(void) strlcpy(zone, prop_id, sizeof (zone));
