261c56b106effed6954d8b32094dcc3af6896e07
dminer <none@none>
2006-03-01 11:23:35 -0800

    6357132 DHCP server should not open /dev/ip Contributed by Richard Lowe <richlowe@richlowe.net>.

--- usr/src/cmd/cmd-inet/usr.lib/in.dhcpd/interfaces.c
+++ usr/src/cmd/cmd-inet/usr.lib/in.dhcpd/interfaces.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 1993-2003 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -243,8 +242,8 @@ find_interfaces(void)
 	char			**user_if;
 	ENCODE 			*hecp;
 
-	if ((ip = open("/dev/ip", 0)) < 0) {
-		dhcpmsg(LOG_ERR, "Error: opening /dev/ip: %s\n",
+	if ((ip = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		dhcpmsg(LOG_ERR, "Error opening socket: %s\n",
 		    strerror(errno));
 		return (1);
 	}
@@ -253,6 +252,7 @@ find_interfaces(void)
 		dhcpmsg(LOG_WARNING,
 		    "Error discovering number of network interfaces: %s\n",
 		    strerror(errno));
+		(void) close(ip);
 		return (1);
 	}
 
