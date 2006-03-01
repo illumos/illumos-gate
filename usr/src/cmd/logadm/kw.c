3010f05b8e7c41a103780aa9306659706e47f207
dp <none@none>
2006-02-28 16:15:15 -0800

    PSARC 2006/134 logadm(1m) $zonename keyword 4823583 crontab command prints incorrect usage 6352485 RFE: add $zonename to the list of macros supported by logadm Contributed by Richard Lowe <richlowe@richlowe.net>.

--- usr/src/cmd/logadm/kw.c
+++ usr/src/cmd/logadm/kw.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  *
  * logadm/kw.c -- manage keywords table
@@ -47,6 +46,7 @@
 #include <strings.h>
 #include <time.h>
 #include <ctype.h>
+#include <zone.h>
 #include "err.h"
 #include "lut.h"
 #include "fn.h"
@@ -82,6 +82,8 @@ kw_init(struct fn *fnp, struct fn *nfnp)
 	static char *home;
 	static char *user;
 	static char *logname;
+	static char zonename[ZONENAME_MAX];
+	static zoneid_t zoneid;
 	static int initialized;
 	char *ptr;
 
@@ -152,6 +154,12 @@ kw_init(struct fn *fnp, struct fn *nfnp)
 
 	if ((logname = getenv("LOGNAME")) != NULL)
 		Keywords = lut_add(Keywords, "logname", STRDUP(logname));
+
+	zoneid = getzoneid();
+	if ((getzonenamebyid(zoneid, zonename, sizeof (zonename))) == -1)
+		err(EF_WARN|EF_SYS, "getzonenamebyid() failed.");
+	else
+		Keywords = lut_add(Keywords, "zonename", STRDUP(zonename));
 
 	initialized = 1;
 }
