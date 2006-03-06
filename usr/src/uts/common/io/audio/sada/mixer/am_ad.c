dfb9e8d753e971e3a4136d19823ad919e3fd75c7
fl147353 <none@none>
2006-03-05 23:14:16 -0800

    PSARC/2005/507 Solaris audio driver for ATI IXP400 6301905 Want support for Realtek/ATI Express AC'97 codec 6367508 setting playback interrupt rate to small value for audio driver may panic system

--- usr/src/uts/common/io/audio/sada/mixer/am_ad.c
+++ usr/src/uts/common/io/audio/sada/mixer/am_ad.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,8 +18,9 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 1999-2003 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -3461,7 +3461,11 @@ am_get_samples(audio_ch_t *chptr, int samples, void *buf, int mode)
 	mute = info->output_muted;
 	if (mute) {
 		/* we return zeros */
-		bytes_needed = samples << AM_INT32_SHIFT;
+		if (mode == AM_COMPAT_MODE)
+			bytes_needed = samples << AM_INT16_SHIFT;
+		else /* mixer mode */
+			bytes_needed = samples << AM_INT32_SHIFT;
+
 		ATRACE("am_get_samples() bzero", buf);
 		bzero(buf, bytes_needed);
 	}
--- usr/src/uts/common/sys/audio/ac97.h
+++ usr/src/uts/common/sys/audio/ac97.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,10 +18,13 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright (c) 2000-2001 by Sun Microsystems, Inc.
- * All rights reserved.
- *
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+/*
  * CAUTION: This header file has not gone through a formal review process.
  *	Thus its commitment level is very low and may change or be removed
  *	at any time.
@@ -246,6 +248,7 @@ typedef struct ac97_v21 ac97_v21_t;
 #define	PCMOVR_LEFT_0dB_GAIN				0x1000
 #define	PCMOVR_LEFT_MAX_ATTEN				0x1f00
 #define	PCMOVR_MUTE					0x8000
+#define	PCMOVR_GAIN_BITS				5
 
 /* Record Select Control Register			Index 1ah */
 #define	AC97_RECORD_SELECT_CTRL_REGISTER		0x1a
