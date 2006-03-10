86be1f3c5612d21bb7abd9d4f44f6007df8c5461
ahl <none@none>
2006-03-10 14:20:13 -0800

    6395902 use of USDT probes can cause plockstat and other scripts to fail

--- usr/src/lib/libdtrace/common/dt_pid.c
+++ usr/src/lib/libdtrace/common/dt_pid.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -644,7 +643,7 @@ dt_pid_create_probes(dtrace_probedesc_t *pdp, dtrace_hdl_t *dtp, dt_pcb_t *pcb)
 	struct ps_prochandle *P;
 	dt_proc_t *dpr;
 	pid_t pid;
-	int err;
+	int err = 0;
 
 	assert(pcb != NULL);
 
