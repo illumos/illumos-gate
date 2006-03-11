7646ae23a696cea1e135b888a6908c1dfdaae4db
lianep <none@none>
2006-03-10 17:48:29 -0800

    6252417 svcadm(1M) returns the wrong exit value with the -s option

--- usr/src/cmd/svc/svcadm/svcadm.c
+++ usr/src/cmd/svc/svcadm/svcadm.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1775,16 +1774,6 @@ wait_fmri_disabled(void *data, scf_walkinfo_t *wip)
 			/*
 			 * We're done.
 			 */
-			goto out;
-		}
-
-		if (strcmp(state, SCF_STATE_STRING_MAINT) == 0) {
-			/*
-			 * The service is ill.
-			 */
-			uu_warn(gettext("Instance \"%s\" is in maintenance"
-			    " state.\n"), wip->fmri);
-			exit_status = EXIT_SVC_FAILURE;
 			goto out;
 		}
 
