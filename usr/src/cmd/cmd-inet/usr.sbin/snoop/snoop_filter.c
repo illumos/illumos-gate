d6f299a85cda414e9c71cbe2ff5978b7ac4daf3f
dg199075 <none@none>
2006-03-30 14:01:11 -0800

    6404685 Snoop cannot filter OSPF traffic from a packet capture file

--- usr/src/cmd/cmd-inet/usr.sbin/snoop/snoop_filter.c
+++ usr/src/cmd/cmd-inet/usr.sbin/snoop/snoop_filter.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 1991-2003 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1264,6 +1263,8 @@ static struct match_type {
 	"udp",		6, 1, IPPROTO_UDP,	1,	OP_OFFSET_LINK,
 	"icmp",		9, 1, IPPROTO_ICMP,	0,	OP_OFFSET_LINK,
 	"icmp6",	6, 1, IPPROTO_ICMPV6,	1,	OP_OFFSET_LINK,
+	"ospf",		9, 1, IPPROTO_OSPF,	0,	OP_OFFSET_LINK,
+	"ospf",		6, 1, IPPROTO_OSPF,	1,	OP_OFFSET_LINK,
 	"ip-in-ip",	9, 1, IPPROTO_ENCAP,	0,	OP_OFFSET_LINK,
 	"esp",		9, 1, IPPROTO_ESP,	0,	OP_OFFSET_LINK,
 	"esp",		6, 1, IPPROTO_ESP,	1,	OP_OFFSET_LINK,
