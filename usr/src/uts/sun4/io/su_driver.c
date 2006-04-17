7cb42c7ebab29418f23e6bc16bd40cea6f303d10
rameshc <none@none>
2006-04-17 05:40:19 -0700

    6387359 su'asyopen() consumes ~99% of CPU0, or ~44% of overall  CPU resources, on a V240 2 x 1.28GHz.

--- usr/src/uts/sun4/io/su_driver.c
+++ usr/src/uts/sun4/io/su_driver.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -1988,11 +1987,11 @@ async_softint(struct asycom *asy)
 					mutex_enter(asy->asy_excl);
 					mutex_enter(asy->asy_excl_hi);
 				}
+				async->async_flags &= ~ASYNC_CARR_ON;
+				mutex_exit(asy->asy_excl_hi);
+				cv_broadcast(&async->async_flags_cv);
+				mutex_enter(asy->asy_excl_hi);
 			}
-			async->async_flags &= ~ASYNC_CARR_ON;
-			mutex_exit(asy->asy_excl_hi);
-			cv_broadcast(&async->async_flags_cv);
-			mutex_enter(asy->asy_excl_hi);
 		}
 	}
 
