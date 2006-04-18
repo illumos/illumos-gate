68ae785055658995f54cb50bc0a4e120c882f11b
garypen <none@none>
2006-04-18 07:10:32 -0700

    6402653 pool_set_status() service state changes should be permanent, not temporary

--- usr/src/lib/libpool/common/pool.c
+++ usr/src/lib/libpool/common/pool.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,8 +18,9 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -735,13 +735,13 @@ pool_set_status(int state)
 		if (getenv("SMF_FMRI") == NULL) {
 			FILE *p;
 			if (state) {
-				char *cmd = "/usr/sbin/svcadm enable -st " \
+				char *cmd = "/usr/sbin/svcadm enable -s " \
 				    SMF_SVC_INSTANCE;
 				if ((p = popen(cmd, "w")) == NULL ||
 				    pclose(p) != 0)
 					return (PO_FAIL);
 			} else {
-				char *cmd = "/usr/sbin/svcadm disable -st " \
+				char *cmd = "/usr/sbin/svcadm disable -s " \
 				    SMF_SVC_INSTANCE;
 				if ((p = popen(cmd, "w")) == NULL ||
 				    pclose(p) != 0)
