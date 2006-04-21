d3aa0f72925fc33d815f2fcc39e259e22dfff35d
anish <none@none>
2006-04-21 11:07:07 -0700

    6410294 PCIE nexus driver incorrect assigns 'PCIe-PCI bridge' model to SysKonnect SK-9I22 ExpressModule

--- usr/src/uts/i86pc/io/pciex/pcie_ck804_boot.c
+++ usr/src/uts/i86pc/io/pciex/pcie_ck804_boot.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -21,7 +20,7 @@
  */
 
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -82,7 +81,8 @@ check_if_device_is_pciex(uchar_t bus, uchar_t dev, uchar_t func,
 			 * device is a PCIe2PCI bridge
 			 */
 			*is_pci_bridge =
-			    (status & PCIE_PCIECAP_DEV_TYPE_PCIE2PCI) ? 1 : 0;
+			    ((status & PCIE_PCIECAP_DEV_TYPE_PCIE2PCI) ==
+			    PCIE_PCIECAP_DEV_TYPE_PCIE2PCI) ? 1 : 0;
 
 			/*
 			 * Check for "Slot  Implemented" bit
