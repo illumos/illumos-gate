f1f711336964964c391c797fdd56f4b3904e811d
bustos <none@none>
2006-04-22 17:28:28 -0700

    6325312 mfstscan dumps core given non-existent file Contributed by Yann Poupet <yann.poupet@free.fr>.

--- usr/src/cmd/svc/mfstscan/mfstscan.c
+++ usr/src/cmd/svc/mfstscan/mfstscan.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -120,7 +119,14 @@ main(int argc, char *argv[])
 
 	for (i = optind; i < argc; i++) {
 		if (tflag) {
-			(void) puts(mhash_filename_to_propname(argv[i]));
+			char *pname = mhash_filename_to_propname(argv[i]);
+
+			if (pname != NULL)
+				(void) puts(pname);
+			else
+				uu_warn(gettext("cannot resolve pathname "
+				    "for %s"), argv[i]);
+
 			continue;
 		}
 
