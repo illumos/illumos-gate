bc2e39ce7235bea393ca9d5c28ff3571b401bc09
sommerfe <none@none>
2006-04-24 12:03:54 -0700

    6416849 devinfo -p not consistent in its output format

--- usr/src/cmd/devinfo/devinfo.c
+++ usr/src/cmd/devinfo/devinfo.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -145,7 +144,7 @@ partinfo(int fd, char *device)
 
 	if ((slice = readvtoc(fd, device, &vtdata)) >= 0) {
 
-		(void) printf("%s\t%0lx	%0lx\t%ld\t%ld\t%x\t%x\n",
+		(void) printf("%s\t%0lx\t%0lx\t%ld\t%ld\t%x\t%x\n",
 			device, maj, min,
 			vtdata.v_part[slice].p_start,
 			vtdata.v_part[slice].p_size,
@@ -153,7 +152,7 @@ partinfo(int fd, char *device)
 			vtdata.v_part[slice].p_tag);
 	} else if ((slice == VT_ENOTSUP) &&
 	    (slice = efi_alloc_and_read(fd, &efi)) >= 0) {
-		(void) printf("%s\t%lx  %lx\t%lld\t%lld\t%hx\t%hx\n",
+		(void) printf("%s\t%lx\t%lx\t%lld\t%lld\t%hx\t%hx\n",
 			device, maj, min,
 			efi->efi_parts[slice].p_start,
 			efi->efi_parts[slice].p_size,
