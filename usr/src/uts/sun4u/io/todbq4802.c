4295e777ecfd95d75f96d8b95a2eae6136debf96
rameshc <none@none>
2006-04-24 10:05:52 -0700

    6407867 WARNING: Time of Day clock error: reason [Jumped by 0xe11]. --  Stopped tracking Time Of Day clock.

--- usr/src/uts/sun4u/io/todbq4802.c
+++ usr/src/uts/sun4u/io/todbq4802.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -103,7 +102,7 @@ _init(void)
 		if (v_rtc_addr_reg == NULL)
 			cmn_err(CE_PANIC, "addr not set, cannot read RTC\n");
 
-		BQ4802_DATA_REG(RTC_CNTRL) = (RTC_DSE | RTC_HM | RTC_STOP_N);
+		BQ4802_DATA_REG(RTC_CNTRL) = (RTC_HM | RTC_STOP_N);
 
 		/* Clear AF flag by reading reg Flags (D) */
 		(void) BQ4802_DATA_REG(RTC_FLAGS);
