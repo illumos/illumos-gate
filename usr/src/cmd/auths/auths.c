14839a76b454c7de413fbc7c57842b674873ee79
bubbva <none@none>
2006-04-26 15:16:04 -0700

    6249706 rbac utilities have unused variables Contributed by Stephen Potter <spp@unixsa.net>.

--- usr/src/cmd/auths/auths.c
+++ usr/src/cmd/auths/auths.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -124,7 +123,7 @@ show_auths(char *username, char **defauths, int defauth_cnt, int print_name)
 			return (status);
 		}
 		username = pw->pw_name;
-	} else if ((pw = getpwnam(username)) == NULL) {
+	} else if (getpwnam(username) == NULL) {
 		status = EXIT_NON_FATAL;
 		(void) fprintf(stderr, "%s: %s : ", progname, username);
 		(void) fprintf(stderr, gettext("No such user\n"));
