f2f278521a1d65f81d30778b7bd252147b2d325f
cf46844 <none@none>
2006-04-27 13:49:52 -0700

    6409995 vi causes core dump if .exrc is world or group writable; vi_01.sh in VSC dumps core

--- usr/src/cmd/vi/port/ex_cmds2.c
+++ usr/src/cmd/vi/port/ex_cmds2.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -283,9 +282,18 @@ error1(unsigned char *str)
 		setlastchar('\n');
 
 	if (inexrc) {
-	  lprintf(gettext("Error detected in .exrc.[Hit return to continue] "), 0);
-	  putNFL();
-	  getkey();
+		/*
+		 * Set inexrc to 0 so that this error is printed only
+		 * once (eg. when stdin is redirected from /dev/null and
+		 * vi prints "Input read error" because it is unable to
+		 * read() the <CR>).
+		 */
+		inexrc = 0;
+		lprintf(gettext(
+		    "Error detected in .exrc.[Hit return to continue] "),
+		    0);
+		putNFL();
+		getkey();
 	}
 
 	while ((lastchar() != '\n') && (lastchar() != EOF))
