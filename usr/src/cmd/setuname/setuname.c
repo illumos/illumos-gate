8279b0c8ffe70e1861fe29f0215bbad0a2083252
elowe <none@none>
2006-05-09 06:32:04 -0700

    6249695 setuname cmd calls kvm_nlist even if kvm_open fails Contributed by Yann Poupet <yann.poupet@free.fr>

--- usr/src/cmd/setuname/setuname.c
+++ usr/src/cmd/setuname/setuname.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -24,8 +23,8 @@
 
 
 /*
- * Copyright (c) 1997, by Sun Microsystems, Inc.
- * All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
  */
 
 #pragma ident	"%Z%%M%	%I%	%E% SMI"       /* SVr4.0 1.3 */
@@ -426,7 +425,7 @@ setuname(temp, sysname, nodename)
         /* Check out namelist and memory files. */
 	if ((kd = kvm_open(NULL, NULL, NULL, O_RDWR, NULL)) == NULL)
 		rtncd = -1;
-	if (kvm_nlist(kd, nl) != 0)
+	else if (kvm_nlist(kd, nl) != 0)
 		rtncd = -1;
 	else if (nl[0].n_value == 0)
 		rtncd = -1;
