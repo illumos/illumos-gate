c311de31b53b476c8f42e0788052a0ac5807d93d
arutz <none@none>
2006-05-11 08:13:31 -0700

    6410586 mc-us3i: can corrupt sb2500 memory, exposed with kmem_flags of 0x7f

--- usr/src/uts/sun4u/io/mc-us3i.c
+++ usr/src/uts/sun4u/io/mc-us3i.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1428,7 +1427,7 @@ mc_delete(int mc_id)
 		}
 
 		/* bank and segments go together */
-		if (!(segi = mc_node_get(banki->seg_id, seg_head))) {
+		if ((segi = mc_node_get(banki->seg_id, seg_head)) != NULL) {
 			mc_node_del((mc_dlist_t *)segi, &seg_head, &seg_tail);
 			kmem_free(segi, sizeof (*segi));
 			nsegments--;
