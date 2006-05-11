76716eaced8d7659d4594350eb3f343c31fe2806
izick <none@none>
2006-05-11 11:10:36 -0700

    6389255 aes softtoken sparcv9 will core dump if a signal occurs during assembly code call

--- usr/src/common/crypto/aes/sun4u/aes_crypt_asm.s
+++ usr/src/common/crypto/aes/sun4u/aes_crypt_asm.s
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -56,6 +55,8 @@
  *
  */
 
+#include <sys/stack.h>
+	
 #if defined(lint) || defined(__lint)
 	/* LINTED */
 	/* Nothing to be linted in this file, its pure assembly source */
@@ -2477,7 +2478,7 @@ aes_const:
 	
 #ifdef __sparcv9
 
-	save	%sp, -232, %sp
+	save	%sp, -SA(MINFRAME), %sp
 
 #ifdef PIC
 	sethi	%hi(aes_const), %l0
@@ -2521,7 +2522,7 @@ aes_const:
 	
 #else /* __sparcv9 */
 
-	save	%sp, -184, %sp
+	save	%sp, -SA(MINFRAME), %sp
 	sethi	%hi(aes_const), %l0
 
 #ifdef PIC
@@ -2994,7 +2995,7 @@ aes_const:
 	
 #ifdef __sparcv9
 
-	save	%sp, -232, %sp
+	save	%sp, -SA(MINFRAME), %sp
 
 #ifdef PIC
 	sethi	%hi(aes_const), %l0
@@ -3038,7 +3039,7 @@ aes_const:
 	
 #else /* __sparcv9 */
 	
-	save	%sp, -184, %sp
+	save	%sp, -SA(MINFRAME), %sp
 	sethi	%hi(aes_const), %l0
 
 #ifdef PIC
