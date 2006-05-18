7d7a4f21751ca8bed6480f60e0689f4c9a0433b9
zk194757 <none@none>
2006-05-18 06:53:58 -0700

    6416089 cdrw -p 24 does not set correct speed for Matshita drive

--- usr/src/cmd/cdrw/misc_scsi.h
+++ usr/src/cmd/cdrw/misc_scsi.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -80,7 +79,8 @@ struct track_info {
  * Some devices just multiply speed by 176. But more accurate ones
  * multiply speed by 176.4.
  */
-#define	CD_RATE_TO_X(r)	((r) % 176 ? ((((r)*10)+5)/1764) : (r) / 176)
+#define	CD_RATE_TO_X(r) ((r) % 176 ? ((uint_t)(((double)(r)*10)/1764 + 0.5)) :\
+		(r) / 176)
 #define	CD_X_TO_RATE(s)	((((s)*1764)+5)/10)
 
 /*
