c203fc81f70f1716c969e5594fb698596e632014
krishna <none@none>
2006-05-19 16:18:47 -0700

    6416743 kernel SSL does not work when SSL client is on the same machine as server

--- usr/src/uts/common/inet/tcp/tcp_fusion.c
+++ usr/src/uts/common/inet/tcp/tcp_fusion.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -217,10 +216,11 @@ tcp_fuse(tcp_t *tcp, uchar_t *iphdr, tcph_t *tcph)
 	 * Fuse the endpoints; we perform further checks against both
 	 * tcp endpoints to ensure that a fusion is allowed to happen.
 	 * In particular we bail out for non-simple TCP/IP or if IPsec/
-	 * IPQoS policy exists.
+	 * IPQoS policy/kernel SSL exists.
 	 */
 	if (!tcp->tcp_unfusable && !peer_tcp->tcp_unfusable &&
 	    !TCP_LOOPBACK_IP(tcp) && !TCP_LOOPBACK_IP(peer_tcp) &&
+	    tcp->tcp_kssl_ent == NULL &&
 	    !IPP_ENABLED(IPP_LOCAL_OUT|IPP_LOCAL_IN)) {
 		mblk_t *mp;
 		struct stroptions *stropt;
@@ -229,6 +229,7 @@ tcp_fuse(tcp_t *tcp, uchar_t *iphdr, tcph_t *tcph)
 		ASSERT(!TCP_IS_DETACHED(peer_tcp) && peer_rq != NULL);
 		ASSERT(tcp->tcp_fused_sigurg_mp == NULL);
 		ASSERT(peer_tcp->tcp_fused_sigurg_mp == NULL);
+		ASSERT(tcp->tcp_kssl_ctx == NULL);
 
 		/*
 		 * We need to drain data on both endpoints during unfuse.
