842620c77e31840c2879213c67efd80bc90925e8
meem <none@none>
2006-05-31 12:43:20 -0700

    6431982 dhcpagent is misreporting SIG{TERM,QUIT}

--- usr/src/cmd/cmd-inet/sbin/dhcpagent/util.c
+++ usr/src/cmd/cmd-inet/sbin/dhcpagent/util.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -502,7 +501,8 @@ inactivity_shutdown(iu_tq_t *tqp, void *arg)
 void
 graceful_shutdown(int sig)
 {
-	iu_stop_handling_events(eh, sig, drain_script, NULL);
+	iu_stop_handling_events(eh, (sig == SIGTERM ? DHCP_REASON_TERMINATE :
+	    DHCP_REASON_SIGNAL), drain_script, NULL);
 }
 
 /*
