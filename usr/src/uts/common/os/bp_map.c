7a1b360bf3af8c80bace5a9e1d4edf588782df50
cth <none@none>
2006-06-19 18:00:33 -0700

    6437833 SNV_42 panics on post installation mount for the machines happy.ireland and narcotic.ireland

--- usr/src/uts/common/os/bp_map.c
+++ usr/src/uts/common/os/bp_map.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -217,6 +216,7 @@ bp_mapout(struct buf *bp)
 			pp = *bp->b_shadow;
 		else
 			pp = bp->b_pages;
+		addr = (caddr_t)((uintptr_t)addr & MMU_PAGEMASK);
 		hat_kpm_mapout(pp, NULL, addr);
 		bp->b_flags &= ~B_REMAPPED;
 		return;
