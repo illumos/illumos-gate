17e9c21c2c124e74a7e4a7cd9fcaa3f2d31b7069
hyw <none@none>
2006-06-30 11:19:57 -0700

    6444295 OPL Olympus-C UE detected in intr packet cause spurious intrs

--- usr/src/uts/sun4u/ml/mach_interrupt.s
+++ usr/src/uts/sun4u/ml/mach_interrupt.s
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -317,6 +316,15 @@ vec_intr_spurious(void)
 	!	%g2 - panic message
 	!	%g4 - current pil
 	!
+#ifdef CLEAR_INTR_BUSYBIT_ON_SPURIOUS
+	/*
+	 * Certain processors (OPL) need to explicitly
+	 * clear the intr busy bit even though it is
+	 * not visibly set (spurious intrs)
+	 */
+	stxa	%g0, [%g0]ASI_INTR_RECEIVE_STATUS	! clear the BUSY bit
+	membar  #Sync
+#endif /* CLEAR_INTR_BUSYBIT_ON_SPURIOUS */
 	sub	%g0, 1, %g4
 	set	_not_ready, %g2
 	sethi	%hi(sys_tl1_panic), %g1
