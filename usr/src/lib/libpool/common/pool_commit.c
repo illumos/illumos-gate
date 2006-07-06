f9e2a31fcb74a362a725343aea080a7e5b858b70
sdussud <none@none>
2006-07-05 23:21:29 -0700

    6437810 commit_update() fails to locate relevant element in dynamic configuration

--- usr/src/lib/libpool/common/pool_commit.c
+++ usr/src/lib/libpool/common/pool_commit.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -295,7 +294,7 @@ commit_update(pool_elem_t *e1, pool_elem_t *e2, int pass)
 				pool_component_t *comps[2] = { NULL };
 
 				comps[0] = pool_elem_comp(e2);
-				name = elem_get_name(TO_ELEM(res2));
+				name = elem_get_name(TO_ELEM(res1));
 				newres = pool_get_resource(TO_CONF(e2),
 				    pool_elem_class_string(TO_ELEM(res1)),
 				    name);
