6ee4b8d7ef9a262e3f564e81b2b2f4d8e61ffe15
setje <none@none>
2006-07-06 18:52:53 -0700

    6412224 GRUB hangs when no i8042 is present 6412226 multiboot hangs when no i8042 is present Contributed by Juergen Keil <jk@tools.de>.

--- usr/src/psm/stand/boot/i386/common/keyboard.c
+++ usr/src/psm/stand/boot/i386/common/keyboard.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -182,8 +181,10 @@ kb_ischar(void)
 		return (1);
 
 	for (;;) {
-		buffer_stat =
-		    inb(I8042_STAT) & (I8042_STAT_OUTBF | I8042_STAT_AUXBF);
+		buffer_stat = inb(I8042_STAT);
+		if (buffer_stat == 0xff)
+			return (0);
+		buffer_stat &= (I8042_STAT_OUTBF | I8042_STAT_AUXBF);
 
 		switch (buffer_stat) {
 		case 0:
@@ -456,7 +457,11 @@ kb_translate(unsigned char code)
 void
 kb_send(unsigned char cmd)
 {
-	while (inb(I8042_STAT) & I8042_STAT_INBF)
+	int retries;
+
+	for (retries = 0;
+	    (inb(I8042_STAT) & I8042_STAT_INBF) != 0 && retries < 100000;
+	    retries++)
 		/* LOOP */;
 	outb(I8042_DATA, cmd);
 }
@@ -487,6 +492,7 @@ void
 kb_init(void)
 {
 	unsigned char pic_mask;
+	int retries;
 
 	/*
 	 * Write the command byte to turn off interrupts and
@@ -507,11 +513,15 @@ kb_init(void)
 	 * 0x01:  0 = Disable aux interrupts.
 	 */
 
-	while (inb(I8042_STAT) & I8042_STAT_INBF)
+	for (retries = 0;
+	    (inb(I8042_STAT) & I8042_STAT_INBF) != 0 && retries < 100000;
+	    retries++)
 		/* LOOP */;
 	outb(I8042_CMD, I8042_WCB);
 
-	while (inb(I8042_STAT) & I8042_STAT_INBF)
+	for (retries = 0;
+	    (inb(I8042_STAT) & I8042_STAT_INBF) != 0 && retries < 100000;
+	    retries++)
 		/* LOOP */;
 	outb(I8042_DATA, 0x64);
 
