ca45b50aef827df03f900f16fef96335005d1d7c
seb <none@none>
2006-07-11 12:20:23 -0700

    6447667 panic in getmacaddr_gldv3() during netinstall of multi-interface system

--- usr/src/uts/common/io/strplumb.c
+++ usr/src/uts/common/io/strplumb.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -770,7 +769,7 @@ getmacaddr_gldv3(char *drv, int inst, int *maclenp)
 	uchar_t *macaddr;
 
 	(void) snprintf(ifname, sizeof (ifname), "%s%d", drv, inst);
-	if (mac_open(ifname, 0, &mh) < 0) {
+	if (mac_open(ifname, inst, &mh) < 0) {
 		return (NULL);
 	}
 	*maclenp = sizeof (struct ether_addr);
