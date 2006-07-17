bc9ec910d60a3256c0b6bb68a13afa62e106730a
nakanon <none@none>
2006-07-17 13:12:08 -0700

    5066548 devfsadmd dumps core due to /etc/mnttab mntfs hazard

--- usr/src/cmd/syseventd/modules/devfsadmd_mod/devfsadmd_mod.c
+++ usr/src/cmd/syseventd/modules/devfsadmd_mod/devfsadmd_mod.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -123,6 +122,7 @@ system1(const char *s_path, const char *s)
 			/* Set-up an initial signal mask for the child */
 			(void) sigemptyset(&mask);
 			(void) sigprocmask(SIG_SETMASK, &mask, NULL);
+			closefrom(3);
 			(void) execl(s_path, s, (char *)0);
 			_exit(-1);
 			break;
