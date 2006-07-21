7f4f06d2ae918f5a65beea8cd5d9192940b9101d
petede <none@none>
2006-07-21 13:27:11 -0700

    6449938 Studio 10 and 11 compiler optimization exposes mdb scoping bugs

--- usr/src/cmd/mdb/common/mdb/mdb_fdio.c
+++ usr/src/cmd/mdb/common/mdb/mdb_fdio.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -126,9 +125,9 @@ mdb_fdio_create_path(const char *path[], const char *fname,
     int flags, mode_t mode)
 {
 	int fd;
+	char buf[MAXPATHLEN];
 
 	if (path != NULL && strchr(fname, '/') == NULL) {
-		char buf[MAXPATHLEN];
 		int i;
 
 		for (fd = -1, i = 0; path[i] != NULL; i++) {
--- usr/src/cmd/mdb/common/mdb/mdb_target.c
+++ usr/src/cmd/mdb/common/mdb/mdb_target.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -604,12 +603,12 @@ mdb_tgt_lookup_by_scope(mdb_tgt_t *t, const char *s, GElf_Sym *symp,
 {
 	const char *object = MDB_TGT_OBJ_EVERY;
 	const char *name = s;
+	char buf[MDB_TGT_SYM_NAMLEN];
 
 	if (t == NULL)
 		return (set_errno(EINVAL));
 
 	if (strchr(name, '`') != NULL) {
-		char buf[MDB_TGT_SYM_NAMLEN];
 
 		(void) strncpy(buf, s, sizeof (buf));
 		buf[sizeof (buf) - 1] = '\0';
