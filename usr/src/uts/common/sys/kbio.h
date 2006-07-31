551573c4ab9f840f01f0869572f5dec897c9cee3
rz201010 <none@none>
2006-07-30 22:23:32 -0700

    5018017 keyboard repeat delay/repeat rate should be more configurable

--- usr/src/uts/common/sys/kbio.h
+++ usr/src/uts/common/sys/kbio.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -214,7 +213,7 @@ struct kiockeymap {
 #define	KIOCSRPTRATE	(KIOC|25)
 
 /* Used to control the AutoRepeat Min-delay and Min-Rate */
-#define	KIOCRPTDELAY_MIN	(300)
+#define	KIOCRPTDELAY_MIN	(100)
 #define	KIOCRPTRATE_MIN		(30)
 
 #ifdef	__cplusplus
