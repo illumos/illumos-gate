ef69670ded4ed2349f664bb59f0d513cc0364906
cf46844 <none@none>
2006-08-23 14:02:51 -0700

    6459214 UNIX03: *vsc* xpg6 version of xargs fails null eofstr test

--- usr/src/cmd/xargs/xargs.c
+++ usr/src/cmd/xargs/xargs.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -726,8 +725,8 @@ getarg()
 end_arg:		*next++ = '\0';
 			/* we finished without requeuing so free requeue_buf */
 			free(requeue_buf);
-			if (strcmp(arg, LEOF) == 0 || (c == '\0' &&
-			    strlen(arg) == 0)) {
+			if ((strcmp(arg, LEOF) == 0 && *LEOF != '\0') ||
+			    (c == '\0' && strlen(arg) == 0)) {
 				MORE = FALSE;
 				/* absorb the rest of the line */
 				if ((c != '\n') && (c != '\0'))
