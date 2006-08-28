742e7b7f67598e6aa656a0b21427a1e017a7143e
maheshvs <none@none>
2006-08-28 08:58:29 -0700

    6458704 Solaris 10 system panics on xdrmblk_getint32

--- usr/src/uts/common/rpc/xdr_mblk.c
+++ usr/src/uts/common/rpc/xdr_mblk.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -530,6 +529,8 @@ xdrmblk_control(XDR *xdrs, int request, void *info)
 			return (FALSE);
 		int32p = (int32_t *)info;
 		len = RNDUP((int)(*int32p));
+		if (len < 0)
+		    return (FALSE);
 		while ((xdrs->x_handy -= len) < 0) {
 			if ((xdrs->x_handy += len) > 0) {
 				m->b_rptr += xdrs->x_handy;
