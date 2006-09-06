ccfce1d8e7219e7616d8df1e8cfca8f97febf5b8
na195498 <none@none>
2006-09-06 12:34:42 -0700

    6297074 gmsgfmt may cause SEGV if an entry of the header does not end with "\n" 6419809 msgfmt cmd Makefile fails to remove automatically generated files

--- usr/src/cmd/msgfmt/gnu_handle.c
+++ usr/src/cmd/msgfmt/gnu_handle.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -430,11 +429,14 @@ handle_message(struct entry *id, struct entry *str)
 			conv_init(NULL);
 		} else {
 			charset += CHARSET_LEN;
-			p = charset;
-			while ((*p != ' ') && (*p != '\t') &&
-			    (*p != '\n'))
-				p++;
-			len = p - charset;
+			p = strpbrk(charset, " \t\n");
+			if (p != NULL) {
+				/* p points to a space, tab or new line char */
+				len = p - charset;
+			} else {
+				/* not found */
+				len = strlen(charset);
+			}
 			tmp = Xmalloc(len + 1);
 			(void) memcpy(tmp, charset, len);
 			*(tmp + len) = '\0';
