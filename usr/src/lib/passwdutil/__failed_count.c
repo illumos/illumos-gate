c5d759b6c4d31d7580700457fd8d47aaf2395023
jjj <none@none>
2006-09-08 07:28:56 -0700

    6329516 unlimited password guesses allowed 6461434 saw message "/usr/lib/snmp/snmpdx: can't open the file" during rebooting on s11_b45

--- usr/src/lib/passwdutil/__failed_count.c
+++ usr/src/lib/passwdutil/__failed_count.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -55,7 +54,7 @@ __incr_failed_count(char *username, char *repname, int max_failures)
 		goto out;
 
 	/* Did we just exceed "max_failures" ? */
-	if (items[0].data.val_i == max_failures) {
+	if (items[0].data.val_i >= max_failures) {
 		syslog(LOG_AUTH|LOG_NOTICE,
 		    "Excessive (%d) login failures for %s: locking account.",
 		    max_failures, username);
