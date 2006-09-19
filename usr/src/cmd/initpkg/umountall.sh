ef4c14d24cff432d9d11d0a63d2676aa5d58fefe
setje <none@none>
2006-09-19 15:34:23 -0700

    6471546 umountall should only run bootadm when unmouting everything

--- usr/src/cmd/initpkg/umountall.sh
+++ usr/src/cmd/initpkg/umountall.sh
@@ -3,9 +3,8 @@
 # CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
 #
 # You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
 # or http://www.opensolaris.org/os/licensing.
@@ -167,9 +166,9 @@ fi
 
 ZONENAME=`zonename`
 
-# Check and if needed sync the boot archive before unmounting things.
+# Check and if needed sync the boot archive before unmounting everything.
 #
-if [ -z "${RFLAG}${NFLAG}${HFLAG}" -a "$ZONENAME" = "global" -a \
+if [ -z "${RFLAG}${NFLAG}${HFLAG}${FSType}" -a "$ZONENAME" = "global" -a \
     `uname -p` = "i386" -a -x /sbin/bootadm ] ; then
 	/sbin/bootadm -a update_all
 fi
