7f184c34de512173410aedac2f1fc0a595b150c6
sl108498 <none@none>
2006-09-25 14:14:04 -0700

    6461866 SysloglikeFormatter uses unstable "Sun proprietary" API sun.security.action.GetPropertyAction

--- usr/src/cmd/pools/poold/com/sun/solaris/service/logging/SysloglikeFormatter.java
+++ usr/src/cmd/pools/poold/com/sun/solaris/service/logging/SysloglikeFormatter.java
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -34,6 +33,7 @@ import java.text.*;
 import java.util.*;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
+import java.security.PrivilegedAction;
 
 import com.sun.solaris.service.exception.SuccinctStackTraceFormatter;
 
@@ -71,8 +71,11 @@ public class SysloglikeFormatter extends Formatter {
 	 * Line separator string.
 	 */
 	private String lineSeparator = (String)java.security.AccessController
-	    .doPrivileged(new sun.security.action.GetPropertyAction(
-	    "line.separator"));
+	    .doPrivileged(new PrivilegedAction() {
+		public Object run() {
+			return System.getProperty("line.separator");
+		}
+	});
 
 	/**
 	 * Flag to set whether log records should indicate the name of
@@ -123,7 +126,7 @@ public class SysloglikeFormatter extends Formatter {
 			dateFormatter = new SimpleDateFormat(dateFormat);
 		sb.append(dateFormatter.format(date));
 
-		if (record.getSourceClassName() != null && useClassName) {	
+		if (record.getSourceClassName() != null && useClassName) {
 			sb.append(" ");
 			sb.append(record.getSourceClassName());
 		} else if (useLoggerName) {
@@ -144,7 +147,7 @@ public class SysloglikeFormatter extends Formatter {
 			}
 		}
 
-		if (record.getSourceMethodName() != null && useMethodName) {	
+		if (record.getSourceMethodName() != null && useMethodName) {
 			sb.append(" ");
 			sb.append(record.getSourceMethodName());
 		}
