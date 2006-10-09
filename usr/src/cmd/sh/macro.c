33bc07fdaa23036c9c38a8fdba8105d9f2c744d8
craigm <none@none>
2006-10-09 15:17:13 -0700

    6471812 update 3 zones upgrade is failing with SEGV in /bin/sh from memcpy

--- usr/src/cmd/sh/macro.c
+++ usr/src/cmd/sh/macro.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 1995 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -376,7 +375,7 @@ retry:
 						newargp = fixstak();
 						assign(n, newargp);
 						tdystak(savptr);
-						memcpy(stakbot, savptr, strlngth);
+						(void) memcpystak(stakbot, savptr, strlngth);
 						staktop = stakbot + strlngth;
 					}
 					else
@@ -510,7 +509,7 @@ comsubst(int trimflag)
 		savpipe = -1;
 	}
 	tdystak(savptr);
-	memcpy(stakbot, savptr, strlngth);
+	(void) memcpystak(stakbot, savptr, strlngth);
 	oldstaktop = staktop = stakbot + strlngth;
 	while (d = readwc()) {
 		if(quote || (d == '\\' && trimflag)) {
