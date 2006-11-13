dae6ba85caff4b224e672b0291da66d93687f6ec
bubbva <none@none>
2006-11-13 11:33:00 -0800

    6489058 elfsign generates bogus activation files

--- usr/src/uts/common/sys/crypto/elfsign.h
+++ usr/src/uts/common/sys/crypto/elfsign.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -145,10 +144,11 @@ struct filesignatures {
 enum ES_ACTION {
 	ES_GET,
 	ES_GET_CRYPTO,
+	ES_UPDATE,
 	ES_UPDATE_RSA_MD5_SHA1,
 	ES_UPDATE_RSA_SHA1
 };
-#define	ES_ACTISUPDATE(a)	((a) >= ES_UPDATE_RSA_MD5_SHA1)
+#define	ES_ACTISUPDATE(a)	((a) >= ES_UPDATE)
 
 /*
  * ELF signature handling
@@ -174,6 +174,10 @@ extern ELFsign_status_t elfsign_hash(ELFsign_t ess, uchar_t *hash,
     size_t *hash_len);
 extern ELFsign_status_t elfsign_hash_mem_resident(ELFsign_t ess,
     uchar_t *hash, size_t *hash_len);
+extern ELFsign_status_t elfsign_hash_esa(ELFsign_t ess,
+    uchar_t *esa_buf, size_t esa_buf_len, uchar_t **hash, size_t *hash_len);
+extern void elfsign_buffer_len(ELFsign_t ess, size_t *ip, uchar_t *cp,
+    enum ES_ACTION action);
 
 extern ELFsign_status_t elfsign_signatures(ELFsign_t ess,
     struct filesignatures **fspp, size_t *fs_len, enum ES_ACTION action);
