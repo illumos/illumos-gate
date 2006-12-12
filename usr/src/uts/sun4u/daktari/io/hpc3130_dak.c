8e03c085279b9b72f71ed74b8f0b4db5f057d92b
zk194757 <none@none>
2006-12-12 09:54:39 -0800

    6263586 hpc3130_ioctl() incorrectly validates port number

--- usr/src/uts/sun4u/daktari/io/hpc3130_dak.c
+++ usr/src/uts/sun4u/daktari/io/hpc3130_dak.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -424,16 +423,21 @@ hpc3130_ioctl(dev_t dev, int cmd, intptr_t arg, int mode, cred_t *credp,
 	int instance = MINOR_TO_INST(getminor(dev));
 	hpc3130_slot_table_entry_t *ste;
 
-	if (!(port >= 0 && port < HPC3130_MAX_SLOT)) {
-		return (EINVAL);
-	}
-
 	unitp = (hpc3130_unit_t *)
 		ddi_get_soft_state(hpc3130soft_statep, instance);
 
 	if (unitp == NULL) {
 		D1CMN_ERR((CE_WARN, "unitp not filled"));
 		return (ENOMEM);
+	}
+
+	/*
+	 * It should be the case that the port number is a valid
+	 * index in the per instance slot table. If it is not
+	 * then we should fail out.
+	 */
+	if (!(port >= 0 && port < unitp->hpc3130_slot_table_length)) {
+		return (EINVAL);
 	}
 
 	mutex_enter(&unitp->hpc3130_mutex);
