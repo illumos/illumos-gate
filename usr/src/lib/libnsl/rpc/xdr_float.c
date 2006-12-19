5f9e186f08c9119f60f4f357c8905e75244ee0aa
gt29601 <none@none>
2006-12-19 08:41:21 -0800

    6493074 zero decoded to incorrect value by XDR on Soalris 10 x86

--- usr/src/lib/libnsl/rpc/xdr_float.c
+++ usr/src/lib/libnsl/rpc/xdr_float.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -121,7 +120,7 @@ xdr_float(XDR *xdrs, float *fp)
 
 	case XDR_ENCODE:
 #if defined(mc68000) || defined(sparc) || defined(u3b2) || \
-	defined(u3b15) || defined(i386)
+	defined(u3b15) || defined(i386) || defined(amd64)
 		return (XDR_PUTINT32(xdrs, (int *)fp));
 #else
 #if defined(vax)
@@ -196,7 +195,7 @@ xdr_float(XDR *xdrs, float *fp)
 
 	case XDR_DECODE:
 #if defined(mc68000) || defined(sparc) || defined(u3b2) || \
-	defined(u3b15) || defined(i386)
+	defined(u3b15) || defined(i386) || defined(amd64)
 		return (XDR_GETINT32(xdrs, (int *)fp));
 #else
 #if defined(vax)
