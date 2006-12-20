f6c0ee12afa096973e277ee50c7e9a04030b129b
govinda <none@none>
2006-12-19 17:46:24 -0800

    6504229 prtconf -v core dumps with snv_54

--- usr/src/cmd/prtconf/prt_xxx.c
+++ usr/src/cmd/prtconf/prt_xxx.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -65,7 +64,11 @@ static struct di_priv_format ppd_format[] = {
 		 * of nexus drivers. Note that obio driver
 		 * went away with sun4m.
 		 */
+#ifdef	__sparc
+		"central dma ebus fhc isa pci rootnex",
+#else
 		"central dma ebus fhc isa pci pci_pci rootnex",
+#endif	/* __sparc */
 		sizeof (struct ddi_parent_private_data),
 
 		sizeof (struct regspec),		/* first pointer */
