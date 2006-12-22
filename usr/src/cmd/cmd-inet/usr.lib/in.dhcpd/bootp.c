7854eba3a0b1b0f6d738d64d1fbf4e6e68f82c3c
ns92644 <none@none>
2006-12-22 06:18:54 -0800

    6418659 dhcp server provides bad address 0.0.0.0

--- usr/src/cmd/cmd-inet/usr.lib/in.dhcpd/bootp.c
+++ usr/src/cmd/cmd-inet/usr.lib/in.dhcpd/bootp.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 #pragma ident	"%Z%%M%	%I%	%E% SMI"
@@ -80,6 +79,8 @@ bootp(dsvc_clnt_t *pcd, PKT_LIST *plp)
 	char		ntoab[INET_ADDRSTRLEN], cipbuf[INET_ADDRSTRLEN];
 	char		cidbuf[DHCP_MAX_OPT_SIZE];
 	char		hbuf[NSS_BUFLEN_HOSTS];
+
+	ciaddr.s_addr = htonl(INADDR_ANY);
 
 #ifdef	DEBUG
 	dhcpmsg(LOG_DEBUG, "BOOTP request received on %s\n", ifp->nm);
