8ad9cc092881e79ce158e2a519edf49045957f80
th199096 <none@none>
2007-02-18 20:01:00 -0800

    6507643 Hash function in nfs id mapper ignores the last character in the string

--- usr/src/uts/common/fs/nfs/nfs4_idmap.c
+++ usr/src/uts/common/fs/nfs/nfs4_idmap.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -168,7 +167,7 @@ static int			nfs4_idcache_tout;
                                                                         \
 	(hash) = MOD2((key + (len)), NFSID_CACHE_ANCHORS);		\
                                                                         \
-	for (i = 0; i < ((len) - 1); i++) {				\
+	for (i = 0; i < (len); i++) {					\
 		(hash) = MOD2(((hash) + (msg)[i]), NFSID_CACHE_ANCHORS); \
 		(hash) = pkp_tab[(hash)];				\
 	}                                                               \
