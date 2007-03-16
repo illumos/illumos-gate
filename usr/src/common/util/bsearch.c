ba5f51de16765847969b9dc0eab9a7b4bc15177e
at204353 <none@none>
2007-03-16 00:54:51 -0700

    6505933 bsearch breaks if the table size is larger than 2GB

--- usr/src/common/util/bsearch.c
+++ usr/src/common/util/bsearch.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -24,7 +23,7 @@
 
 
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -49,7 +48,7 @@ bsearch(const void *ky,		/* Key to be located */
 	int (*compar)(const void *, const void *)) /* Comparison function */
 {
 	char *base;
-	ssize_t two_width;
+	size_t two_width;
 	char *last;		/* Last element in table */
 
 	if (nel == 0)
