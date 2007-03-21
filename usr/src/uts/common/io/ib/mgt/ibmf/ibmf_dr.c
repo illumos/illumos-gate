880e0df1f057217de7e4bf18b04b73d93730bdfa
rm78299 <none@none>
2007-03-21 09:33:24 -0700

    6516892 ibmf_i_dr_loopback_filter() could cause panic under memory shortage

--- usr/src/uts/common/io/ib/mgt/ibmf/ibmf_dr.c
+++ usr/src/uts/common/io/ib/mgt/ibmf/ibmf_dr.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -358,7 +357,7 @@ ibmf_i_dr_loopback_filter(ibmf_client_t *clientp, ibmf_msg_impl_t *msgimplp,
 
 		rmsgimplp = (ibmf_msg_impl_t *)kmem_zalloc(
 		    sizeof (ibmf_msg_impl_t), KM_NOSLEEP);
-		if (ret != IBMF_SUCCESS) {
+		if (rmsgimplp == NULL) {
 			IBMF_TRACE_1(IBMF_TNF_NODEBUG, DPRINT_L1,
 			    ibmf_recv_pkt_cb_err, IBMF_TNF_TRACE, "",
 			    "ibmf_i_dr_loopback_filter(): %s\n",
