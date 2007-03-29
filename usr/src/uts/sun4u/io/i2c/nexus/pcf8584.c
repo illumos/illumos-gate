ecd343b647e2ba2d0bf8f09646e721f05eb752aa
arutz <none@none>
2007-03-29 09:50:09 -0700

    4654814 smbus and sc_nct nexus must service DDI_CTLOPS_REPORTDEV

--- usr/src/uts/sun4u/io/i2c/nexus/pcf8584.c
+++ usr/src/uts/sun4u/io/i2c/nexus/pcf8584.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -568,7 +567,7 @@ pcf8584_bus_ctl(dev_info_t *dip, dev_info_t *rdip, ddi_ctl_enum_t op,
 		pcf8584_uninitchild((dev_info_t *)arg);
 
 		return (DDI_SUCCESS);
-	CTLOPS_REPORTDEV:
+	case DDI_CTLOPS_REPORTDEV:
 		pcf8584_reportdev(dip, rdip);
 
 		return (DDI_SUCCESS);
--- usr/src/uts/sun4u/io/i2c/nexus/smbus.c
+++ usr/src/uts/sun4u/io/i2c/nexus/smbus.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -462,7 +461,7 @@ smbus_bus_ctl(dev_info_t *dip, dev_info_t *rdip, ddi_ctl_enum_t op,
 		smbus_uninitchild((dev_info_t *)arg);
 
 		return (DDI_SUCCESS);
-	CTLOPS_REPORTDEV:
+	case DDI_CTLOPS_REPORTDEV:
 		smbus_reportdev(dip, rdip);
 
 		return (DDI_SUCCESS);
