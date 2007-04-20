8ef5f9c7176ceb1a2759764e9bfa99fe1ce4384b
chin <none@none>
2007-04-20 11:51:00 -0700

    6548104 *getconf* wrong value returned for SSIZE_MAX requests

--- usr/src/cmd/getconf/getconf.c
+++ usr/src/cmd/getconf/getconf.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2003 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -480,7 +479,7 @@ static struct sctab {
 
 	/* POSIX.1-1990 */
 	CHARCLASS_NAME_MAX,	"CHARCLASS_NAME_MAX",	  SELFCONF,   NOFLAGS,
-	_POSIX_SSIZE_MAX,	"SSIZE_MAX",	SELFCONF,   NOFLAGS,
+	SSIZE_MAX,		"SSIZE_MAX",	SELFCONF,   NOFLAGS,
 
 	/* POSIX.1-1988 */
 	CHAR_BIT,		"CHAR_BIT",	SELFCONF,   NOFLAGS,
