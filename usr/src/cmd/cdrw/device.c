61ee869ef3fff08996d6710c5d4f7a17bc81415c
rameshc <none@none>
2007-04-20 05:39:31 -0700

    6315147 cdrw fails audio extraction with method unknown error

--- usr/src/cmd/cdrw/device.c
+++ usr/src/cmd/cdrw/device.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -198,12 +197,6 @@ get_device(char *user_supplied, char *node)
 	if (is_old_sun_drive(dev)) {
 		dev->d_read_audio = toshiba_read_audio;
 		dev->d_speed_ctrl = toshiba_speed_ctrl;
-	} else if (use_cd_speed)  {
-		/*
-		 * Work around for Lite-on FW bug in which rt_speed_ctrl
-		 * doesn't work correctly.
-		 */
-		dev->d_speed_ctrl = cd_speed_ctrl;
 	} else {
 		/*
 		 * If the CD Read Feature is supported, READ CD will work
@@ -222,10 +215,13 @@ get_device(char *user_supplied, char *node)
 		}
 		/*
 		 * If the Real Time Streaming Feature is supported then
-		 * Real-time streaming commands can be used for speed control.
+		 * Real-time streaming commands can be used for speed control
+		 * (except when we want to use cd_speed_ctrl explicitly which
+		 * is specified by setting use_cd_speed to 1).
 		 * Otherwise try SET CD SPEED.
 		 */
-		if (ftr_supported(fd, MMC_FTR_RT_STREAM) == 1) {
+		if ((ftr_supported(fd, MMC_FTR_RT_STREAM) == 1) &&
+		    !use_cd_speed) {
 			dev->d_speed_ctrl = rt_streaming_ctrl;
 			if (debug)
 				(void) printf("using rt speed ctrl\n");
