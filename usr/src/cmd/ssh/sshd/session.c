4dd46c65567d297a973829471c9ffd03896227d1
jp161948 <none@none>
2007-05-08 02:19:03 -0700

    6527064 *sshd* uses LC_CTIME instead of LC_TIME

--- usr/src/cmd/ssh/sshd/session.c
+++ usr/src/cmd/ssh/sshd/session.c
@@ -32,7 +32,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1120,7 +1120,7 @@ do_setup_env(Session *s, const char *shell)
 	PASS_ENV("LC_ALL")
 	PASS_ENV("LC_CTYPE")
 	PASS_ENV("LC_COLLATE")
-	PASS_ENV("LC_CTIME")
+	PASS_ENV("LC_TIME")
 	PASS_ENV("LC_NUMERIC")
 	PASS_ENV("LC_MONETARY")
 	PASS_ENV("LC_MESSAGES")
