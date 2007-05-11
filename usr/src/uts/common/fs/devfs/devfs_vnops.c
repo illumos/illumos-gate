946a6cb1be200249d5bee839d90bb5599d3c4ee2
dm120769 <none@none>
2007-05-11 16:59:11 -0700

    backout 6525694: causes 6556488

--- usr/src/uts/common/fs/devfs/devfs_vnops.c
+++ usr/src/uts/common/fs/devfs/devfs_vnops.c
@@ -62,7 +62,6 @@
 
 #include <fs/fs_subr.h>
 #include <sys/fs/dv_node.h>
-#include <sys/sunndi.h>
 
 extern struct vattr	dv_vattr_dir, dv_vattr_file;
 extern dev_t rconsdev;
@@ -862,7 +861,6 @@ devfs_readdir(struct vnode *dvp, struct uio *uiop, struct cred *cred, int *eofp)
 	int error;
 	struct vattr va;
 	size_t bufsz;
-	int circ;
 
 	ddv = VTODV(dvp);
 	dcmn_err2(("devfs_readdir %s: offset %lld len %ld\n",
@@ -884,8 +882,6 @@ devfs_readdir(struct vnode *dvp, struct uio *uiop, struct cred *cred, int *eofp)
 
 	/* Load the initial contents */
 	if (ddv->dv_flags & DV_BUILD) {
-		/* Lock the underlying devi structure */
-		ndi_devi_enter(ddv->dv_devi, &circ);
 		if (!rw_tryupgrade(&ddv->dv_contents)) {
 			rw_exit(&ddv->dv_contents);
 			rw_enter(&ddv->dv_contents, RW_WRITER);
@@ -896,7 +892,6 @@ devfs_readdir(struct vnode *dvp, struct uio *uiop, struct cred *cred, int *eofp)
 			dv_filldir(ddv);
 
 		rw_downgrade(&ddv->dv_contents);
-		ndi_devi_exit(ddv->dv_devi, circ);
 	}
 
 	soff = uiop->uio_loffset;
