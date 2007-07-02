c151b7fa492e1f62c5460a2f05ba0311a16e09aa
sp92102 <none@none>
2007-07-01 19:40:49 -0700

    6573795 dumpadm should do sanity checking

--- usr/src/uts/common/io/dump.c
+++ usr/src/uts/common/io/dump.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2002 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -124,6 +123,12 @@ dump_ioctl(dev_t dev, int cmd, intptr_t arg, int mode, cred_t *cred, int *rvalp)
 						pages_locked -
 						pages_claimed -
 						pages_useclaim);
+
+			/*
+			 * Protect against vm vagaries.
+			 */
+			if (dumpsize_in_pages > (uint64_t)physmem)
+				dumpsize_in_pages = (uint64_t)physmem;
 
 			size = ptob(dumpsize_in_pages) / DUMP_COMPRESS_RATIO;
 		}
