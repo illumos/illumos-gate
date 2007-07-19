437427fb0e9def2a846945f03aca7152a1438123
ps29005 <none@none>
2007-07-19 08:08:57 -0700

    6479152 lpadmin form alignment test gets stuck in endless loop

--- usr/src/cmd/lp/cmd/lpadmin/do_align.c
+++ usr/src/cmd/lp/cmd/lpadmin/do_align.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -24,7 +23,7 @@
 
 
 /*
- * Copyright 2002 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -634,9 +633,13 @@ static int		again ()
 		gettext("Press return to print an alignment pattern [q to quit]: ")
 		);
 
+		if (!fgets(answer, sizeof (answer), stdin))
+			return (0);
+
+		answer[strlen(answer) -1] = '\0';
+
 		if (
-			!fgets(answer, sizeof (answer), stdin)
-		     || STREQU(answer, "q")
+		        STREQU(answer, "q")
 		     || STREQU(answer, "n")
 		     || STREQU(answer, "no")
 		)
