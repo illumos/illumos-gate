74ed3e963b20ba6344270ca5163d3cfb1565c0c6
keerthi <none@none>
2007-08-08 12:41:08 -0700

    4815729 hasmntopt(3C) incorrectly finds substrings

--- usr/src/lib/libc/port/gen/getmntent.c
+++ usr/src/lib/libc/port/gen/getmntent.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,8 +18,9 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -194,13 +194,21 @@ hasmntopt(struct mnttab *mnt, char *opt)
 {
 	char tmpopts[MNT_LINE_MAX];
 	char *f, *opts = tmpopts;
+	size_t	len;
 
 	if (mnt->mnt_mntopts == NULL)
 		return (NULL);
 	(void) strcpy(opts, mnt->mnt_mntopts);
+	len = strlen(opt);
 	f = mntopt(&opts);
 	for (; *f; f = mntopt(&opts)) {
-		if (strncmp(opt, f, strlen(opt)) == 0)
+		/*
+		 * Match only complete substrings. For options
+		 * which use a delimiter (such as 'retry=3'),
+		 * treat the delimiter as the end of the substring.
+		 */
+		if (strncmp(opt, f, len) == 0 &&
+		    (f[len] == '\0' || !isalnum(f[len])))
 			return (f - tmpopts + mnt->mnt_mntopts);
 	}
 	return (NULL);
