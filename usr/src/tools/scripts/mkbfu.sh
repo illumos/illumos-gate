71d2b1316bb06034c80cbc447eb01503c67796d9
rscott <none@none>
2007-08-08 10:34:47 -0700

    6589236 mkbfu and bfu argue over $(PLATFORM) vs. $(CLASS)

--- usr/src/tools/scripts/mkbfu.sh
+++ usr/src/tools/scripts/mkbfu.sh
@@ -3,9 +3,8 @@
 # CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
 #
 # You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
 # or http://www.opensolaris.org/os/licensing.
@@ -21,7 +20,7 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+# Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 # 
 # ident	"%Z%%M%	%I%	%E% SMI"
@@ -127,7 +126,7 @@ fi
 PROTO=$1
 CPIODIR=$2
 
-PLATFORM=`uname -i`
+CLASS=`uname -m`
 
 [ -d $PROTO ] || fail "Proto directory $PROTO does not exist."
 
@@ -198,8 +197,8 @@ do
 done
 
 if [ -d boot ]; then
-	( find boot -depth -print | create_archive $PLATFORM boot ) \
-	    2>$CPIODIR/${PLATFORM}.boot.err &
+	( find boot -depth -print | create_archive $CLASS boot ) \
+	    2>$CPIODIR/${CLASS}.boot.err &
 	bgcheck
 fi
 
