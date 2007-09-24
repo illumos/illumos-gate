4ac394880b9cfb15287d3e33a9b92c51240c4776
prasad <none@none>
2007-09-24 14:51:26 -0700

    6603261 panic: mutex_enter: bad mutex, at pciehpc_attn_btn_handler()
    --HG--
    rename : usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_nvidia.c => deleted_files/usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_nvidia.c
    rename : usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_nvidia.h => deleted_files/usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_nvidia.h

--- usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_x86.c
+++ usr/src/uts/intel/io/hotplug/pciehpc/pciehpc_x86.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -21,7 +20,7 @@
  */
 
 /*
- *  Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ *  Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  *  Use is subject to license terms.
  */
 
@@ -44,7 +43,6 @@
 
 extern int pciehpc_acpi_hotplug_enabled(dev_info_t *dip);
 extern void pciehpc_acpi_setup_ops(pciehpc_t *ctrl_p);
-extern void pciehpc_ck804_update_ops(pciehpc_t *ctrl_p);
 
 /*
  * Update ops vector with platform specific (ACPI, CK8-04,...) functions.
@@ -57,8 +55,5 @@ pciehpc_update_ops(pciehpc_t *ctrl_p)
 		/* update ops vector for ACPI mode */
 		pciehpc_acpi_setup_ops(ctrl_p);
 		ctrl_p->hp_mode = PCIEHPC_ACPI_HP_MODE;
-	} else {
-		/* check for CK8-04 and update the ops if necessary */
-		(void) pciehpc_ck804_update_ops(ctrl_p);
 	}
 }
