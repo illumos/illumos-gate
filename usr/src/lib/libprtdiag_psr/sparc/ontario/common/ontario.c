0c46f359faca211007ef8ee972a1391f96fc9bdc
anovick <none@none>
2007-11-01 11:58:09 -0700

    6621110 T6320 needs to lose its platform-specific prtdiag_psr overlay
    --HG--
    rename : usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale.c => deleted_files/usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale.c
    rename : usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale.h => deleted_files/usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale.h
    rename : usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale_common.h => deleted_files/usr/src/lib/libprtdiag_psr/sparc/ontario/common/glendale_common.h

--- usr/src/lib/libprtdiag_psr/sparc/ontario/common/ontario.c
+++ usr/src/lib/libprtdiag_psr/sparc/ontario/common/ontario.c
@@ -56,7 +56,6 @@
 #include "pelton.h"
 #include "stpaul.h"
 #include "huron.h"
-#include "glendale_common.h"
 
 #if !defined(TEXT_DOMAIN)
 #define	TEXT_DOMAIN	"SYS_TEST"
@@ -360,12 +359,6 @@ sun4v_display_pci(picl_nodehdl_t plafh)
 			    "niu", huron_pci_callback);
 			(void) picl_walk_tree_by_class(plafh, "pciex",
 			    "pciex", huron_pci_callback);
-	} else if ((strncmp(platbuf, GLENDALE_PLATFORM,
-	    strlen(GLENDALE_PLATFORM))) == 0) {
-		(void) picl_walk_tree_by_class(plafh, "sun4v",
-		    "niu", glendale_pci_callback);
-		(void) picl_walk_tree_by_class(plafh, "pciex",
-		    "pciex", glendale_pci_callback);
 	} else {
 		(void) picl_walk_tree_by_class(plafh, "pciex", "pciex",
 		    erie_pci_callback);
@@ -588,20 +581,6 @@ sun4v_display_hw_revisions(Prom_node *root, picl_nodehdl_t plafh)
 		    "network", huron_hw_rev_callback);
 		(void) picl_walk_tree_by_class(plafh, "scsi-2", "scsi-2",
 		    huron_hw_rev_callback);
-	} else if ((strncmp(platbuf, GLENDALE_PLATFORM,
-	    strlen(GLENDALE_PLATFORM))) == 0) {
-		(void) picl_walk_tree_by_class(plafh, "pciex",
-		    "pciex", glendale_hw_rev_callback);
-		(void) picl_walk_tree_by_class(plafh, "sun4v",
-		    "niu", glendale_hw_rev_callback);
-		(void) picl_walk_tree_by_class(plafh, "pci",
-		    "pci", glendale_hw_rev_callback);
-		(void) picl_walk_tree_by_class(plafh, "sun4v",
-		    "pci", glendale_hw_rev_callback);
-		(void) picl_walk_tree_by_class(plafh, "network",
-		    "network", glendale_hw_rev_callback);
-		(void) picl_walk_tree_by_class(plafh, "scsi-2",
-		    "scsi-2", glendale_hw_rev_callback);
 	} else {
 		(void) picl_walk_tree_by_class(plafh, "pciex", "pciex",
 		    erie_hw_rev_callback);
