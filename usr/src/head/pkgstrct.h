47f7505b27117d2fd560a8f9c14b99e8bf031263
suha <none@none>
2007-11-05 18:15:15 -0800

    backout 6526496: causes 6626125

--- usr/src/head/pkgstrct.h
+++ usr/src/head/pkgstrct.h
@@ -2,8 +2,9 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").
- * You may not use this file except in compliance with the License.
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -22,8 +23,8 @@
 /*	  All Rights Reserved  	*/
 
 /*
- * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
+ * Copyright (c) 1994, by Sun Microsystems, Inc.
+ * All rights reserved.
  */
 
 #ifndef	_PKGSTRCT_H
@@ -63,9 +64,9 @@ struct ainfo {
 };
 
 struct cinfo {
-	long		cksum;
-	fsblkcnt_t	size;
-	time_t		modtime;
+	long	cksum;
+	long	size;
+	time_t	modtime;
 };
 
 struct pinfo {
