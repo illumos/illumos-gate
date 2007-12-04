d33344bb58c147b58c280160c390eec316f9e813
sy25831 <none@none>
2007-12-04 10:05:53 -0800

    6581184 installgrub doesn't work if there is there is a solaris and linux swap are defined

--- usr/src/cmd/boot/installgrub/message.h
+++ usr/src/cmd/boot/installgrub/message.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -65,6 +64,12 @@ extern "C" {
 #define	OPEN_FAIL_FILE	gettext("cannot open %s\n")
 
 #define	OPEN_FAIL_PCFS	gettext("cannot open /boot/grub/stage2 on pcfs\n")
+
+#define	PART_FAIL	\
+	gettext("cannot get the partition information of the disk\n")
+
+#define	BAD_PART	\
+	gettext("Partition %d of the disk has an incorrect offset\n")
 
 #define	READ_FAIL_STAGE1	gettext("cannot read stage1 file %s\n")
 
