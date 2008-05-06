5068e65be2e7b42bed0d9c76ac5dfb2874ff4385
sv204098 <none@none>
2008-05-06 09:33:59 -0700

    4305097 accounting fails to create an active<date> file it claims to create

--- usr/src/cmd/acct/runacct.sh
+++ usr/src/cmd/acct/runacct.sh
@@ -3,9 +3,8 @@
 # CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
 #
 # You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
 # or http://www.opensolaris.org/os/licensing.
@@ -22,7 +21,7 @@
 #
 #       Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T
 #         All Rights Reserved
-#	Copyright 2003 Sun Microsystems, Inc.  All rights reserved.
+#	Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
 #	Use is subject to license terms.
 
 
@@ -225,7 +224,7 @@ acctcon -l lineuse -o reboots < tmpwtmp  2> log > ctacct.${_date}
 # 
 if test -s log ; then 
 	(date ; cat ${_nite}/log) | mailx adm root
-	echo "${_errormsg}" | logger -p daemon.err
+	echo "\n\n************ ACCT ERRORS : see  ${_nite}log********\n\n" | logger -p daemon.err
 	cat ${_nite}/log >> ${_active}${_date}
 fi
 
