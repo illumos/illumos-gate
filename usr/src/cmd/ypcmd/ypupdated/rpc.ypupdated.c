66786d5e1a8c8843193d1d6d52d6c97f3ad96a2e
djl <none@none>
2008-05-12 08:49:51 -0700

    6679204 rpc.ypupdated(1M) may allow execution of arbitrary code

--- usr/src/cmd/ypcmd/ypupdated/rpc.ypupdated.c
+++ usr/src/cmd/ypcmd/ypupdated/rpc.ypupdated.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ *  You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -305,11 +304,13 @@ update(requester, mapname, op, keylen, key, datalen, data)
 	err = yp_match(default_domain, mapname,
 			fake_key, strlen(fake_key), &outval, &outval_len);
 	switch (err) {
-		case YPERR_MAP:
-			return (YPERR_YPERR);
+		case 0:
+		case YPERR_KEY:
+			/* do nothing, only worry about above return code */
 			break;
 		default:
-			/* do nothing, only worry about above return code */
+			/* defensive programming */
+			return (YPERR_YPERR);
 			break;
 	}
 
