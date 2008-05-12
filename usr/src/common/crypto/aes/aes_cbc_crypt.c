0d4a0bb3f17c7804d5070ef0b598571de5bfe91f
mcpowers <none@none>
2008-05-12 15:57:29 -0700

    6615681 aes-ccm failed with the ieee802.11i test case

--- usr/src/common/crypto/aes/aes_cbc_crypt.c
+++ usr/src/common/crypto/aes/aes_cbc_crypt.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1250,6 +1250,7 @@ aes_ccm_format_initial_blocks(uchar_t *nonce, ulong_t nonceSize,
 	bzero(&(cb[1+nonceSize]), q);
 
 	/* Create the mask for the counter field based on the size of nonce */
+	q <<= 3;
 	while (q-- > 0) {
 		mask |= (1ULL << q);
 	}
