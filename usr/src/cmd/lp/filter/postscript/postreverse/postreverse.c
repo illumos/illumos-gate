5699d47b2c2478c973f1ae011c628b3e2f24de88
ps29005 <none@none>
2008-05-13 11:47:48 -0700

    6645820 postreverse(1) may dump core on memory allocation in DocumentPages()

--- usr/src/cmd/lp/filter/postscript/postreverse/postreverse.c
+++ usr/src/cmd/lp/filter/postscript/postreverse/postreverse.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -524,14 +523,14 @@ DocumentPages(DOCUMENT * document)
 	char *label, *tmp, *tmp_end;
 
 	for (ptr1 = nstrstr(document->start, PS_PAGE, bptr); ptr1 != NULL;
-		ptr1 = nstrstr(++ptr1, PS_PAGE, bptr)) {
+	    ptr1 = nstrstr(++ptr1, PS_PAGE, bptr)) {
 		no_pages++;
 
 		if (no_pages > allocated_slots) {
 			pages = (PAGE **) nrealloc(pages,
-			(allocated_slots + BLOCKSIZE) * sizeof (PAGE *));
-			memset(pages + allocated_slots * sizeof (PAGE *), 0,
-				BLOCKSIZE * sizeof (PAGE *));
+			    (allocated_slots + BLOCKSIZE) * sizeof (PAGE *));
+			memset(pages + allocated_slots, 0,
+			    BLOCKSIZE * sizeof (PAGE *));
 			allocated_slots += BLOCKSIZE;
 		}
 		page = (PAGE *) nmalloc(sizeof (PAGE));
