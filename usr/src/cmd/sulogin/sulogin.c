3289e1bdec732dbdf04517907516fd0d00f6a6f8
nakanon <none@none>
2008-05-20 15:17:46 -0700

    6673637 sulogin resets echoctl (ECHOCTL flag) in single user mode

--- usr/src/cmd/sulogin/sulogin.c
+++ usr/src/cmd/sulogin/sulogin.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -357,12 +356,12 @@ static void
 sanitize_tty(int fd)
 {
 	(void) ioctl(fd, TCGETA, &ttymodes);
-	ttymodes.c_iflag |= SET_IFLAG;
 	ttymodes.c_iflag &= ~RESET_IFLAG;
-	ttymodes.c_oflag |= SET_OFLAG;
+	ttymodes.c_iflag |= SET_IFLAG;
 	ttymodes.c_oflag &= ~RESET_OFLAG;
-	ttymodes.c_lflag |= SET_LFLAG;
+	ttymodes.c_oflag |= SET_OFLAG;
 	ttymodes.c_lflag &= ~RESET_LFLAG;
+	ttymodes.c_lflag |= SET_LFLAG;
 	ttymodes.c_cc[VERASE] = CERASE;
 	ttymodes.c_cc[VKILL] = CKILL;
 	ttymodes.c_cc[VQUIT] = CQUIT;
