23310c0096a05972772d3765714742fb3873fa2f
keerthi <none@none>
2008-06-11 16:39:47 -0700

    n/a

--- usr/src/cmd/lp/model/netpr/net.c
+++ usr/src/cmd/lp/model/netpr/net.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -21,7 +20,7 @@
  */
 
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -149,7 +148,7 @@ xfer_file(int sockfd, caddr_t pa, int filesize, int seed)
 	syslog(LOG_DEBUG, "xfer_file: timeout = %d", timeout);
 
 		(void) signal(SIGALRM, null_sighandler);
-		(void) alarm(2);
+		(void) alarm(10);
 		nw = write(sockfd, pa, ctr);
 	syslog(LOG_DEBUG, "xfer_file: write while loop => nw = %d", nw);
 		(void) alarm(0);
