fd80436ba046ae97faae41e9b481a95fffae6cb9
gm89044 <none@none>
2008-06-17 08:39:46 -0700

    6712888 remove sccs keywords from crypto driver module identification string

--- usr/src/uts/sun4v/io/n2rng/n2rng.c
+++ usr/src/uts/sun4v/io/n2rng/n2rng.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -84,7 +84,7 @@ static struct dev_ops devops = {
  */
 static struct modldrv modldrv = {
 	&mod_driverops,			/* drv_modops */
-	"N2 RNG Driver v%I%",		/* drv_linkinfo */
+	"N2 RNG Driver",		/* drv_linkinfo */
 	&devops,			/* drv_dev_ops */
 };
 
