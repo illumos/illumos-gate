9db67a327daf1243e630c20b81978ffd2a7baad7
anbui <none@none>
2008-06-30 18:00:28 -0700

    6578791 grfan is missing the CDDL

--- usr/src/uts/sun4u/grover/io/grfans.c
+++ usr/src/uts/sun4u/grover/io/grfans.c
@@ -1,5 +1,25 @@
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -73,7 +93,7 @@ static struct dev_ops grfans_ops = {
 
 static struct modldrv grfans_modldrv = {
 	&mod_driverops,		/* type of module - driver */
-	"grfans device driver v%I%",
+	"grfans device driver",
 	&grfans_ops,
 };
 
@@ -94,7 +114,7 @@ _init(void)
 	error = mod_install(&grfans_modlinkage);
 	if (error == 0) {
 		(void) ddi_soft_state_init(&grfans_soft_statep,
-			sizeof (struct grfans_unit), 1);
+		    sizeof (struct grfans_unit), 1);
 	}
 
 	return (error);
