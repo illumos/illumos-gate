664a5ea1dff1ae974e2937d67d09de30f0c098e9
robj <none@none>
2008-07-31 19:21:46 -0700

    PSARC 2008/463 Extending HC FMRI scheme to represent sensors/indicators 6635159 libtopo: extend hc scheme to allow for representing sensors and indicators in the topology (missing files)

--- usr/src/lib/fm/topo/libtopo/common/topo_list.h
+++ usr/src/lib/fm/topo/libtopo/common/topo_list.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -34,11 +33,6 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-
-typedef struct topo_list {
-	struct topo_list *l_prev;
-	struct topo_list *l_next;
-} topo_list_t;
 
 #define	topo_list_prev(elem)	((void *)(((topo_list_t *)(elem))->l_prev))
 #define	topo_list_next(elem)	((void *)(((topo_list_t *)(elem))->l_next))
