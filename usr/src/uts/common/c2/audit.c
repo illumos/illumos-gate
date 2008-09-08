950d67053b32c6cf19ecb9eba01f07543bef1558
Paul Wernau <Paul.Wernau@Sun.COM>
2008-09-08 15:22:31 -0400

    6743863 *c2audit* panics system when flushing non-existent policy

--- usr/src/uts/common/c2/audit.c
+++ usr/src/uts/common/c2/audit.c
@@ -19,15 +19,13 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
 /*
  * This file contains the audit hook support code for auditing.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include <sys/types.h>
 #include <sys/proc.h>
@@ -1859,8 +1857,13 @@ add_return_token(caddr_t *ad, unsigned int scid, int err, int rval)
 	unsigned int sy_flags;
 
 #ifdef _SYSCALL32_IMPL
-	if (lwp_getdatamodel(
-		ttolwp(curthread)) == DATAMODEL_NATIVE)
+	/*
+	 * Guard against t_lwp being NULL when this function is called
+	 * from a kernel queue instead of from a direct system call.
+	 * In that case, assume the running kernel data model.
+	 */
+	if ((curthread->t_lwp == NULL) || (lwp_getdatamodel(
+	    ttolwp(curthread)) == DATAMODEL_NATIVE))
 		sy_flags = sysent[scid].sy_flags & SE_RVAL_MASK;
 	else
 		sy_flags = sysent32[scid].sy_flags & SE_RVAL_MASK;
