782793a8b246388db78d0ec779167f35350516f7
Vikram Hegde <Vikram.Hegde@Sun.COM>
2008-09-15 08:52:21 -0700

    6714111 Solaris needs to support the Intel IOMMU (fix sparc build)

--- usr/src/uts/common/os/main.c
+++ usr/src/uts/common/os/main.c
@@ -344,8 +344,10 @@ start_init(void)
 	lwp_rtt();
 }
 
+#if defined(__i386) || defined(__amd64)
 extern void return_instr(void);
 void (*rootnex_iommu_add_intr)(void) = (void (*)(void))return_instr;
+#endif
 
 void
 main(void)
@@ -447,10 +449,12 @@ main(void)
 	(void) spl0();
 	interrupts_unleashed = 1;
 
+#if defined(__i386) || defined(__amd64)
 	/*
 	 * add intel iommu fault event handler
 	 */
 	rootnex_iommu_add_intr();
+#endif
 
 	vfs_mountroot();	/* Mount the root file system */
 	errorq_init();		/* after vfs_mountroot() so DDI root is ready */
