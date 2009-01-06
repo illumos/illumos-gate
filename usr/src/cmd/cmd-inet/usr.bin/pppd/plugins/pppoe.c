dc632b73287253d17d0d918869bc5e852e5d3bb7
James Carlson <james.d.carlson@sun.com>
2009-01-06 14:23:15 -0500

    6631055 PPPoE comments need to be cleaned up

--- usr/src/cmd/cmd-inet/usr.bin/pppd/plugins/pppoe.c
+++ usr/src/cmd/cmd-inet/usr.bin/pppd/plugins/pppoe.c
@@ -1,8 +1,31 @@
 /*
- * pppoe.c - pppd plugin to handle PPPoE operation.
+ * CDDL HEADER START
  *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
  * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
+ */
+
+/*
+ * pppoe.c - pppd plugin to handle PPPoE operation.
  */
 
 #include <unistd.h>
@@ -20,8 +43,6 @@
 
 #include "pppd.h"
 #include "pathnames.h"
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 /* Saved hook pointers */
 static int (*old_check_options)(uid_t uid);
--- usr/src/uts/common/io/ppp/sppptun/sppptun_mod.h
+++ usr/src/uts/common/io/ppp/sppptun/sppptun_mod.h
@@ -1,14 +1,35 @@
 /*
- * sppptun_mod.h - References between sppptun.c and sppptun_mod.c
+ * CDDL HEADER START
  *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
  * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
+/*
+ * sppptun_mod.h - References between sppptun.c and sppptun_mod.c
+ */
+
 #ifndef	_SPPPTUN_MOD_H
 #define	_SPPPTUN_MOD_H
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #ifdef	__cplusplus
 extern "C" {
