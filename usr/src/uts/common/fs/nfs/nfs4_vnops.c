2a1607d0851752e51b37d665b6605ebb7e480562
Dai Ngo <dai.ngo@sun.com>
2009-01-15 10:46:34 -0800

    6583602 nfs4lookup_setup() incorrectly strips a leading dot from the first component

--- usr/src/uts/common/fs/nfs/nfs4_vnops.c
+++ usr/src/uts/common/fs/nfs/nfs4_vnops.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -6097,7 +6097,7 @@ nfs4lookup_setup(char *nm, lookup4_param_t *lookupargp, int needgetfh)
 			break;
 
 		/* ".." is counted as 1 component */
-		if (*nm == '.' && *(nm + 1) == '.')
+		if (*nm == '.' && *(nm + 1) != '/')
 			break;
 	}
 
