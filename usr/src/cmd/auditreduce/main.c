77e01d41d44b2e8d121c8ab6f07828f307c1a1c3
Tony Nguyen <Ton.Nguyen@Sun.COM>
2009-02-24 10:10:38 -0800

    6658421 auditreduce can core on SEGV

--- usr/src/cmd/auditreduce/main.c
+++ usr/src/cmd/auditreduce/main.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -19,13 +18,11 @@
  *
  * CDDL HEADER END
  */
+
 /*
- * Copyright 1987-2000, 2002 Sun Microsystems, Inc.
- * All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 /*
  * The Secure SunOS audit reduction tool - auditreduce.
@@ -986,6 +983,10 @@ init_sig(void)
 	}
 	if (signal(SIGXFSZ, int_handler) == SIG_ERR) {
 		perror(gettext("auditreduce: SIGXFSZ signal failed"));
+		return (-1);
+	}
+	if (signal(SIGSEGV, int_handler) == SIG_ERR) {
+		perror(gettext("auditreduce: SIGSEGV signal failed"));
 		return (-1);
 	}
 
