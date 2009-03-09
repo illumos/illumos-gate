b0d0a1c8457bb524369930a3ec38e33ee968f690
Viswanathan Kannappan <Viswanathan.Kannappan@Sun.COM>
2009-03-09 10:11:26 -0700

    6626020 crontab doesn't have have non-zero exit value for out-of-bounds errors Contributed by Chad Mynhier <cmynhier@gmail.com>

--- usr/src/cmd/cron/crontab.c
+++ usr/src/cmd/cron/crontab.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
@@ -544,8 +544,9 @@ cont:
 			unlink(tnam);
 			crabort(BADCREATE);
 		}
-	} else
-		fprintf(stderr, "crontab: %s\n", gettext(ERRSFND));
+	} else {
+		crabort(ERRSFND);
+	}
 	unlink(tnam);
 }
 
