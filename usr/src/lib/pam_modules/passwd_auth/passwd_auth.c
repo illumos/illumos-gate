3ed4a803a4d4b35e2773c194c9a6f4977687b542
Joep Vesseur <Joep.Vesseur@Sun.COM>
2009-03-23 14:34:40 +0100

    6699885 misconfiguring the passwd auth stack leads to unexpected results

--- usr/src/lib/pam_modules/passwd_auth/passwd_auth.c
+++ usr/src/lib/pam_modules/passwd_auth/passwd_auth.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,11 +19,9 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include <sys/types.h>
 #include <sys/varargs.h>
@@ -120,7 +117,7 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 		return (res);
 
 	if (password != NULL)
-		return (PAM_SUCCESS);
+		return (PAM_IGNORE);
 
 	res = pam_get_item(pamh, PAM_SERVICE, (void **)&service);
 	if (res != PAM_SUCCESS)
@@ -240,9 +237,9 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 	 * return PAM_IGNORE
 	 */
 	if (server_policy &&
-		strcmp(repository_name, "files") != 0 &&
-		strcmp(repository_name, "nis") != 0 &&
-		strcmp(repository_name, "nisplus") != 0) {
+	    strcmp(repository_name, "files") != 0 &&
+	    strcmp(repository_name, "nis") != 0 &&
+	    strcmp(repository_name, "nisplus") != 0) {
 		retval = PAM_IGNORE;
 		goto out;
 	}
