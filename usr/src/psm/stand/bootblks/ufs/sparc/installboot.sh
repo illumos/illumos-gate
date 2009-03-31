e2960effe6eeb690387f66e897aa8dc1cf651bc7
batschul <Frank.Batschulat@Sun.COM>
2009-03-31 12:13:36 -0600

    6804418 installboot gets dd from PATH, fails on OpenSolaris

--- usr/src/psm/stand/bootblks/ufs/sparc/installboot.sh
+++ usr/src/psm/stand/bootblks/ufs/sparc/installboot.sh
@@ -20,11 +20,11 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+# Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
-#ident	"%Z%%M%	%I%	%E% SMI"
-#
+
+PATH=/usr/bin
 
 away() {
 	echo $2 1>&2
