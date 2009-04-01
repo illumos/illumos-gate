ef287aad55c36c55e43651ee7ec7aa169b89408d
Greg Price <gap@sun.com>
2009-04-02 07:57:23 +1100

    6696813 mpt's NVDATA version information needs externalizing

--- usr/src/uts/common/sys/mpt/mpi_cnfg.h
+++ usr/src/uts/common/sys/mpt/mpi_cnfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -1284,7 +1284,8 @@ typedef struct mpi_sas_io_unit0_phy_data {
 
 typedef struct config_page_sas_io_unit_0 {
 	config_extended_page_header_t	Header;
-	uint32_t			Reserved1;
+	uint16_t			NvdataVersionDefault;
+	uint16_t			NvdataVersionPersistent;
 	uint8_t				NumPhys;
 	uint8_t				Reserved2;
 	uint16_t			Reserved3;
