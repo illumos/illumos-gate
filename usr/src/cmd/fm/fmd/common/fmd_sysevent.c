43e32e83c61440df3d45dec349b96cb858ed949b
Stephen Hanson <Stephen.Hanson@Sun.COM>
2009-06-25 01:51:48 -0700

    6849551 Many duplicated ereports are delivered to the DE

--- usr/src/cmd/fm/fmd/common/fmd_sysevent.c
+++ usr/src/cmd/fm/fmd/common/fmd_sysevent.c
@@ -115,12 +115,17 @@ sysev_legacy(sysevent_t *sep)
 
 	/* construct the event payload */
 	(void) nvlist_xalloc(&nvl, NV_UNIQUE_NAME, &fmd.d_nva);
-	(void) nvlist_add_string(nvl, FM_CLASS, fullclass);
-	(void) nvlist_add_uint8(nvl, FM_VERSION, FM_RSRC_VERSION);
 	if (sysevent_get_attr_list(sep, &attr) == 0) {
 		(void) nvlist_merge(nvl, attr, 0);
 		nvlist_free(attr);
 	}
+
+	/*
+	 * Add class and version after the nvlist_merge() just in case
+	 * the sysevent has an attribute called class or version.
+	 */
+	(void) nvlist_add_string(nvl, FM_CLASS, fullclass);
+	(void) nvlist_add_uint8(nvl, FM_VERSION, FM_RSRC_VERSION);
 
 	/*
 	 * Dispatch the event.  Ideally, we'd like to use the same transport
