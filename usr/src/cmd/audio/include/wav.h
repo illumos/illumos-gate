3197c70857a2786b3ed9ff68d615627b373d6799
Jimmy Vetayases <Jimmy.Vetayases@Sun.COM>
2009-08-27 21:03:25 -0700

    6874895 audiorecord produces incorrect wave files

--- usr/src/cmd/audio/include/wav.h
+++ usr/src/cmd/audio/include/wav.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -30,8 +29,6 @@
 
 #ifndef _WAV_H
 #define	_WAV_H
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include <sys/types.h>
 
@@ -115,8 +112,8 @@ typedef struct wav_filehdr wav_filehdr_t;
 #define	AUDIO_WAV_FMT_BITS_PER_SAMPLE_16_BITS		16
 
 /* defines for wav_filehdr.wav_data_ID */
-#define	AUDIO_WAV_DATA_ID_LC		((uint32_t)0x41544144)	/* data */
-#define	AUDIO_WAV_DATA_ID_UC		((uint32_t)0x61746164)	/* DATA */
+#define	AUDIO_WAV_DATA_ID_UC		((uint32_t)0x41544144)	/* DATA */
+#define	AUDIO_WAV_DATA_ID_LC		((uint32_t)0x61746164)	/* data */
 
 
 /* byte swapping macros */
