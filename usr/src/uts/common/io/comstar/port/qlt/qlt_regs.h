7f728218b4eee71d7d07ccc3f3963fafabacacda
Daniel Beauregard <Daniel.Beauregard@Sun.COM>
2009-08-31 16:39:07 -0700

    6804502 qlt hangs 6861099 Add FCoE support to the qlt driver

--- usr/src/uts/common/io/comstar/port/qlt/qlt_regs.h
+++ usr/src/uts/common/io/comstar/port/qlt/qlt_regs.h
@@ -18,10 +18,17 @@
  *
  * CDDL HEADER END
  */
+
+/*
+ * Copyright 2009 QLogic Corporation.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
 /*
  * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
+
 #ifndef	_QLT_REGS_H
 #define	_QLT_REGS_H
 
@@ -66,6 +73,9 @@ extern "C" {
 #define	DMA_ACTIVE_STATUS	BIT_17
 #define	DMA_SHUTDOWN_CTRL	BIT_16
 #define	FUNCTION_NUMBER		BIT_15
+/*
+ * #define	81XX_FUNCTION_NUMBER	BIT_15 | BIT_14 | BIT_13 | BIT_12
+ */
 #define	PCI_X_BUS_MODE		(BIT_8 | BIT_9 | BIT_10 | BIT_11)
 #define	PCI_X_XFER_CTRL		(BIT_4 | BIT_5)
 #define	PCI_64_BIT_SLOT		BIT_2
@@ -100,6 +110,7 @@ extern "C" {
 #define	FLASH_DATA_FLAG		BIT_31
 #define	FLASH_CONF_ADDR		0x7FFD0000
 #define	FLASH_DATA_ADDR		0x7FF00000
+#define	FLASH_DATA_ADDR_81XX	0x7F8D0000
 #define	NVRAM_CONF_ADDR		0x7FFF0000
 #define	NVRAM_DATA_ADDR		0x7FFE0000
 
@@ -108,6 +119,9 @@ extern "C" {
 
 #define	QLT25_NVRAM_FUNC0_ADDR	(FLASH_DATA_ADDR + 0x48080)
 #define	QLT25_NVRAM_FUNC1_ADDR	(FLASH_DATA_ADDR + 0x48180)
+
+#define	QLT81_NVRAM_FUNC0_ADDR	(FLASH_DATA_ADDR_81XX + 0x80)
+#define	QLT81_NVRAM_FUNC1_ADDR	(FLASH_DATA_ADDR_81XX + 0x180)
 
 typedef struct qlt_nvram {
 	/* NVRAM header. */
@@ -371,6 +385,222 @@ typedef struct qlt_nvram {
 
 	uint8_t checksum[4];
 } qlt_nvram_t;
+
+/* ISP81xx Extended Initialisation Control Block */
+typedef struct qlt_ext_icb_81xx {
+
+	uint8_t version[2];
+	uint8_t fcf_vlan_match;
+	uint8_t reserved_6[3];
+	uint8_t fcf_vlan_id[2];
+	uint8_t fcf_fabric_name[8];
+	uint8_t reserved_7[14];
+	uint8_t spma_proposed_mac_address[6];
+	uint8_t reserved_8[28];
+
+} qlt_ext_icb_81xx_t;
+
+typedef struct qlt_nvram_81xx {
+	/* NVRAM header. */
+	uint8_t id[4];
+	uint8_t nvram_version[2];
+	uint8_t reserved_0[2];
+
+	/* Firmware Initialization Control Block. */
+	uint8_t version[2];
+	uint8_t reserved_1[2];
+	uint8_t max_frame_length[2];
+	uint8_t execution_throttle[2];
+	uint8_t exchange_count[2];
+	uint8_t reserved_2[2];
+	uint8_t port_name[8];
+	uint8_t node_name[8];
+	uint8_t login_retry_count[2];
+	uint8_t reserved_3[2];
+	uint8_t interrupt_delay_timer[2];
+	uint8_t login_timeout[2];
+
+	/*
+	 * BIT 0  = Hard Assigned Loop ID
+	 * BIT 1  = Enable Fairness
+	 * BIT 2  = Enable Full-Duplex
+	 * BIT 3  = Reserved
+	 * BIT 4  = Target Mode Enable
+	 * BIT 5  = Initiator Mode Disable
+	 * BIT 6  = Reserved
+	 * BIT 7  = Reserved
+	 *
+	 * BIT 8  = Reserved
+	 * BIT 9  = Reserved
+	 * BIT 10 = Reserved
+	 * BIT 11 = Reserved
+	 * BIT 12 = Reserved
+	 * BIT 13 = Reserved
+	 * BIT 14 = Node Name Option
+	 * BIT 15-31 = Reserved
+	 */
+	uint8_t firmware_options_1[4];
+
+	/*
+	 * BIT 0  = Operation Mode bit 0
+	 * BIT 1  = Operation Mode bit 1
+	 * BIT 2  = Operation Mode bit 2
+	 * BIT 3  = Operation Mode bit 3
+	 * BIT 4  = Reserved
+	 * BIT 5  = Reserved
+	 * BIT 6  = Reserved
+	 * BIT 7  = Reserved
+	 *
+	 * BIT 8  = Enable Class 2
+	 * BIT 9  = Enable ACK0
+	 * BIT 10 = Reserved
+	 * BIT 11 = Enable FC-SP Security
+	 * BIT 12 = FC Tape Enable
+	 * BIT 13 = Reserved
+	 * BIT 14 = Target PRLI Control
+	 * BIT 15-31 = Reserved
+	 */
+	uint8_t firmware_options_2[4];
+
+	/*
+	 * BIT 0  = Reserved
+	 * BIT 1  = Soft ID only
+	 * BIT 2  = Reserved
+	 * BIT 3  = Reserved
+	 * BIT 4  = FCP RSP Payload bit 0
+	 * BIT 5  = FCP RSP Payload bit 1
+	 * BIT 6  = Enable Rec Out-of-Order data frame handling
+	 * BIT 7  = Reserved
+	 *
+	 * BIT 8  = Reserved
+	 * BIT 9  = Enable Out-of-Order FCP_XFER_RDY relative
+	 *	    offset handling
+	 * BIT 10 = Reserved
+	 * BIT 11 = Reserved
+	 * BIT 12 = Reserved
+	 * BIT 13 = Reserved
+	 * BIT 14 = Reserved
+	 * BIT 15 = Reserved
+	 * BIT 16 = Reserved
+	 * BIT 17 = Enable Multiple FCFs
+	 * BIT 18-20 = MAC Addressing Mode
+	 * BIT 21-25 = Ethernet Data Rate
+	 * BIT 26 = Enable Ethernet Header Receive ATIO_Q
+	 * BIT 27 = Enable Ethernet Header Receive RSP_Q
+	 * BIT 28-29 = SPMA Selection
+	 * BIT 30-31 = Reserved
+	 */
+	uint8_t firmware_options_3[4];
+
+	/* Offset 56 (38h). */
+	uint8_t reserved_4[8];
+
+	/* Offset 64 (40h). */
+	uint8_t enode_mac[6];
+
+	/* Offset 70 (46h). */
+	uint8_t reserved_5[26];
+
+	/* Offset 96 (60h). */
+	uint8_t oem_specific;
+	uint8_t reserved_6[15];
+
+	/* Offset 112 (70h). */
+	uint8_t reserved_7[16];
+
+	/* Offset 128 (80h). */
+	qlt_ext_icb_81xx_t   ext_blk;
+
+	/* Offset 192. */
+	uint8_t reserved_8[32];
+
+	/* Offset 224. */
+	uint8_t reserved_9[32];
+
+	uint8_t host_p[4];
+
+	uint8_t alternate_port_name[8];
+	uint8_t alternate_name_name[8];
+
+	uint8_t boot_port_name[8];
+	uint8_t boot_lun_number[2];
+	uint8_t reserved_10[2];
+
+	uint8_t alt1_boot_port_name[8];
+	uint8_t alt1_boot_lun_number[2];
+	uint8_t reserved_11[2];
+
+	uint8_t alt2_boot_port_name[8];
+	uint8_t alt2_boot_lun_number[2];
+	uint8_t reserved_12[2];
+
+	uint8_t alt3_boot_port_name[8];
+	uint8_t alt3_boot_lun_number[2];
+	uint8_t reserved_13[2];
+
+	/*
+	 * BIT 0 = Selective Login
+	 * BIT 1 = Alt-Boot Enable
+	 * BIT 2 = Reserved
+	 * BIT 3 = Enable Boot Order List
+	 * BIT 4 = Reserved
+	 * BIT 5 = Enable Selective LUN
+	 * BIT 6 = Reserved
+	 * BIT 7-31 =
+	 */
+	uint8_t efi_parameters[4];
+
+	uint8_t reset_delay;
+	uint8_t reserved_14;
+	uint8_t reserved_15[2];
+
+	uint8_t boot_id_number[2];
+	uint8_t reserved_16[2];
+
+	uint8_t max_luns_per_target[2];
+	uint8_t reserved_17[2];
+
+	uint8_t port_down_retry_count[2];
+	uint8_t link_down_timeout[2];
+
+	/*
+	 * FCode parameters word (offset 344)
+	 *
+	 * BIT 0 = Enable BIOS pathname
+	 * BIT 1 = fcode qlc
+	 * BIT 2 = fcode host
+	 * BIT 3-7 =
+	 */
+	uint8_t	fcode_parameter[2];
+	uint8_t reserved_18[6];
+
+	/* Offset 352. */
+	uint8_t reserved_19[4];
+	uint8_t reserved_20[10];
+	uint8_t reserved_21[2];
+	uint8_t reserved_22[16];
+
+	/* Offset 384. */
+	uint8_t	reserved_23[16];
+	uint8_t reserved_24[16];
+
+	/* Offset 416. */
+	uint8_t reserved_25[64];
+
+	/* Offset 480. */
+	uint8_t model_name[16];
+
+	/* Offset 496. */
+	uint8_t feature_mask_l[2];
+	uint8_t feature_mask_h[2];
+	uint8_t reserved_26[4];
+
+	uint8_t subsystem_vendor_id[2];
+	uint8_t subsystem_device_id[2];
+
+	uint8_t checksum[4];
+
+} qlt_nvram_81xx_t;
 
 #ifdef	__cplusplus
 }
