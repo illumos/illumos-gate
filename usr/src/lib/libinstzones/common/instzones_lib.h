22fb2eeb50dd2ffd3aefea05536263203fae0600
Moriah Waterland <Moriah.Waterland@Sun.COM>
2009-09-04 10:51:44 -0600

    6872091 Need to deliver instzones_api.h in SUNWinstallint
    --HG--
    rename : usr/src/lib/libinstzones/common/instzones_api.h => usr/src/lib/libinstzones/hdrs/instzones_api.h

--- usr/src/lib/libinstzones/common/instzones_lib.h
+++ usr/src/lib/libinstzones/common/instzones_lib.h
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -46,10 +46,9 @@
 #include <zone.h>
 #include <libzonecfg.h>
 #include <libcontract.h>
+#include <instzones_api.h>
 
-/* Local includes */
 
-#include "instzones_api.h"
 
 /*
  * C++ prefix
