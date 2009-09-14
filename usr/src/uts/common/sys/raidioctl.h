d193de62b9e1115fd66e2032bbcdb2bd8e1ce51c
Ling Albert Ke <Ling.Ke@Sun.COM>
2009-09-14 18:53:07 +0800

    PSARC/2009/439 Remove obsolete ioctl interfaces in mpt driver 6833319 EOL obsolete ioctl interface in mpt driver

--- usr/src/uts/common/sys/raidioctl.h
+++ usr/src/uts/common/sys/raidioctl.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,14 +19,12 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
 #ifndef	_SYS_RAIDIOCTL_H
 #define	_SYS_RAIDIOCTL_H
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #ifdef	__cplusplus
 extern "C" {
@@ -38,10 +35,7 @@ extern "C" {
  */
 #define	RAID_IOC (('R' << 16) | ('D' << 8))
 #define	RAID_GETCONFIG		(RAID_IOC|1)	/* Get raid information */
-#define	RAID_CREATE		(RAID_IOC|2)	/* Create raid mirror */
-#define	RAID_DELETE		(RAID_IOC|3)	/* Delete raid mirror */
 #define	RAID_UPDATEFW		(RAID_IOC|4)	/* Update IOC firmware */
-#define	RAID_GETDEVID		(RAID_IOC|5)	/* Retrieve devid of chip */
 #define	RAID_NUMVOLUMES		(RAID_IOC|6)	/* Retrieve max RAID volumes */
 
 /*
