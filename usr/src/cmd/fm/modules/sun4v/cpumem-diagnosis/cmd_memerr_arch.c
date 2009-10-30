41d4fbbe41ff68b2451773fa974828dcb122c1e3
Karl Davis <Karl.Davis@Sun.COM>
2009-10-30 11:47:18 -0700

    6887865 Need to reactivate ereport.cpu.ultraSPARC-T2.dsc events for T2 platforms

--- usr/src/cmd/fm/modules/sun4v/cpumem-diagnosis/cmd_memerr_arch.c
+++ usr/src/cmd/fm/modules/sun4v/cpumem-diagnosis/cmd_memerr_arch.c
@@ -292,9 +292,8 @@ cmd_evdisp_t
 cmd_ce(fmd_hdl_t *hdl, fmd_event_t *ep, nvlist_t *nvl, const char *class,
     cmd_errcl_t clcode)
 {
-	if ((strcmp(class, "ereport.cpu.ultraSPARC-T2plus.dsc") == 0) ||
-	    (strcmp(class, "ereport.cpu.ultraSPARC-T2.dsc") == 0))
-		return (CMD_EVD_UNUSED); /* drop T2/T2+ dsc's */
+	if (strcmp(class, "ereport.cpu.ultraSPARC-T2plus.dsc") == 0)
+		return (CMD_EVD_UNUSED); /* drop VF dsc's */
 	else
 		return (xe_common(hdl, ep, nvl, class, clcode, cmd_ce_common));
 }
