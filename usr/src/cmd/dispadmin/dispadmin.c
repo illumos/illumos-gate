ae0476dbaa01cfe1e3f6783f2a62d23e26d53c7a
Jayakara Kini <Jayakara.Kini@Sun.COM>
2009-11-05 10:44:03 +0530

    6557253 dispadmin.c:usage[] contains undocumented -u option Portions contributed by Rishi Nair and Avinash Joshi

--- usr/src/cmd/dispadmin/dispadmin.c
+++ usr/src/cmd/dispadmin/dispadmin.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -23,13 +22,10 @@
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved  	*/
 
-
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include	<stdio.h>
 #include	<stdlib.h>
@@ -72,8 +68,7 @@ extern char *basename();
 static char usage[] =
 "usage:	dispadmin -l\n\
 	dispadmin -c class [class-specific options]\n\
-	dispadmin -d [class]\n\
-	dispadmin -u\n";
+	dispadmin -d [class]\n";
 
 static char basenm[BASENMSZ];
 static char cmdpath[PATH_MAX];
