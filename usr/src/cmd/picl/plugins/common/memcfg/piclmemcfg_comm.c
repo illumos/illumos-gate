22f288db255a577a61382cbe06ebbe70b453a49a
Michael Bergknoff <Michael.Bergknoff@Sun.COM>
2009-11-30 08:05:06 -0800

    6789972 Error message appears on G12F Quadcore CPU with Open Solaris

--- usr/src/cmd/picl/plugins/common/memcfg/piclmemcfg_comm.c
+++ usr/src/cmd/picl/plugins/common/memcfg/piclmemcfg_comm.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,11 +19,9 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 /*
  * This plugin creates memory configuration nodes and properties in the
@@ -707,6 +704,7 @@ init_mc(void)
 	struct dirent		*retp;
 	char			path[PATH_MAX];
 	int 			found = 0;
+	int			valid_entry = 0;
 
 	/* open the directory */
 	if ((dirp = opendir(MC_DIR)) == NULL) {
@@ -734,11 +732,14 @@ init_mc(void)
 			found = 1;
 			break;
 		}
+		if (errno != ENOENT)
+			valid_entry = 1;
 	}
 	(void) closedir(dirp);
 
 	if (!found) {
-		syslog(LOG_ERR, EM_INIT_MC_FAILED);
+		if (valid_entry)
+			syslog(LOG_ERR, EM_INIT_MC_FAILED);
 		return (-1);
 	}
 
