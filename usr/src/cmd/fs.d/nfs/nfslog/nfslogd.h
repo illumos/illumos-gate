ea259b731ee40808603fbf271e8c051e8a2d483a
Marcel Telka <Marcel.Telka@Sun.COM>
2010-03-23 20:23:12 +0100

    6913655 nfslogd(1M) contains file handling error

--- usr/src/cmd/fs.d/nfs/nfslog/nfslogd.h
+++ usr/src/cmd/fs.d/nfs/nfslog/nfslogd.h
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,14 +19,12 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
 #ifndef _NFSLOGD_H
 #define	_NFSLOGD_H
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #ifdef	__cplusplus
 extern "C" {
@@ -39,7 +36,7 @@ extern "C" {
 #include "../lib/nfslog_config.h"
 #include "buffer_list.h"
 
-#define	NFSLOGD_PIDFILE		"/tmp/.nfslogd.pid"
+#define	NFSLOGD_PIDFILE		"/var/run/nfslogd.pid"
 #define	NFSLOG_OPTIONS_FILE	"/etc/default/nfslogd"
 
 #define	MIN_PROCESSING_SIZE	512*1024	/* Minimum size buffer */
