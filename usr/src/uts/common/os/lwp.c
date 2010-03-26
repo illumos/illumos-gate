1bc02a70e2e54d0353dea2f6d1fa4e12c2c46b3b
Jakub Jermar <Jakub.Jermar@Sun.COM>
2010-03-26 10:38:43 +0100

    6924278 lwp_exit() race causes proc with bad p_tlist, panicking ps(1)

--- usr/src/uts/common/os/lwp.c
+++ usr/src/uts/common/os/lwp.c
@@ -887,6 +887,8 @@ lwp_exit(void)
 	if (PROC_IS_BRANDED(p))
 		BROP(p)->b_lwpexit(lwp);
 
+	lwp_pcb_exit();
+
 	mutex_enter(&p->p_lock);
 	lwp_cleanup();
 
@@ -897,6 +899,12 @@ lwp_exit(void)
 	 */
 	if (p->p_flag & SCOREDUMP)
 		stop(PR_SUSPENDED, SUSPEND_NORMAL);
+
+	/*
+	 * Block the process against /proc now that we have really acquired
+	 * p->p_lock (to decrement p_lwpcnt and manipulate p_tlist at least).
+	 */
+	prbarrier(p);
 
 	/*
 	 * Call proc_exit() if this is the last non-daemon lwp in the process.
@@ -916,19 +924,8 @@ lwp_exit(void)
 		 */
 		mutex_enter(&p->p_lock);
 		ASSERT(curproc->p_flag & SEXITLWPS);
+		prbarrier(p);
 	}
-
-	mutex_exit(&p->p_lock);
-
-	lwp_pcb_exit();
-
-	mutex_enter(&p->p_lock);
-
-	/*
-	 * Block the process against /proc now that we have really acquired
-	 * p->p_lock (to decrement p_lwpcnt and manipulate p_tlist at least).
-	 */
-	prbarrier(p);
 
 	DTRACE_PROC(lwp__exit);
 
