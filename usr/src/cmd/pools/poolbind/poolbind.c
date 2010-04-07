af6a63accf40878250435baa6f5bde2ca6b41428
Gangadhar Mylapuram <Gangadhar.M@Sun.COM>
2010-04-06 23:31:14 -0700

    6417547 poolbind -e doesn't work with command contains slash

--- usr/src/cmd/pools/poolbind/poolbind.c
+++ usr/src/cmd/pools/poolbind/poolbind.c
@@ -2,9 +2,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or http://www.opensolaris.org/os/licensing.
@@ -20,11 +19,8 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
+ * Copyright (c) 2001, 2010, Oracle and/or its affiliates. All rights reserved.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 /*
  * poolbind - bind processes, tasks, and projects to pools, and query process
@@ -198,7 +194,7 @@ exec_cmd(char *pool_name, char *argv[])
 		return;
 	}
 
-	if (execvp(basename(argv[0]), argv) == -1)
+	if (execvp(argv[0], argv) == -1)
 		die(gettext("exec of %s failed"), argv[0]);
 	/*NOTREACHED*/
 }
