955ef35916e692ccbbeed5e4b2e63690ed03c62c
Lin Ling <Lin.Ling@Sun.COM>
2010-06-10 11:29:20 -0700

    6951283 hotspare_import_001_pos FAILs unless doing a sync before detach

--- usr/src/uts/common/fs/zfs/spa.c
+++ usr/src/uts/common/fs/zfs/spa.c
@@ -3228,6 +3228,8 @@ spa_import(const char *pool, nvlist_t *config, nvlist_t *props)
 		return (error);
 	}
 
+	spa_async_resume(spa);
+
 	/*
 	 * Override any spares and level 2 cache devices as specified by
 	 * the user, as these may have correct device names/devids, etc.
@@ -3277,8 +3279,6 @@ spa_import(const char *pool, nvlist_t *config, nvlist_t *props)
 		 */
 		spa_config_update(spa, SPA_CONFIG_UPDATE_POOL);
 	}
-
-	spa_async_resume(spa);
 
 	/*
 	 * It's possible that the pool was expanded while it was exported.
