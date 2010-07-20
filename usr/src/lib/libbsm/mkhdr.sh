a129221122aff8dab675d034d68598fec6909ab0
Marek Pospisil <Marek.Pospisil@Sun.COM>
2010-07-20 05:04:01 -0700

    6404196 <bsm/audit_uevents.h> doesn't appear to have a proper copyright or CDDL license.

--- usr/src/lib/libbsm/mkhdr.sh
+++ usr/src/lib/libbsm/mkhdr.sh
@@ -19,31 +19,44 @@
 #
 # CDDL HEADER END
 #
-#
-# Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
-# Use is subject to license terms.
-#
-# ident	"%Z%%M%	%I%	%E% SMI"
+# Copyright (c) 1992, 2010, Oracle and/or its affiliates. All rights reserved.
 #
 
 # Automagically generate the audit_uevents.h header file.
 #
 DATABASE=audit_event.txt
 HEADER_FILE=audit_uevents.h
+CR_YEAR=`/usr/bin/date '+%Y'`
 
 cat <<EOF > $HEADER_FILE
 /*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright (c) 1992, $CR_YEAR, Oracle and/or its affiliates. All rights reserved.
  */
 
 #ifndef	_BSM_AUDIT_UEVENTS_H
 #define	_BSM_AUDIT_UEVENTS_H
 
 EOF
-
-grep '^# ident' $DATABASE | sed -e 's/^#/#pragma/' \
-	-e "s/$DATABASE/$HEADER_FILE/" >> $HEADER_FILE
 
 cat <<EOF >> $HEADER_FILE
 
