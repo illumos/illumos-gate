d1524a28956116d3ea6af731008fc75f51926ccd
Rich Burridge <rich.burridge@oracle.com>
2010-08-16 13:30:30 -0700

    4858166 makewhatis should remove duplicate windex entries 6565880 catman produces broken synopsis lines in windex

--- usr/src/cmd/man/src/makewhatis.sh
+++ usr/src/cmd/man/src/makewhatis.sh
@@ -3,9 +3,8 @@
 # CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
 #
 # You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
 # or http://www.opensolaris.org/os/licensing.
@@ -21,8 +20,7 @@
 # CDDL HEADER END
 #
 #
-# Copyright (c) 1998 by Sun Microsystems, Inc.
-# All rights reserved
+# Copyright (c) 1990, 2010, Oracle and/or its affiliates. All rights reserved.
 #
 #	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T
 #	  All Rights Reserved
@@ -35,8 +33,6 @@
 # University Acknowledgment- Portions of this document are derived from
 # software developed by the University of California, Berkeley, and its
 # contributors
-
-#ident	"%Z%%M%	%I%	%E% SMI"	/* SVr4.0 1.2	*/
 
 PATH=/usr/xpg4/bin:$PATH
 
@@ -68,6 +64,7 @@ sed  <$tmpdir/whatisx \
 	-e 's/ VAX-11//' \
 	-e 's/\\f[PRIB01234]//g' \
 	-e 's/\\s[-+0-9]*//g' \
+	-e 's/\\&//g' \
 	-e '/ - /!d' \
 	-e 's/.TH [^ ]* \([^ 	]*\).*	\(.*\) -/\2 (\1)	 -/' \
 	-e 's/	 /	/g' | \
@@ -81,7 +78,7 @@ awk '{	title = substr($0, 1, index($0, "- ") - 1)
 	}
 }' >$tmpdir/whatis
 /usr/bin/expand -16,32,36,40,44,48,52,56,60,64,68,72,76,80,84,88,92,96,100 \
-	$tmpdir/whatis | LC_CTYPE=C LC_COLLATE=C sort | \
+	$tmpdir/whatis | LC_CTYPE=C LC_COLLATE=C sort -u | \
 	/usr/bin/unexpand -a > windex
 chmod 644 windex >/dev/null 2>&1
 rm -rf $tmpdir
