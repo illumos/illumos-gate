cb2df8815fd80018d65e08f25ae5614934a3d8b3
Richard Lowe <richlowe@richlowe.net>
2011-05-16 00:20:30 +0100

    va_impl: Deal with GCC 4.4 removal __builtin_stdarg_start alias

--- usr/src/uts/common/sys/va_impl.h
+++ usr/src/uts/common/sys/va_impl.h
@@ -31,8 +31,6 @@
 #ifndef	_SYS_VA_IMPL_H
 #define	_SYS_VA_IMPL_H
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 /*
  * An application should not include this header directly.  Instead it
  * should be included only through the inclusion of other Sun headers,
@@ -149,8 +147,12 @@ char *__builtin_va_arg_incr();
 
 #elif defined(__GNUC__)	&& ((__GNUC__ == 2 && __GNUC_MINOR >= 96) || \
 	(__GNUC__ >= 3))		/* ------------------------ protocol */
-
+#if __GNUC__ < 4 || __GNUC_MINOR__ < 4
 #define	__va_start(list, name)	__builtin_stdarg_start(list, name)
+#else
+#define	__va_start(list, name)	__builtin_va_start(list, name)
+#endif
+
 #define	__va_arg(list, type)	__builtin_va_arg(list, type)
 #define	__va_end(list)		__builtin_va_end(list)
 #define	__va_copy(to, from)	__builtin_va_copy(to, from)
