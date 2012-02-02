76288daf983e135aabe22475137f7afe6f249764
John Sonnenschein <johns@joyent.com>
2012-02-02 23:57:25 +0000

    OS-912 wall(1) needs zones support Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com>

--- usr/src/cmd/wall/wall.c
+++ usr/src/cmd/wall/wall.c
@@ -29,7 +29,9 @@
  * Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
+/*
+ * Copyright 2012 Joyent, Inc. All rights reserved.
+ */
 
 #include <signal.h>
 #include <stdio.h>
@@ -51,6 +53,11 @@
 #include <syslog.h>
 #include <sys/wait.h>
 #include <limits.h>
+#include <libzonecfg.h>
+#include <zone.h>
+#include <sys/contract/process.h>
+#include <libcontract.h>
+#include <sys/ctfs.h>
 
 /*
  * utmpx defines wider fields for user and line.  For compatibility of output,
@@ -69,6 +76,7 @@
 static char	mesg[3000];
 static char	*infile;
 static int	gflag;
+static int	aflag;
 static struct	group *pgrp;
 static char	*grpname;
 static char	line[MAXNAMLEN+1] = "???";
@@ -79,26 +87,35 @@ static char	who[9]	= "???";
 static char	time_buf[50];
 #define	DATE_FMT	"%a %b %e %H:%M:%S"
 
-static void sendmes(struct utmpx *);
+static void sendmes(struct utmpx *, zoneid_t);
+static void sendmes_tozone(zoneid_t);
 static int chkgrp(char *);
 static char *copy_str_till(char *, char *, char, int);
+
+static int init_template(void);
+static int contract_abandon_id(ctid_t);
 
 int
 main(int argc, char *argv[])
 {
-	int	i = 0;
-	struct utmpx *p;
 	FILE	*f;
 	char	*ptr, *start;
 	struct	passwd *pwd;
 	char	*term_name;
 	int	c;
-	int	aflag = 0;
 	int	errflg = 0;
+	int zflg = 0;
+	int Zflg = 0;
+
+	char *zonename = NULL;
+	zoneid_t *zoneidlist = NULL;
+	uint_t nzids_saved, nzids = 0;
+
+	aflag = 0;
 
 	(void) setlocale(LC_ALL, "");
 
-	while ((c = getopt(argc, argv, "g:a")) != EOF)
+	while ((c = getopt(argc, argv, "g:az:Z")) != EOF)
 		switch (c) {
 		case 'a':
 			aflag++;
@@ -116,6 +133,18 @@ main(int argc, char *argv[])
 			}
 			gflag++;
 			break;
+		case 'z':
+			zflg++;
+			zonename = optarg;
+			if (getzoneidbyname(zonename) == -1) {
+				(void) fprintf(stderr, "Specified zone %s "
+				    "is invalid", zonename);
+				exit(1);
+			}
+			break;
+		case 'Z':
+			Zflg++;
+			break;
 		case '?':
 			errflg++;
 			break;
@@ -123,7 +152,7 @@ main(int argc, char *argv[])
 
 	if (errflg) {
 		(void) fprintf(stderr,
-		    "Usage: wall [-a] [-g group] [files...]\n");
+		    "Usage: wall [-a] [-g group] [-z zone] [-Z] [files...]\n");
 		return (1);
 	}
 
@@ -202,24 +231,41 @@ main(int argc, char *argv[])
 	}
 	(void) time(&tloc);
 	(void) strftime(time_buf, sizeof (time_buf),
-			    DATE_FMT, localtime(&tloc));
+	    DATE_FMT, localtime(&tloc));
 
-	setutxent();
-	while ((p = getutxent()) != NULL) {
-		if (p->ut_type != USER_PROCESS)
-			continue;
-		/*
-		 * if (-a option OR NOT pty window login), send the message
-		 */
-		if (aflag || !nonuser(*p))
-			sendmes(p);
+	if (zflg != 0) {
+		if ((zoneidlist =
+		    (zoneid_t *)malloc(sizeof (zoneid_t))) == NULL ||
+		    (*zoneidlist = getzoneidbyname(zonename)) == -1)
+			exit(errno);
+		nzids = 1;
 	}
-	endutxent();
-
-	(void) alarm(60);
-	do {
-		i = (int)wait((int *)0);
-	} while (i != -1 || errno != ECHILD);
+	if (Zflg != 0) {
+		if (zflg != 0 && zoneidlist != NULL) {
+			free(zoneidlist);
+		}
+		if (zone_list(NULL, &nzids) != 0)
+			exit(errno);
+again:
+		nzids *= 2;
+		if ((zoneidlist = malloc(nzids * sizeof (zoneid_t))) == NULL)
+			exit(errno);
+		nzids_saved = nzids;
+		if (zone_list(zoneidlist, &nzids) != 0) {
+			free(zoneidlist);
+			return (errno);
+		}
+		if (nzids > nzids_saved) {
+			free(zoneidlist);
+			goto again;
+		}
+	}
+	if (zflg || Zflg) {
+		for (; nzids > 0; --nzids)
+			sendmes_tozone(zoneidlist[nzids-1]);
+		free(zoneidlist);
+	} else
+		sendmes_tozone(getzoneid());
 
 	return (0);
 }
@@ -249,6 +295,41 @@ copy_str_till(char *dst, char *src, char delim, int len)
 	return (src);
 }
 
+static void
+sendmes_tozone(zoneid_t zid) {
+	int i = 0;
+	char zonename[ZONENAME_MAX], root[MAXPATHLEN];
+	struct utmpx *p;
+
+	if (zid != getzoneid()) {
+		root[0] = '\0';
+		getzonenamebyid(zid, zonename, ZONENAME_MAX);
+		(void) zone_get_rootpath(zonename, root, sizeof (root));
+		(void) strlcat(root, UTMPX_FILE, sizeof (root));
+		if (!utmpxname(root)) {
+			(void) fprintf(stderr, "Cannot open %s\n", root);
+			return;
+		}
+	}
+	setutxent();
+	while ((p = getutxent()) != NULL) {
+		if (p->ut_type != USER_PROCESS)
+			continue;
+		/*
+		 * if (-a option OR NOT pty window login), send the message
+		 */
+		if (aflag || !nonuser(*p))
+			sendmes(p, zid);
+	}
+	endutxent();
+
+	(void) alarm(60);
+	do {
+		i = (int)wait((int *)0);
+	} while (i != -1 || errno != ECHILD);
+
+}
+
 /*
  * Note to future maintainers: with the change of wall to use the
  * getutxent() API, the forked children (created by this function)
@@ -257,7 +338,7 @@ copy_str_till(char *dst, char *src, char delim, int len)
  * processing).
  */
 static void
-sendmes(struct utmpx *p)
+sendmes(struct utmpx *p, zoneid_t zid)
 {
 	int i;
 	char *s;
@@ -265,11 +346,19 @@ sendmes(struct utmpx *p)
 	char *bp;
 	int ibp;
 	FILE *f;
-	int fd;
+	int fd, tmpl_fd;
+	boolean_t zoneenter = B_FALSE;
 
-	if (gflag)
-		if (!chkgrp(p->ut_user))
+	if (zid != getzoneid()) {
+		zoneenter = B_TRUE;
+		tmpl_fd = init_template();
+		if (tmpl_fd == -1) {
+			(void) fprintf(stderr, "Could not initialize "
+			    "process contract");
 			return;
+		}
+	}
+
 	while ((i = (int)fork()) == -1) {
 		(void) alarm(60);
 		(void) wait((int *)0);
@@ -278,6 +367,19 @@ sendmes(struct utmpx *p)
 
 	if (i)
 		return;
+
+	if (zoneenter && zone_enter(zid) == -1) {
+		char zonename[ZONENAME_MAX];
+		(void) getzonenamebyid(zid, zonename, ZONENAME_MAX);
+		(void) fprintf(stderr, "Could not enter zone "
+		    "%s\n", zonename);
+	}
+	if (zoneenter)
+		(void) ct_tmpl_clear(tmpl_fd);
+
+	if (gflag)
+		if (!chkgrp(p->ut_user))
+			_exit(0);
 
 	(void) signal(SIGHUP, SIG_IGN);
 	(void) alarm(60);
@@ -377,4 +479,25 @@ chkgrp(char *name)
 	}
 
 	return (0);
+}
+
+static int
+init_template(void) {
+	int fd = 0;
+	int err = 0;
+
+	fd = open64(CTFS_ROOT "/process/template", O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	err |= ct_tmpl_set_critical(fd, 0);
+	err |= ct_tmpl_set_informative(fd, 0);
+	err |= ct_pr_tmpl_set_fatal(fd, CT_PR_EV_HWERR);
+	err |= ct_pr_tmpl_set_param(fd, CT_PR_PGRPONLY | CT_PR_REGENT);
+	if (err || ct_tmpl_activate(fd)) {
+		(void) close(fd);
+		return (-1);
+	}
+
+	return (fd);
 }
