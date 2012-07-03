096b23783d19eb083bccc3cbd5225b5345ca83fb
Jerry Jelinek <jerry.jelinek@joyent.com>
2012-07-03 12:49:09 +0000

    3972 ps - missing column separator Reviewed by: Robert Mustacchi <rm@joyent.com> Approved by: Richard Lowe <richlowe@richlowe.net>

--- usr/src/cmd/ps/ucbps.c
+++ usr/src/cmd/ps/ucbps.c
@@ -21,6 +21,7 @@
 /*
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
+ * Copyright 2012, Joyent, Inc.  All rights reserved.
  */
 
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
@@ -933,13 +934,13 @@ prcom(int found, psinfo_t *psinfo, char *psargs)
 		if (psinfo->pr_flag & SSYS)			/* SZ */
 			(void) printf("    0");
 		else if (psinfo->pr_size)
-			(void) printf("%5lu", (ulong_t)psinfo->pr_size);
+			(void) printf(" %4lu", (ulong_t)psinfo->pr_size);
 		else
 			(void) printf("    ?");
 		if (psinfo->pr_flag & SSYS)			/* RSS */
 			(void) printf("    0");
 		else if (psinfo->pr_rssize)
-			(void) printf("%5lu", (ulong_t)psinfo->pr_rssize);
+			(void) printf(" %4lu", (ulong_t)psinfo->pr_rssize);
 		else
 			(void) printf("    ?");
 	}
