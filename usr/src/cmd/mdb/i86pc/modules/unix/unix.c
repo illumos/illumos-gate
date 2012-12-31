151488748198bc1decc86d275b3eee41ac9b7042
Robert Mustacchi <rm@joyent.com>
2012-12-31 04:52:06 +0000

    OS-1782 want ::x86_featureset

--- usr/src/cmd/mdb/i86pc/modules/unix/unix.c
+++ usr/src/cmd/mdb/i86pc/modules/unix/unix.c
@@ -22,6 +22,10 @@
  * Copyright (c) 1999, 2010, Oracle and/or its affiliates. All rights reserved.
  */
 
+/*
+ * Copyright (c) 2012 Joyent, Inc.  All rights reserved.
+ */
+
 #include <mdb/mdb_modapi.h>
 #include <mdb/mdb_ctf.h>
 #include <sys/cpuvar.h>
@@ -36,6 +40,8 @@
 #include <sys/mutex_impl.h>
 #include "i86mmu.h"
 #include <sys/apix.h>
+#include <sys/x86_archext.h>
+#include <sys/bitmap.h>
 
 #define	TT_HDLR_WIDTH	17
 
@@ -745,6 +751,137 @@ ptable_help(void)
 	    "-m Interpret the PFN as an MFN (machine frame number)\n");
 }
 
+/*
+ * NSEC_SHIFT is replicated here (it is not defined in a header file),
+ * but for amusement, the reader is directed to the comment that explains
+ * the rationale for this particular value on x86.  Spoiler:  the value is
+ * selected to accommodate 60 MHz Pentiums!  (And a confession:  if the voice
+ * in that comment sounds too familiar, it's because your author also wrote
+ * that code -- some fifteen years prior to this writing in 2011...)
+ */
+#define	NSEC_SHIFT 5
+
+/*ARGSUSED*/
+static int
+scalehrtime_cmd(uintptr_t addr, uint_t flags, int argc, const mdb_arg_t *argv)
+{
+	uint32_t nsec_scale;
+	hrtime_t tsc = addr, hrt;
+	unsigned int *tscp = (unsigned int *)&tsc;
+	uintptr_t scalehrtimef;
+	uint64_t scale;
+	GElf_Sym sym;
+
+	if (!(flags & DCMD_ADDRSPEC)) {
+		if (argc != 1)
+			return (DCMD_USAGE);
+
+		switch (argv[0].a_type) {
+		case MDB_TYPE_STRING:
+			tsc = mdb_strtoull(argv[0].a_un.a_str);
+			break;
+		case MDB_TYPE_IMMEDIATE:
+			tsc = argv[0].a_un.a_val;
+			break;
+		default:
+			return (DCMD_USAGE);
+		}
+	}
+
+	if (mdb_readsym(&scalehrtimef,
+	    sizeof (scalehrtimef), "scalehrtimef") == -1) {
+		mdb_warn("couldn't read 'scalehrtimef'");
+		return (DCMD_ERR);
+	}
+
+	if (mdb_lookup_by_name("tsc_scalehrtime", &sym) == -1) {
+		mdb_warn("couldn't find 'tsc_scalehrtime'");
+		return (DCMD_ERR);
+	}
+
+	if (sym.st_value != scalehrtimef) {
+		mdb_warn("::scalehrtime requires that scalehrtimef "
+		    "be set to tsc_scalehrtime\n");
+		return (DCMD_ERR);
+	}
+
+	if (mdb_readsym(&nsec_scale, sizeof (nsec_scale), "nsec_scale") == -1) {
+		mdb_warn("couldn't read 'nsec_scale'");
+		return (DCMD_ERR);
+	}
+
+	scale = (uint64_t)nsec_scale;
+
+	hrt = ((uint64_t)tscp[1] * scale) << NSEC_SHIFT;
+	hrt += ((uint64_t)tscp[0] * scale) >> (32 - NSEC_SHIFT);
+
+	mdb_printf("0x%llx\n", hrt);
+
+	return (DCMD_OK);
+}
+
+/*
+ * The x86 feature set is implemented as a bitmap array. That bitmap array is
+ * stored across a number of uchars based on the BT_SIZEOFMAP(NUM_X86_FEATURES)
+ * macro. We have the names for each of these features in unix's text segment
+ * so we do not have to duplicate them and instead just look them up.
+ */
+/*ARGSUSED*/
+static int
+x86_featureset_cmd(uintptr_t addr, uint_t flags, int argc,
+    const mdb_arg_t *argv)
+{
+	uchar_t *fset;
+	GElf_Sym sym;
+	uintptr_t nptr;
+	char name[128];
+	int ii;
+
+	size_t sz = sizeof (uchar_t) * BT_SIZEOFMAP(NUM_X86_FEATURES);
+
+	if (argc != 0)
+		return (DCMD_USAGE);
+
+	if (mdb_lookup_by_name("x86_feature_names", &sym) == -1) {
+		mdb_warn("couldn't find x86_feature_names");
+		return (DCMD_ERR);
+	}
+
+	fset = mdb_zalloc(sz, UM_NOSLEEP);
+	if (fset == NULL) {
+		mdb_warn("failed to allocate memory for x86_featureset");
+		return (DCMD_ERR);
+	}
+
+	if (mdb_readvar(fset, "x86_featureset") != sz) {
+		mdb_warn("failed to read x86_featureset");
+		mdb_free(fset, sz);
+		return (DCMD_ERR);
+	}
+
+	for (ii = 0; ii < NUM_X86_FEATURES; ii++) {
+		if (!BT_TEST((ulong_t *)fset, ii))
+			continue;
+
+		if (mdb_vread(&nptr, sizeof (char *), sym.st_value +
+		    sizeof (void *) * ii) != sizeof (char *)) {
+			mdb_warn("failed to read feature array %d", ii);
+			mdb_free(fset, sz);
+			return (DCMD_ERR);
+		}
+
+		if (mdb_readstr(name, sizeof (name), nptr) == -1) {
+			mdb_warn("failed to read feature %d", ii);
+			mdb_free(fset, sz);
+			return (DCMD_ERR);
+		}
+		mdb_printf("%s\n", name);
+	}
+
+	mdb_free(fset, sz);
+	return (DCMD_OK);
+}
+
 static const mdb_dcmd_t dcmds[] = {
 	{ "gate_desc", ":", "dump a gate descriptor", gate_desc },
 	{ "idt", ":[-v]", "dump an IDT", idt },
@@ -765,6 +902,10 @@ static const mdb_dcmd_t dcmds[] = {
 	{ "mfntopfn", ":", "convert hypervisor machine page to physical page",
 	    mfntopfn_dcmd },
 	{ "memseg_list", ":", "show memseg list", memseg_list },
+	{ "scalehrtime", ":",
+	    "scale an unscaled high-res time", scalehrtime_cmd },
+	{ "x86_featureset", NULL, "dump the x86_featureset vector",
+		x86_featureset_cmd },
 	{ NULL }
 };
 
