861bfc8941f2c557f1bcb7a6d7f62f9da98ee4db
Theo Schlossnagle <jesus@omniti.com>
2013-06-23 18:40:00 +0000

    3843 kstat should handle NULL string values Reviewed by: Carlos Cardenas <carlos.cardenas@joyent.com> Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com> Reviewed by: Richard Lowe <richlowe@richlowe.net> Approved by: Dan McDonald <danmcd@nexenta.com>

--- usr/src/cmd/stat/kstat/kstat.h
+++ usr/src/cmd/stat/kstat/kstat.h
@@ -143,7 +143,7 @@ typedef union ks_value {
 {							\
 	ks_value_t v;					\
 	v.str.addr.ptr = safe_strdup(V);		\
-	v.str.len = strlen(V);				\
+	v.str.len = (V) ? strlen(V) : 0;		\
 	nvpair_insert(I, N, &v, KSTAT_DATA_STRING);	\
 }
 
