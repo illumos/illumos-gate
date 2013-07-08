f99db78fa089c50b0d8fcef75f99d7a7f2e27dda
Christopher Siden <chris.siden@delphix.com>
2013-07-07 18:15:31 -0800

    3861 tcp buffer size tunables for iscsi connections (fix sizeof calls)

--- usr/src/uts/common/io/idm/idm_so.c
+++ usr/src/uts/common/io/idm/idm_so.c
@@ -719,9 +719,9 @@ idm_set_postconnect_options(ksocket_t ks)
 
 	/* Set connect options */
 	(void) ksocket_setsockopt(ks, SOL_SOCKET, SO_RCVBUF,
-	    (char *)&idm_so_rcvbuf, sizeof (int), CRED());
+	    (char *)&idm_so_rcvbuf, sizeof (idm_so_rcvbuf), CRED());
 	(void) ksocket_setsockopt(ks, SOL_SOCKET, SO_SNDBUF,
-	    (char *)&idm_so_sndbuf, sizeof (int), CRED());
+	    (char *)&idm_so_sndbuf, sizeof (idm_so_sndbuf), CRED());
 	(void) ksocket_setsockopt(ks, IPPROTO_TCP, TCP_NODELAY,
 	    (char *)&on, sizeof (on), CRED());
 }
