6344b441ffaa746030673ed598c637141b24f080
Keith M Wesolowski <wesolows@foobazco.org>
2013-08-24 16:07:34 +0000

    2932 support crash dumps to raidz, etc. pools (fix mismerge)

--- usr/src/uts/common/fs/zfs/vdev_disk.c
+++ usr/src/uts/common/fs/zfs/vdev_disk.c
@@ -658,27 +658,6 @@ vdev_disk_physio(vdev_t *vd, caddr_t data,
 }
 
 int
-vdev_disk_physio(vdev_t *vd, caddr_t data,
-    size_t size, uint64_t offset, int flags, boolean_t isdump)
-{
-	vdev_disk_t *dvd = vd->vdev_tsd;
-
-	ASSERT(vd->vdev_ops == &vdev_disk_ops);
-
-	/*
-	 * If in the context of an active crash dump, use the ldi_dump(9F)
-	 * call instead of ldi_strategy(9F) as usual.
-	 */
-	if (isdump) {
-		ASSERT3P(dvd, !=, NULL);
-		return (ldi_dump(dvd->vd_lh, data, lbtodb(offset),
-		    lbtodb(size)));
-	}
-
-	return (vdev_disk_ldi_physio(dvd->vd_lh, data, size, offset, flags));
-}
-
-int
 vdev_disk_ldi_physio(ldi_handle_t vd_lh, caddr_t data,
     size_t size, uint64_t offset, int flags)
 {
