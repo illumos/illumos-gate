511588bb13d2462265d682dc1cb7ba5c7a27a771
Yuri Pankov <yuri.pankov@nexenta.com>
2013-08-29 02:06:39 +0400

    4048 cpu_acpi is too verbose about disabled SpeedStep/PowerNow! support Reviewed by: Albert Lee <trisk@nexenta.com> Reviewed by: Garrett D'Amore <garrett@damore.org> Approved by: Dan McDonald <danmcd@nexenta.com>

--- usr/src/uts/i86pc/os/cpupm/speedstep.c
+++ usr/src/uts/i86pc/os/cpupm/speedstep.c
@@ -185,6 +185,7 @@ speedstep_init(cpu_t *cp)
 	    (cpupm_mach_state_t *)cp->cpu_m.mcpu_pm_mach_state;
 	cpu_acpi_handle_t handle = mach_state->ms_acpi_handle;
 	cpu_acpi_pct_t *pct_stat;
+	static int logged = 0;
 
 	ESSDEBUG(("speedstep_init: processor %d\n", cp->cpu_id));
 
@@ -192,9 +193,12 @@ speedstep_init(cpu_t *cp)
 	 * Cache the P-state specific ACPI data.
 	 */
 	if (cpu_acpi_cache_pstate_data(handle) != 0) {
-		cmn_err(CE_NOTE, "!SpeedStep support is being "
-		    "disabled due to errors parsing ACPI P-state objects "
-		    "exported by BIOS.");
+		if (!logged) {
+			cmn_err(CE_NOTE, "!SpeedStep support is being "
+			    "disabled due to errors parsing ACPI P-state "
+			    "objects exported by BIOS.");
+			logged = 1;
+		}
 		speedstep_fini(cp);
 		return (ESS_RET_NO_PM);
 	}
