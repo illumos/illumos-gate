c596e8663644f226509d50dd96c7e4e15ad23f24
Marcel Telka <marcel.telka@nexenta.com>
2014-01-30 17:35:08 +0100

    4551 mountd: Busy do_logging_queue() eats memory Reviewed by: Michael Tsymbalyuk <michael.tsymbalyuk@nexenta.com> Reviewed by: Gordon Ross <gordon.ross@nexenta.com> Approved by: Richard Lowe <richlowe@richlowe.net>

--- usr/src/cmd/fs.d/nfs/mountd/mountd.c
+++ usr/src/cmd/fs.d/nfs/mountd/mountd.c
@@ -288,7 +288,6 @@ remove_head_of_queue(void)
 static void
 do_logging_queue(logging_data *lq)
 {
-	logging_data	*lq_clean = NULL;
 	int		cleared = 0;
 	char		*host;
 
@@ -314,20 +313,12 @@ do_logging_queue(logging_data *lq)
 		if (lq->ld_host != host)
 			netdir_free(clnames, ND_HOSTSERVLIST);
 
-		lq->ld_next = lq_clean;
-		lq_clean = lq;
+		free_logging_data(lq);
+		cleared++;
 
 		(void) mutex_lock(&logging_queue_lock);
 		lq = remove_head_of_queue();
 		(void) mutex_unlock(&logging_queue_lock);
-	}
-
-	while (lq_clean) {
-		lq = lq_clean;
-		lq_clean = lq->ld_next;
-
-		free_logging_data(lq);
-		cleared++;
 	}
 
 	DTRACE_PROBE1(mountd, logging_cleared, cleared);
