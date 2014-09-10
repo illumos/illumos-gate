e94f268a6264bf027b3bc556fb4cceb84f7323c5
Matthew Ahrens <mahrens@delphix.com>
2014-09-10 10:19:51 -0800

    5141 zfs minimum indirect block size is 4K Reviewed by: Christopher Siden <christopher.siden@delphix.com> Reviewed by: George Wilson <george.wilson@delphix.com> Reviewed by: Richard Elling <richard.elling@gmail.com> Approved by: Dan McDonald <danmcd@omniti.com>

--- usr/src/grub/grub-0.97/stage2/zfs-include/dnode.h
+++ usr/src/grub/grub-0.97/stage2/zfs-include/dnode.h
@@ -20,6 +20,9 @@
  * Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
+/*
+ * Copyright (c) 2014 by Delphix. All rights reserved.
+ */
 
 #ifndef	_SYS_DNODE_H
 #define	_SYS_DNODE_H
@@ -28,7 +31,7 @@
  * Fixed constants.
  */
 #define	DNODE_SHIFT		9	/* 512 bytes */
-#define	DN_MIN_INDBLKSHIFT	10	/* 1k */
+#define	DN_MIN_INDBLKSHIFT	12	/* 4k */
 #define	DN_MAX_INDBLKSHIFT	14	/* 16k */
 #define	DNODE_BLOCK_SHIFT	14	/* 16k */
 #define	DNODE_CORE_SIZE		64	/* 64 bytes for dnode sans blkptrs */
