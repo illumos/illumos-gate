813c5e62b8b9a810e70a3c9127ce242952fc6fda
Jerry Jelinek <jerry.jelinek@joyent.com>
2014-12-01 17:07:51 +0000

    OS-3596 lxbrand 64bit truncate03 fails, max file size limit not enforced

--- usr/src/lib/brand/lx/lx_brand/common/truncate.c
+++ usr/src/lib/brand/lx/lx_brand/common/truncate.c
@@ -28,9 +28,48 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
 #include <sys/lx_types.h>
 #include <sys/lx_misc.h>
 #include <sys/lx_syscall.h>
+
+/*
+ * ZFS does not enforce the process.max-file-size rctl on a file which is
+ * grown in size via truncate/ftruncate, since that is simply metadata which
+ * does  not consume any additional space. However, LTP truncate03 depends on
+ * this behavior so we enforce it here.
+ */
+static boolean_t
+p_fsize_excd(const char *path, off_t length)
+{
+	struct stat64 sb;
+	struct rlimit rl;
+
+	if (stat64(path, &sb) == 0 && sb.st_size < length) {
+		/* We are growing the file, check the rlimit */
+		if (getrlimit(RLIMIT_FSIZE, &rl) == 0 && length > rl.rlim_cur)
+			return (B_TRUE);
+	}
+
+	return (B_FALSE);
+}
+
+static boolean_t
+f_fsize_excd(int fd, off_t length)
+{
+	struct stat64 sb;
+	struct rlimit rl;
+
+	if (fstat64(fd, &sb) == 0 && sb.st_size < length) {
+		/* We are growing the file, check the rlimit */
+		if (getrlimit(RLIMIT_FSIZE, &rl) == 0 && length > rl.rlim_cur)
+			return (B_TRUE);
+	}
+
+	return (B_FALSE);
+}
 
 /*
  * On Illumos, truncate() and ftruncate() are implemented in libc, so these are
@@ -45,6 +84,9 @@ lx_truncate(uintptr_t path, uintptr_t length)
 		return (-EFBIG);
 #endif
 
+	if (length > 0 && p_fsize_excd((const char *)path, (off_t)length))
+		return (-EFBIG);
+
 	return (truncate((const char *)path, (off_t)length) == 0 ? 0 : -errno);
 }
 
@@ -57,6 +99,9 @@ lx_ftruncate(uintptr_t fd, uintptr_t length)
 	if ((off_t)length >= 0xffffffffUL)
 		return (-EFBIG);
 #endif
+
+	if (length > 0 && f_fsize_excd((int)fd, (off_t)length))
+		return (-EFBIG);
 
 	r = ftruncate((int)fd, (off_t)length);
 	/*
@@ -87,6 +132,9 @@ lx_truncate64(uintptr_t path, uintptr_t length_lo, uintptr_t length_hi)
 	if (len >= 0x7fffffffffffffffULL)
 		return (-EFBIG);
 
+	if (len > 0 && p_fsize_excd((const char *)path, (off_t)len))
+		return (-EFBIG);
+
 	return (truncate64((const char *)path, len) == 0 ? 0 : -errno);
 }
 
@@ -97,6 +145,9 @@ lx_ftruncate64(uintptr_t fd, uintptr_t length_lo, uintptr_t length_hi)
 	uint64_t len = LX_32TO64(length_lo, length_hi);
 
 	if (len >= 0x7fffffffffffffffULL)
+		return (-EFBIG);
+
+	if (len > 0 && f_fsize_excd((int)fd, (off_t)len))
 		return (-EFBIG);
 
 	r = ftruncate64((int)fd, len);
