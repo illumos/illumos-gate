761ce542981ecc1979c36802e0c49730b221741d
Joshua M. Clulow <jmc@joyent.com>
2015-01-24 16:32:07 -0800

    OS-3758 mdb ::regs could print registers from a ucontext Reviewed by: Timothy J Fontaine <tj.fontaine@joyent.com>

--- usr/src/cmd/mdb/intel/mdb/proc_ia32dep.c
+++ usr/src/cmd/mdb/intel/mdb/proc_ia32dep.c
@@ -24,7 +24,7 @@
  * Use is subject to license terms.
  */
 /*
- * Copyright (c) 2012, Joyent, Inc.  All rights reserved.
+ * Copyright 2015 Joyent, Inc.
  */
 
 /*
@@ -40,10 +40,13 @@
 #include <mdb/mdb_ia32util.h>
 #include <mdb/mdb.h>
 
+#include <sys/ucontext.h>
 #include <sys/frame.h>
 #include <libproc.h>
 #include <sys/fp.h>
 #include <ieeefp.h>
+
+#include <stddef.h>
 
 const mdb_tgt_regdesc_t pt_regdesc[] = {
 	{ "gs", GS, MDB_TGT_R_EXPORT },
@@ -108,9 +111,37 @@ pt_regs(uintptr_t addr, uint_t flags, int argc, const mdb_arg_t *argv)
 	mdb_tgt_tid_t tid;
 	prgregset_t grs;
 	prgreg_t eflags;
+	boolean_t from_ucontext = B_FALSE;
 
-	if (argc != 0)
+	if (mdb_getopts(argc, argv,
+	    'u', MDB_OPT_SETBITS, B_TRUE, &from_ucontext, NULL) != argc) {
 		return (DCMD_USAGE);
+	}
+
+	if (from_ucontext) {
+		int off;
+		int o0, o1;
+
+		if (!(flags & DCMD_ADDRSPEC)) {
+			mdb_warn("-u requires a ucontext_t address\n");
+			return (DCMD_ERR);
+		}
+
+		o0 = mdb_ctf_offsetof_by_name("ucontext_t", "uc_mcontext");
+		o1 = mdb_ctf_offsetof_by_name("mcontext_t", "gregs");
+		if (o0 == -1 || o1 == -1) {
+			off = offsetof(ucontext_t, uc_mcontext) +
+			    offsetof(mcontext_t, gregs);
+		} else {
+			off = o0 + o1;
+		}
+
+		if (mdb_vread(&grs, sizeof (grs), addr + off) != sizeof (grs)) {
+			mdb_warn("failed to read from ucontext_t %p", addr);
+			return (DCMD_ERR);
+		}
+		goto print_regs;
+	}
 
 	if (t->t_pshandle == NULL || Pstate(t->t_pshandle) == PS_UNDEAD) {
 		mdb_warn("no process active\n");
@@ -132,6 +163,7 @@ pt_regs(uintptr_t addr, uint_t flags, int argc, const mdb_arg_t *argv)
 		return (DCMD_ERR);
 	}
 
+print_regs:
 	eflags = grs[EFL];
 
 	mdb_printf("%%cs = 0x%04x\t\t%%eax = 0x%0?p %A\n",
