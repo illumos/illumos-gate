307a938260b178544e5468346c1a036e497c7dd0
Patrick Mooney <patrick.f.mooney@gmail.com>
2015-08-03 21:07:03 +0000

    OS-4590 lxbrand wants /proc/sys/vm/swappiness Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com>

--- usr/src/uts/common/brand/lx/procfs/lx_proc.h
+++ usr/src/uts/common/brand/lx/procfs/lx_proc.h
@@ -205,9 +205,10 @@ typedef enum lxpr_nodetype {
 	LXPR_SYS_KERNEL_RAND_BOOTID, /* /proc/sys/kernel/random/boot_id */
 	LXPR_SYS_KERNEL_SHMMAX,	/* /proc/sys/kernel/shmmax */
 	LXPR_SYS_KERNEL_THREADS_MAX,	/* /proc/sys/kernel/threads-max */
-	LXPR_SYS_VMDIR,		/* /proc/sys/vm */
-	LXPR_SYS_KERNEL_VM_MINFR_KB, /* /proc/sys/vm/min_free_kbytes */
-	LXPR_SYS_KERNEL_VM_NHUGEP, /* /proc/sys/vm/nr_hugepages */
+	LXPR_SYS_VMDIR,		/* /proc/sys/vm			*/
+	LXPR_SYS_VM_MINFR_KB,	/* /proc/sys/vm/min_free_kbytes	*/
+	LXPR_SYS_VM_NHUGEP,	/* /proc/sys/vm/nr_hugepages	*/
+	LXPR_SYS_VM_SWAPPINESS,	/* /proc/sys/vm/swappiness	*/
 	LXPR_UPTIME,		/* /proc/uptime		*/
 	LXPR_VERSION,		/* /proc/version	*/
 	LXPR_NFILES		/* number of lx /proc file types */
