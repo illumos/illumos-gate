719d176126cc1c689a469d4c3553d770d1c4e94e
Toomas Soome <tsoome@me.com>
2016-03-04 20:35:11 +0200

    6723 etc/skel needs cleanup 2931 PS1 prompt in default user .profile is incorrect Reviewed by: Andrew Stormont <andyjstormont@gmail.com> Reviewed by: Lauri Tirkkonen <lotheac@iki.fi> Reviewed by: Gordon Ross <Gordon.W.Ross@gmail.com> Reviewed by: Adam Števko <adam.stevko@gmail.com> Reviewed by: Alexander Pyhalov <apyhalov@gmail.com> Approved by: Hans Rosenfeld <rosenfeld@grumpf.hope-2000.org>

--- usr/src/cmd/nsadmin/dot-profile.sh
+++ usr/src/cmd/nsadmin/dot-profile.sh
@@ -1,16 +1,14 @@
 #
-# Simple profile places /usr/gnu/bin at front,
-# adds /usr/X11/bin, /usr/sbin and /sbin to the end.
+# Uncommenting PATH below will place /usr/gnu/bin at front,
+# adds /usr/sbin and /sbin to the end.
 #
-# Use less(1) as the default pager for the man(1) command.
-#
-export PATH=/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
-export PAGER="/usr/bin/less -ins"
-
+# export PATH=/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
 #
 # Define default prompt to <username>@<hostname>:<path><"($|#) ">
 # and print '#' for user "root" and '$' for normal users.
 #
-PS1='${LOGNAME}@$(/usr/bin/hostname):$(
-    [[ "${LOGNAME}" == "root" ]] && printf "%s" "${PWD/${HOME}/~}# " ||
-    printf "%s" "${PWD/${HOME}/~}\$ ")'
+# override default prompt for bash
+# case "$0" in
+# -bash)
+#	export PS1="\u@\h:\w\\$ "
+# esac
