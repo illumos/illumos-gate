dcfa588d2cf0499b6a0a3af7b8bf0877c81cff67
Jerry Jelinek <jerry.jelinek@joyent.com>
2016-04-05 21:37:12 +0000

    OS-5305 latest LTP preadv02_64 and pwritev02_64 failing Reviewed by: Patrick Mooney <patrick.mooney@joyent.com>

--- usr/src/uts/common/brand/lx/syscall/lx_rw.c
+++ usr/src/uts/common/brand/lx/syscall/lx_rw.c
@@ -67,6 +67,13 @@ lx_iovec_copyin(void *uiovp, int iovcnt, iovec_t *kiovp, ssize_t *count)
 			kiovp[i].iov_len = iovlen32;
 			kiovp[i].iov_base =
 			    (caddr_t)(uintptr_t)aiov32[i].iov_base;
+			/* Linux does a basic sanity test on the address */
+			if ((uintptr_t)kiovp[i].iov_base >= USERLIMIT32) {
+				if (aiov32len != 0) {
+					kmem_free(aiov32, aiov32len);
+				}
+				return (EFAULT);
+			}
 		}
 		*count = total32;
 
@@ -85,6 +92,10 @@ lx_iovec_copyin(void *uiovp, int iovcnt, iovec_t *kiovp, ssize_t *count)
 			total += iovlen;
 			if (iovlen < 0 || total < 0) {
 				return (EINVAL);
+			}
+			/* Linux does a basic sanity test on the address */
+			if ((uintptr_t)kiovp[i].iov_base >= USERLIMIT) {
+				return (EFAULT);
 			}
 		}
 		*count = total;
