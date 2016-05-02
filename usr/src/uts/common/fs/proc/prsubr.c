f9d24f95309d8de8d9ba1c11ef62400c593c067e
Patrick Mooney <pmooney@pfmooney.com>
2016-05-02 19:50:56 +0000

    OS-5373 lx_proc panic while reading /proc/<pid>/exe link Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com>

--- usr/src/uts/common/fs/proc/prsubr.c
+++ usr/src/uts/common/fs/proc/prsubr.c
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 1989, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright 2015, Joyent, Inc.
+ * Copyright 2016, Joyent, Inc.
  */
 
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
@@ -535,6 +535,12 @@ prexecend(void)
 			pcp->prc_tslot = tslot;
 		}
 	}
+
+	/*
+	 * There may be threads waiting for the flag change blocked behind the
+	 * pr_pid_cv as well.
+	 */
+	cv_signal(&pr_pid_cv[p->p_slot]);
 }
 
 /*
