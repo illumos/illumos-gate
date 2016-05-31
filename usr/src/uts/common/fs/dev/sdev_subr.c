45b1747515a17db45e8971501ee84a26bdff37b2
Alex Wilson <alex.wilson@joyent.com>
2016-05-31 11:12:50 -0700

    7019 zfsdev_ioctl skips secpolicy when FKIOCTL is set 7020 sdev_cleandir can loop forever Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Richard Lowe <richlowe@richlowe.net> Reviewed by: Matthew Ahrens <mahrens@delphix.com> Approved by: Dan McDonald <danmcd@omniti.com>

--- usr/src/uts/common/fs/dev/sdev_subr.c
+++ usr/src/uts/common/fs/dev/sdev_subr.c
@@ -20,7 +20,7 @@
  */
 /*
  * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2013, Joyent, Inc. All rights reserved.
+ * Copyright (c) 2013, 2016 Joyent, Inc. All rights reserved.
  */
 
 /*
@@ -2281,7 +2281,7 @@ sdev_cleandir(struct sdev_node *ddv, char *expr, uint_t flags)
 	int error = 0;
 	int busy = 0;
 	struct vnode *vp;
-	struct sdev_node *dv;
+	struct sdev_node *dv, *next;
 	int bkstore = 0;
 	int len = 0;
 	char *bks_name = NULL;
@@ -2292,7 +2292,8 @@ sdev_cleandir(struct sdev_node *ddv, char *expr, uint_t flags)
 	 * We try our best to destroy all unused sdev_node's
 	 */
 	rw_enter(&ddv->sdev_contents, RW_WRITER);
-	while ((dv = SDEV_FIRST_ENTRY(ddv)) != NULL) {
+	for (dv = SDEV_FIRST_ENTRY(ddv); dv != NULL; dv = next) {
+		next = SDEV_NEXT_ENTRY(ddv, dv);
 		vp = SDEVTOV(dv);
 
 		if (expr && gmatch(dv->sdev_name, expr) == 0)
