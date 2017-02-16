f53522305c07915a44e86f2455cc62e7aac27037
Toomas Soome <tsoome@me.com>
2017-02-16 02:26:39 +0200

    8324 more: misleading-indentation Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Alexander Pyhalov <alp@rsu.ru> Approved by: Gordon Ross <gwr@nexenta.com>

--- usr/src/cmd/more/more.c
+++ usr/src/cmd/more/more.c
@@ -1089,8 +1089,8 @@ command(char *filename, register FILE *f)
                         done++;
                         goto endsw;
                     }
-                    Currline++;
-                    nlines--;
+                Currline++;
+                nlines--;
             }
             ret (dlines);
         case '\n':
@@ -1470,8 +1470,8 @@ skiplns(register off_t n, register FILE *f)
         while ((c = Getc (f)) != '\n')
             if (c == EOF)
                 return;
-            n--;
-            Currline++;
+        n--;
+        Currline++;
     }
 }
 
