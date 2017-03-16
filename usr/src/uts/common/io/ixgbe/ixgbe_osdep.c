0d9563764ec8cdc17c50d355ac32de77c4a0c335
Robert Mustacchi <rm@joyent.com>
2017-03-16 18:15:23 +0000

    OS-5891 ixgbe inifinte loops in rar and vmdq clearing Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com> Approved by: Jerry Jelinek <jerry.jelinek@joyent.com>

--- usr/src/uts/common/io/ixgbe/ixgbe_osdep.c
+++ usr/src/uts/common/io/ixgbe/ixgbe_osdep.c
@@ -24,10 +24,10 @@
 /*
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
+ * Copyright (c) 2017, Joyent, Inc.
  */
 
-#include "ixgbe_osdep.h"
-#include "ixgbe_api.h"
+#include "ixgbe_sw.h"
 
 uint16_t
 ixgbe_read_pci_cfg(struct ixgbe_hw *hw, uint32_t reg)
@@ -39,4 +39,31 @@ void
 ixgbe_write_pci_cfg(struct ixgbe_hw *hw, uint32_t reg, uint32_t val)
 {
 	pci_config_put16(OS_DEP(hw)->cfg_handle, reg, val);
+}
+
+/*
+ * This is our last line of defense against a hardware device that has decided
+ * to somehow disappear without our knowledge of it. To try and deal with this,
+ * we'll read the status register and see if it returns all 1s, indicating an
+ * invalid read. Note the status register is defined to have bits in all current
+ * revisions that are hardwired to zero.
+ */
+boolean_t
+ixgbe_removed(struct ixgbe_hw *hw)
+{
+	uint32_t val;
+
+	val = IXGBE_READ_REG(hw, IXGBE_STATUS);
+	if (val == PCI_EINVAL32) {
+		ixgbe_t *ixgbe = OS_DEP(hw)->ixgbe;
+
+		ixgbe_error(ixgbe, "failed to read status register: device "
+		    "may be gone");
+		if (ixgbe->ixgbe_ks != NULL) {
+			ixgbe_stat_t *s = ixgbe->ixgbe_ks->ks_data;
+			s->dev_gone.value.ui64++;
+		}
+		return (B_TRUE);
+	}
+	return (B_FALSE);
 }
