fe4e42e2947308b70ca67636d033b989c8ab5450
Toomas Soome <tsoome@me.com>
2017-05-02 21:43:54 +0300

    10204 uts: add dboot map debug printf Reviewed by: Gergő Mihály Doma <domag02@gmail.com> Reviewed by: Andy Fiddaman <andy@omniosce.org> Approved by: Robert Mustacchi <rm@joyent.com>

--- usr/src/uts/i86pc/dboot/dboot_startkern.c
+++ usr/src/uts/i86pc/dboot/dboot_startkern.c
@@ -2038,6 +2038,9 @@ build_page_tables(void)
 		end = start + fb->framebuffer_common.framebuffer_height *
 		    fb->framebuffer_common.framebuffer_pitch;
 
+		if (map_debug)
+			dboot_printf("FB 1:1 map pa=%" PRIx64 "..%" PRIx64 "\n",
+			    start, end);
 		pte_bits |= PT_NOCACHE;
 		while (start < end) {
 			map_pa_at_va(start, start, 0);
