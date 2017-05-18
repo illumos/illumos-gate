e34d8872f4a713d904a4b34fb081060d1a7eba62
Sebastian Wiedenroth <wiedi@frubar.net>
2017-05-18 23:19:20 +0200

    3729 getifaddrs must learn to stop worrying and love the other address families Reviewed by: Yuri Pankov <yuri.pankov@gmail.com> Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Dan McDonald <danmcd@kebe.com> Approved by: Gordon Ross <gwr@nexenta.com>

--- usr/src/lib/libipadm/common/ipadm_addr.c
+++ usr/src/lib/libipadm/common/ipadm_addr.c
@@ -375,6 +375,9 @@ retry:
 	for (ifap = ifa; ifap != NULL; ifap = ifap->ifa_next) {
 		struct sockaddr_storage data;
 
+		if (ifap->ifa_addr->sa_family == AF_LINK)
+			continue;
+
 		(void) strlcpy(cifname, ifap->ifa_name, sizeof (cifname));
 		lnum = 0;
 		if ((sep = strrchr(cifname, ':')) != NULL) {
