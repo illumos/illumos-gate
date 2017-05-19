2a6fb28d0877f35efb94c09cc03e8088426d0c30
Marcel Telka <marcel@telka.sk>
2017-05-19 12:59:57 +0200

    8250 libnsl: Raw RPC client sends unlimited data Reviewed by: Yuri Pankov <yuripv@gmx.com> Reviewed by: Jason King <jason.brian.king+illumos@gmail.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/lib/libnsl/rpc/svc_raw.c
+++ usr/src/lib/libnsl/rpc/svc_raw.c
@@ -32,14 +32,11 @@
  * California.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 /*
- * svc_raw.c,   This a toy for simple testing and timing.
+ * svc_raw.c,   This is a toy for simple testing and timing.
  * Interface to create an rpc client and server in the same UNIX process.
- * This lets us similate rpc and get rpc (round trip) overhead, without
+ * This lets us simulate rpc and get rpc (round trip) overhead, without
  * any interference from the kernal.
- *
  */
 
 #include "mt.h"
@@ -47,7 +44,6 @@
 #include <stdlib.h>
 #include <rpc/rpc.h>
 #include <sys/types.h>
-#include <rpc/raw.h>
 #include <syslog.h>
 
 #ifndef UDPMSGSIZE
@@ -58,7 +54,7 @@
  * This is the "network" that we will be moving data over
  */
 static struct svc_raw_private {
-	char	*raw_buf;	/* should be shared with the cl handle */
+	struct netbuf	*raw_netbuf;
 	SVCXPRT	*server;
 	XDR	xdr_stream;
 	char	verf_body[MAX_AUTH_BYTES];
@@ -67,56 +63,66 @@ static struct svc_raw_private {
 static struct xp_ops *svc_raw_ops();
 extern mutex_t	svcraw_lock;
 
-
+/*
+ * This netbuf is shared with the raw client.
+ */
+struct netbuf _rawcomnetbuf;
 
 SVCXPRT *
 svc_raw_create(void)
 {
 	struct svc_raw_private *srp;
-	bool_t flag1 = FALSE, flag2 = FALSE;
 
 /* VARIABLES PROTECTED BY svcraw_lock: svc_raw_private, srp */
 	(void) mutex_lock(&svcraw_lock);
 	srp = svc_raw_private;
-	if (srp == NULL) {
-		srp = calloc(1, sizeof (*srp));
-		if (srp == NULL) {
-			syslog(LOG_ERR, "svc_raw_create: out of memory");
-			(void) mutex_unlock(&svcraw_lock);
-			return (NULL);
-		}
-		flag1 = TRUE;
-		if (_rawcombuf == NULL) {
-			_rawcombuf = calloc(UDPMSGSIZE, sizeof (char));
-			if (_rawcombuf == NULL) {
-				free(srp);
-				syslog(LOG_ERR, "svc_raw_create: "
-					"out of memory");
-				(void) mutex_unlock(&svcraw_lock);
-				return (NULL);
-			}
-			flag2 = TRUE;
-		}
-		srp->raw_buf = _rawcombuf; /* Share it with the client */
-		svc_raw_private = srp;
+	if (srp != NULL) {
+		(void) mutex_unlock(&svcraw_lock);
+		return (srp->server);
 	}
-	if ((srp->server = svc_xprt_alloc()) == NULL) {
-		if (flag2)
-			free(svc_raw_private->raw_buf);
-		if (flag1)
-			free(svc_raw_private);
+
+	srp = calloc(1, sizeof (*srp));
+	if (srp == NULL) {
+		syslog(LOG_ERR, "svc_raw_create: out of memory");
+
 		(void) mutex_unlock(&svcraw_lock);
 		return (NULL);
 	}
+
+	srp->raw_netbuf = &_rawcomnetbuf;
+	srp->raw_netbuf->buf = malloc(UDPMSGSIZE);
+	if (srp->raw_netbuf->buf == NULL) {
+		free(srp);
+		syslog(LOG_ERR, "svc_raw_create: out of memory");
+
+		(void) mutex_unlock(&svcraw_lock);
+		return (NULL);
+	}
+	srp->raw_netbuf->maxlen = UDPMSGSIZE;
+	srp->raw_netbuf->len = 0;
+
+	if ((srp->server = svc_xprt_alloc()) == NULL) {
+		free(srp->raw_netbuf->buf);
+		srp->raw_netbuf->buf = NULL;
+		srp->raw_netbuf->maxlen = 0;
+
+		free(srp);
+
+		(void) mutex_unlock(&svcraw_lock);
+		return (NULL);
+	}
+
 	/*
-	 * By convention, using FD_SETSIZE as the psuedo file descriptor
+	 * By convention, using FD_SETSIZE as the pseudo file descriptor
 	 */
 	srp->server->xp_fd = FD_SETSIZE;
 	srp->server->xp_port = 0;
 	srp->server->xp_ops = svc_raw_ops();
 	srp->server->xp_verf.oa_base = srp->verf_body;
-	xdrmem_create(&srp->xdr_stream, srp->raw_buf, UDPMSGSIZE, XDR_DECODE);
 	xprt_register(srp->server);
+
+	svc_raw_private = srp;
+
 	(void) mutex_unlock(&svcraw_lock);
 	return (srp->server);
 }
@@ -144,9 +150,16 @@ svc_raw_recv(SVCXPRT *xprt, struct rpc_msg *msg)
 	(void) mutex_unlock(&svcraw_lock);
 
 	xdrs = &srp->xdr_stream;
-	xdrs->x_op = XDR_DECODE;
-	(void) XDR_SETPOS(xdrs, 0);
-	return (xdr_callmsg(xdrs, msg));
+
+	xdrmem_create(xdrs, srp->raw_netbuf->buf, srp->raw_netbuf->len,
+	    XDR_DECODE);
+
+	if (!xdr_callmsg(xdrs, msg)) {
+		XDR_DESTROY(xdrs);
+		return (FALSE);
+	}
+
+	return (TRUE);
 }
 
 /*ARGSUSED*/
@@ -155,6 +168,7 @@ svc_raw_reply(SVCXPRT *xprt, struct rpc_msg *msg)
 {
 	struct svc_raw_private *srp;
 	XDR *xdrs;
+	uint_t start;
 
 	(void) mutex_lock(&svcraw_lock);
 	srp = svc_raw_private;
@@ -165,9 +179,19 @@ svc_raw_reply(SVCXPRT *xprt, struct rpc_msg *msg)
 	(void) mutex_unlock(&svcraw_lock);
 
 	xdrs = &srp->xdr_stream;
-	xdrs->x_op = XDR_ENCODE;
-	(void) XDR_SETPOS(xdrs, 0);
-	return (xdr_replymsg(xdrs, msg));
+
+	XDR_DESTROY(xdrs);
+	xdrmem_create(xdrs, srp->raw_netbuf->buf, srp->raw_netbuf->maxlen,
+	    XDR_ENCODE);
+
+	start = XDR_GETPOS(xdrs);
+	if (!xdr_replymsg(xdrs, msg)) {
+		XDR_DESTROY(xdrs);
+		return (FALSE);
+	}
+	srp->raw_netbuf->len = XDR_GETPOS(xdrs) - start;
+
+	return (TRUE);
 }
 
 /*ARGSUSED*/
@@ -183,6 +207,7 @@ svc_raw_getargs(SVCXPRT *xprt, xdrproc_t xdr_args, caddr_t args_ptr)
 		return (FALSE);
 	}
 	(void) mutex_unlock(&svcraw_lock);
+
 	return ((*xdr_args)(&srp->xdr_stream, args_ptr));
 }
 
@@ -191,7 +216,6 @@ static bool_t
 svc_raw_freeargs(SVCXPRT *xprt, xdrproc_t xdr_args, caddr_t args_ptr)
 {
 	struct svc_raw_private *srp;
-	XDR *xdrs;
 
 	(void) mutex_lock(&svcraw_lock);
 	srp = svc_raw_private;
@@ -201,9 +225,11 @@ svc_raw_freeargs(SVCXPRT *xprt, xdrproc_t xdr_args, caddr_t args_ptr)
 	}
 	(void) mutex_unlock(&svcraw_lock);
 
-	xdrs = &srp->xdr_stream;
-	xdrs->x_op = XDR_FREE;
-	return ((*xdr_args)(xdrs, args_ptr));
+	XDR_DESTROY(&srp->xdr_stream);
+
+	xdr_free(xdr_args, args_ptr);
+
+	return (TRUE);
 }
 
 /*ARGSUSED*/
