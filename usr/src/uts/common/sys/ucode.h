c23fbbde57ac87a130a6228768d316eb83c713ca
Robert Mustacchi <rm@joyent.com>
2017-06-14 23:42:14 +0000

    OS-6189 UCODE_MAX_SIZE is too small for modern micrcode Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com> Approved by: Jerry Jelinek <jerry.jelinek@joyent.com>

--- usr/src/uts/common/sys/ucode.h
+++ usr/src/uts/common/sys/ucode.h
@@ -197,7 +197,7 @@ typedef union ucode_file {
 #define	UCODE_DEFAULT_BODY_SIZE		(UCODE_KB(2) - UCODE_HEADER_SIZE_INTEL)
 
 /*
- * For a single microcode file, the minimum size is 1K, maximum size is 16K.
+ * For a single microcode file, the minimum size is 1K, maximum size is 128K.
  * Such limitations, while somewhat artificial, are not only to provide better
  * sanity checks, but also avoid wasting precious memory at startup time as the
  * microcode buffer for the first processor has to be statically allocated.
@@ -206,7 +206,7 @@ typedef union ucode_file {
  * is 16M.
  */
 #define	UCODE_MIN_SIZE			UCODE_KB(1)
-#define	UCODE_MAX_SIZE			UCODE_KB(16)
+#define	UCODE_MAX_SIZE			UCODE_KB(128)
 #define	UCODE_MAX_COMBINED_SIZE		UCODE_MB(16)
 
 #define	UCODE_SIZE_CONVERT(size, default_size) \
