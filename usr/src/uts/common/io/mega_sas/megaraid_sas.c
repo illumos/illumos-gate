2811ec437a3102acd185cc6fafc9a716169cac2a
Toomas Soome <tsoome@me.com>
2017-07-02 00:40:27 +0300

    9232 mega_sas: this statement may fall through Reviewed by: Andy Fiddaman <omnios@citrus-it.co.uk> Reviewed by: Sebastian Wiedenroth <sebastian.wiedenroth@skylime.net> Approved by: Hans Rosenfeld <rosenfeld@grumpf.hope-2000.org>

--- usr/src/uts/common/io/mega_sas/megaraid_sas.c
+++ usr/src/uts/common/io/mega_sas/megaraid_sas.c
@@ -3448,7 +3448,8 @@ build_cmd(struct megasas_instance *instance, struct scsi_address *ap,
 
 			break;
 		}
-		/* fall through For all non-rd/wr cmds */
+		/* For all non-rd/wr cmds */
+		/* FALLTHROUGH */
 	default:
 		pthru	= (struct megasas_pthru_frame *)cmd->frame;
 
