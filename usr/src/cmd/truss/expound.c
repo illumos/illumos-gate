0b09d754d66bb2026be92bbbc38f7c8ba454cf0c
Toomas Soome <tsoome@me.com>
2017-07-16 23:10:16 +0300

    9355 truss: this statement may fall through Reviewed by: Andy Fiddaman <omnios@citrus-it.co.uk> Reviewed by: C Fraire <cfraire@me.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/cmd/truss/expound.c
+++ usr/src/cmd/truss/expound.c
@@ -1171,8 +1171,8 @@ static const struct audio_stuff audio_sw_features[] = {
 
 void
 show_audio_features(const private_t *pri,
-	const struct audio_stuff *audio_porttab, uint_t features,
-	const char *name)
+    const struct audio_stuff *audio_porttab, uint_t features,
+    const char *name)
 {
 	(void) printf("%s\t%s=", pri->pname, name);
 	if (features == 0) {
@@ -1195,7 +1195,7 @@ show_audio_features(const private_t *pri,
 
 void
 show_audio_ports(private_t *pri, const char *mode,
-	const char *field, uint_t ports)
+    const char *field, uint_t ports)
 {
 	const struct audio_stuff *audio_porttab;
 
@@ -3495,7 +3495,7 @@ show_adjtime(private_t *pri, long off1, long off2)
 
 void
 show_sockaddr(private_t *pri,
-	const char *str, long addroff, long lenoff, long len)
+    const char *str, long addroff, long lenoff, long len)
 {
 	/*
 	 * A buffer large enough for PATH_MAX size AF_UNIX address, which is
@@ -5268,11 +5268,11 @@ expound(private_t *pri, long r0, int raw)
 			show_dents32(pri, (long)pri->sys_args[1], r0);
 			break;
 		}
-		/* FALLTHROUGH */
 #else
 		show_dents32(pri, (long)pri->sys_args[1], r0);
 		break;
 #endif
+		/* FALLTHROUGH */
 	case SYS_getdents64:
 		if (err || pri->sys_nargs <= 1 || r0 <= 0)
 			break;
