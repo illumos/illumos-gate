d87d03b4c0f66bf125e607ef8b0d9c5481040d20
Toomas Soome <tsoome@me.com>
2017-08-08 12:31:31 +0300

    8842 libmail: right-hand operand of comma expression has no effect Reviewed by: Yuri Pankov <yuripv@gmx.com> Reviewed by: Andrew Stormont <andyjstormont@gmail.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/lib/libmail/common/s_string.c
+++ usr/src/lib/libmail/common/s_string.c
@@ -27,8 +27,6 @@
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved  	*/
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 #include <sys/types.h>
 #include <stdio.h>
 #include <ctype.h>
