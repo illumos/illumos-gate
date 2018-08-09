6640c13beae5a8b03718dc6b7a9cda5dd6ab9024
Yuri Pankov <yuri.pankov@nexenta.com>
2018-08-09 15:17:53 +0300

    9718 update mandoc to 1.14.4 Reviewed by: Toomas Soome <tsoome@me.com> Reviewed by: Robert Mustacchi <rm@joyent.com> Approved by: Robert Mustacchi <rm@joyent.com>

--- usr/src/tools/scripts/webrev.sh
+++ usr/src/tools/scripts/webrev.sh
@@ -148,119 +148,261 @@ span.new {
 '
 
 # CSS for the HTML version of the man pages.
-# Current version is from mandoc 1.14.2.
+# Current version is from mandoc 1.14.4.
 MANCSS='
-html { max-width: 100ex; }
-body { font-family: Helvetica,Arial,sans-serif; }
-table { margin-top: 0em; margin-bottom: 0em; }
-td { vertical-align: top; }
-ul, ol, dl { margin-top: 0em; margin-bottom: 0em; }
-li, dt { margin-top: 1em; }
-a.selflink { border-bottom: thin dotted; color: inherit; font: inherit;
-    text-decoration: inherit; }
-fieldset { border: thin solid silver; border-radius: 1em; text-align: center; }
-input[name=expr] { width: 25%; }
-table.results { margin-top: 1em; margin-left: 2em; font-size: smaller; }
-table.head { width: 100%; border-bottom: 1px dotted #808080; margin-bottom: 1em;
-    font-size: smaller; }
-td.head-vol { text-align: center; }
-td.head-rtitle { text-align: right; }
-span.Nd { }
-table.foot { width: 100%; border-top: 1px dotted #808080; margin-top: 1em;
-    font-size: smaller; }
-td.foot-os { text-align: right; }
-div.manual-text { margin-left: 5ex; }
-h1.Sh { margin-top: 2ex; margin-bottom: 1ex; margin-left: -4ex;
-    font-size: 110%; }
-h2.Ss { margin-top: 2ex; margin-bottom: 1ex; margin-left: -2ex;
-    font-size: 105%; }
-div.Pp { margin: 1ex 0ex; }
-a.Sx { }
-a.Xr { }
-div.Bd { }
-div.D1 { margin-left: 5ex; }
-ul.Bl-bullet { list-style-type: disc; padding-left: 1em; }
-li.It-bullet { }
-ul.Bl-dash { list-style-type: none; padding-left: 0em; }
-li.It-dash:before { content: "\2014  "; }
-ul.Bl-item { list-style-type: none; padding-left: 0em; }
-li.It-item { }
-ul.Bl-compact > li { margin-top: 0ex; }
-ol.Bl-enum { padding-left: 2em; }
-li.It-enum { }
-ol.Bl-compact > li { margin-top: 0ex; }
-dl.Bl-diag { }
-dt.It-diag { }
-dd.It-diag { margin-left: 0ex; }
-b.It-diag { font-style: normal; }
-dl.Bl-hang { }
-dt.It-hang { }
-dd.It-hang { margin-left: 10.2ex; }
-dl.Bl-inset { }
-dt.It-inset { }
-dd.It-inset { margin-left: 0ex; }
-dl.Bl-ohang { }
-dt.It-ohang { }
-dd.It-ohang { margin-left: 0ex; }
-dl.Bl-tag { margin-left: 10.2ex; }
-dt.It-tag { float: left; margin-top: 0ex; margin-left: -10.2ex;
-    padding-right: 2ex; vertical-align: top; }
-dd.It-tag { clear: right; width: 100%; margin-top: 0ex; margin-left: 0ex;
-    vertical-align: top; overflow: auto; }
-dl.Bl-compact > dt { margin-top: 0ex; }
-table.Bl-column { }
-tr.It-column { }
-td.It-column { margin-top: 1em; }
-table.Bl-compact > tbody > tr > td { margin-top: 0ex; }
-cite.Rs { font-style: normal; font-weight: normal; }
-span.RsA { }
-i.RsB { font-weight: normal; }
-span.RsC { }
-span.RsD { }
-i.RsI { font-weight: normal; }
-i.RsJ { font-weight: normal; }
-span.RsN { }
-span.RsO { }
-span.RsP { }
-span.RsQ { }
-span.RsR { }
-span.RsT { text-decoration: underline; }
-a.RsU { }
-span.RsV { }
-span.eqn { }
-table.tbl { }
+/* $Id: mandoc.css,v 1.36 2018/07/23 22:51:26 schwarze Exp $ */
+/*
+ * Standard style sheet for mandoc(1) -Thtml and man.cgi(8).
+ */
+
+/* Global defaults. */
+
+html {		max-width: 65em; }
+body {		font-family: Helvetica,Arial,sans-serif; }
+table {		margin-top: 0em;
+		margin-bottom: 0em; }
+td {		vertical-align: top; }
+ul, ol, dl {	margin-top: 0em;
+		margin-bottom: 0em; }
+li, dt {	margin-top: 1em; }
+
+.permalink {	border-bottom: thin dotted;
+		color: inherit;
+		font: inherit;
+		text-decoration: inherit; }
+* {		clear: both }
+
+/* Search form and search results. */
+
+fieldset {	border: thin solid silver;
+		border-radius: 1em;
+		text-align: center; }
+input[name=expr] {
+		width: 25%; }
+
+table.results {	margin-top: 1em;
+		margin-left: 2em;
+		font-size: smaller; }
+
+/* Header and footer lines. */
+
+table.head {	width: 100%;
+		border-bottom: 1px dotted #808080;
+		margin-bottom: 1em;
+		font-size: smaller; }
+td.head-vol {	text-align: center; }
+td.head-rtitle {
+		text-align: right; }
+
+table.foot {	width: 100%;
+		border-top: 1px dotted #808080;
+		margin-top: 1em;
+		font-size: smaller; }
+td.foot-os {	text-align: right; }
+
+/* Sections and paragraphs. */
+
+.manual-text {
+		margin-left: 3.8em; }
+.Nd {		display: inline; }
+.Sh {		margin-top: 1.2em;
+		margin-bottom: 0.6em;
+		margin-left: -3.2em;
+		font-size: 110%; }
+.Ss {		margin-top: 1.2em;
+		margin-bottom: 0.6em;
+		margin-left: -1.2em;
+		font-size: 105%; }
+.Pp {		margin: 0.6em 0em; }
+.Sx { }
+.Xr { }
+
+/* Displays and lists. */
+
+.Bd { }
+.Bd-indent {	margin-left: 3.8em; }
+
+.Bl-bullet {	list-style-type: disc;
+		padding-left: 1em; }
+.Bl-bullet > li { }
+.Bl-dash {	list-style-type: none;
+		padding-left: 0em; }
+.Bl-dash > li:before {
+		content: "\2014  "; }
+.Bl-item {	list-style-type: none;
+		padding-left: 0em; }
+.Bl-item > li { }
+.Bl-compact > li {
+		margin-top: 0em; }
+
+.Bl-enum {	padding-left: 2em; }
+.Bl-enum > li { }
+.Bl-compact > li {
+		margin-top: 0em; }
+
+.Bl-diag { }
+.Bl-diag > dt {
+		font-style: normal;
+		font-weight: bold; }
+.Bl-diag > dd {
+		margin-left: 0em; }
+.Bl-hang { }
+.Bl-hang > dt { }
+.Bl-hang > dd {
+		margin-left: 5.5em; }
+.Bl-inset { }
+.Bl-inset > dt { }
+.Bl-inset > dd {
+		margin-left: 0em; }
+.Bl-ohang { }
+.Bl-ohang > dt { }
+.Bl-ohang > dd {
+		margin-left: 0em; }
+.Bl-tag {	margin-left: 5.5em; }
+.Bl-tag > dt {
+		float: left;
+		margin-top: 0em;
+		margin-left: -5.5em;
+		padding-right: 1.2em;
+		vertical-align: top; }
+.Bl-tag > dd {
+		clear: right;
+		width: 100%;
+		margin-top: 0em;
+		margin-left: 0em;
+		vertical-align: top;
+		overflow: auto; }
+.Bl-compact > dt {
+		margin-top: 0em; }
+
+.Bl-column { }
+.Bl-column > tbody > tr { }
+.Bl-column > tbody > tr > td {
+		margin-top: 1em; }
+.Bl-compact > tbody > tr > td {
+		margin-top: 0em; }
+
+.Rs {		font-style: normal;
+		font-weight: normal; }
+.RsA { }
+.RsB {		font-style: italic;
+		font-weight: normal; }
+.RsC { }
+.RsD { }
+.RsI {		font-style: italic;
+		font-weight: normal; }
+.RsJ {		font-style: italic;
+		font-weight: normal; }
+.RsN { }
+.RsO { }
+.RsP { }
+.RsQ { }
+.RsR { }
+.RsT {		text-decoration: underline; }
+.RsU { }
+.RsV { }
+
+.eqn { }
+.tbl { }
+
+.HP {		margin-left: 3.8em;
+		text-indent: -3.8em; }
+
+/* Semantic markup for command line utilities. */
+
 table.Nm { }
-b.Nm { font-style: normal; }
-b.Fl { font-style: normal; }
-b.Cm { font-style: normal; }
-var.Ar { font-style: italic; font-weight: normal; }
-span.Op { }
-b.Ic { font-style: normal; }
-code.Ev { font-style: normal; font-weight: normal; font-family: monospace; }
-i.Pa { font-weight: normal; }
-span.Lb { }
-b.In { font-style: normal; }
+code.Nm {	font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Fl {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Cm {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Ar {		font-style: italic;
+		font-weight: normal; }
+.Op {		display: inline; }
+.Ic {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Ev {		font-style: normal;
+		font-weight: normal;
+		font-family: monospace; }
+.Pa {		font-style: italic;
+		font-weight: normal; }
+
+/* Semantic markup for function libraries. */
+
+.Lb { }
+code.In {	font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
 a.In { }
-b.Fd { font-style: normal; }
-var.Ft { font-style: italic; font-weight: normal; }
-b.Fn { font-style: normal; }
-var.Fa { font-style: italic; font-weight: normal; }
-var.Vt { font-style: italic; font-weight: normal; }
-var.Va { font-style: italic; font-weight: normal; }
-code.Dv { font-style: normal; font-weight: normal; font-family: monospace; }
-code.Er { font-style: normal; font-weight: normal; font-family: monospace; }
-span.An { }
-a.Lk { }
-a.Mt { }
-b.Cd { font-style: normal; }
-i.Ad { font-weight: normal; }
-b.Ms { font-style: normal; }
-span.St { }
-a.Ux { }
-.No { font-style: normal; font-weight: normal; }
-.Em { font-style: italic; font-weight: normal; }
-.Sy { font-style: normal; font-weight: bold; }
-.Li { font-style: normal; font-weight: normal; font-family: monospace; }
+.Fd {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Ft {		font-style: italic;
+		font-weight: normal; }
+.Fn {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Fa {		font-style: italic;
+		font-weight: normal; }
+.Vt {		font-style: italic;
+		font-weight: normal; }
+.Va {		font-style: italic;
+		font-weight: normal; }
+.Dv {		font-style: normal;
+		font-weight: normal;
+		font-family: monospace; }
+.Er {		font-style: normal;
+		font-weight: normal;
+		font-family: monospace; }
+
+/* Various semantic markup. */
+
+.An { }
+.Lk { }
+.Mt { }
+.Cd {		font-style: normal;
+		font-weight: bold;
+		font-family: inherit; }
+.Ad {		font-style: italic;
+		font-weight: normal; }
+.Ms {		font-style: normal;
+		font-weight: bold; }
+.St { }
+.Ux { }
+
+/* Physical markup. */
+
+.Bf {		display: inline; }
+.No {		font-style: normal;
+		font-weight: normal; }
+.Em {		font-style: italic;
+		font-weight: normal; }
+.Sy {		font-style: normal;
+		font-weight: bold; }
+.Li {		font-style: normal;
+		font-weight: normal;
+		font-family: monospace; }
+
+/* Overrides to avoid excessive margins on small devices. */
+
+@media (max-width: 37.5em) {
+.manual-text {
+		margin-left: 0.5em; }
+.Sh, .Ss {	margin-left: 0em; }
+.Bd-indent {	margin-left: 2em; }
+.Bl-hang > dd {
+		margin-left: 2em; }
+.Bl-tag {	margin-left: 2em; }
+.Bl-tag > dt {
+		margin-left: -2em; }
+.HP {		margin-left: 2em;
+		text-indent: -2em; }
+}
 '
 
 #
