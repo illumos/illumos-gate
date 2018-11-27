6466dea896ec0283655f3eb5820688dd3a3e7ff2
Robert Mustacchi <rm@joyent.com>
2018-11-27 16:49:30 +0000

    OS-7393 9903 merge lost OS-3752 modifications Reviewed by: Patrick Mooney <patrick.mooney@joyent.com> Approved by: Patrick Mooney <patrick.mooney@joyent.com>

--- usr/src/uts/common/sys/stream.h
+++ usr/src/uts/common/sys/stream.h
@@ -651,7 +651,7 @@ struct stroptions {
 struct struiod {
 	mblk_t		*d_mp;		/* pointer to mblk (chain) */
 	uio_t		d_uio;		/* uio info */
-	iovec_t d_iov[DEF_IOV_MAX];	/* iov referenced by uio */
+	iovec_t		*d_iov;		/* iov referenced by uio */
 };
 
 /*
