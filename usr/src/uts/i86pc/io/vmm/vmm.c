b3bd764962b86d7c9b6cb4939d04a3bde0ca6519
Hans Rosenfeld <hans.rosenfeld@joyent.com>
2018-12-05 22:47:24 +0000

    OS-7401 bhyve: suspend vcpu after mtrap vmexit Reviewed by: Patrick Mooney <patrick.mooney@joyent.com> Reviewed by: John Levon <john.levon@joyent.com> Approved by: John Levon <john.levon@joyent.com>

--- usr/src/uts/i86pc/io/vmm/vmm.c
+++ usr/src/uts/i86pc/io/vmm/vmm.c
@@ -2205,6 +2205,10 @@ restart:
 			break;
 		}
 
+		case VM_EXITCODE_MTRAP:
+			vm_suspend_cpu(vm, vcpuid);
+			retu = true;
+			break;
 #endif
 		default:
 			retu = true;	/* handled in userland */
