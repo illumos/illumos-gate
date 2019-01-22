51b02b292e47be2588e43efa7cff6e9fc931fba0
Toomas Soome <tsoome@me.com>
2019-01-22 19:15:45 +0200

    11167 libsldap: NULL pointer errors Reviewed by: John Levon <john.levon@joyent.com> Approved by: Richard Lowe <richlowe@richlowe.net>

--- usr/src/lib/libsldap/common/ns_connmgmt.c
+++ usr/src/lib/libsldap/common/ns_connmgmt.c
@@ -2623,7 +2623,7 @@ start_thread(ns_conn_mgmt_t *cmg) {
 	/*
 	 * start a thread to get and process config and server status changes
 	 */
-	if (thr_create(NULL, NULL, get_server_change,
+	if (thr_create(NULL, 0, get_server_change,
 	    (void *)cmg, THR_DETACHED, NULL) != 0) {
 		errnum = errno;
 		syslog(LOG_WARNING, NS_CONN_MSG_NO_PROCCHG_THREAD,
