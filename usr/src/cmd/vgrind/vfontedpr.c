e8bca4ea3a0f5e2a084777440ee86870f8c9d88f
Toomas Soome <tsoome@me.com>
2019-01-26 18:17:48 +0200

    11438 vgrind: NULL pointer errors Reviewed by: Andy Fiddaman <andy@omniosce.org> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/cmd/vgrind/vfontedpr.c
+++ usr/src/cmd/vgrind/vfontedpr.c
@@ -267,7 +267,7 @@ flagsdone:
 	cp = strings;
 	while (*cp) {
 	    while (*cp == ' ' || *cp =='\t')
-		*cp++ = NULL;
+		*cp++ = '\0';
 	    if (*cp)
 		*cpp++ = cp;
 	    while (*cp != ' ' && *cp  != '\t' && *cp)
@@ -355,7 +355,7 @@ flagsdone:
 	blklevel = 0;
 	prclevel = -1;
 	for (psptr=0; psptr<PSMAX; psptr++) {
-	    pstack[psptr][0] = NULL;
+	    pstack[psptr][0] = '\0';
 	    plstack[psptr] = 0;
 	}
 	psptr = -1;
@@ -445,7 +445,7 @@ putScp(char *os)
 	if (psptr < PSMAX-1) {
 	    ++psptr;
 	    strncpy (pstack[psptr], pname, PNAMELEN);
-	    pstack[psptr][PNAMELEN] = NULL;
+	    pstack[psptr][PNAMELEN] = '\0';
 	    plstack[psptr] = blklevel;
 	}
     }
@@ -801,10 +801,9 @@ putcp(int c)
  *	look for a process beginning on this line
  */
 boolean
-isproc(s)
-    char *s;
+isproc(char *s)
 {
-    pname[0] = NULL;
+    pname[0] = '\0';
     if (l_prclevel ? (prclevel == blklevel) : (!l_toplex || blklevel == 0))
 	if (expmatch (s, l_prcbeg, pname) != NIL) {
 	    return (TRUE);
