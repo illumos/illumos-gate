ecbdacdd6b004e9386244a353fb5ddbf60cb6b0a
Vishal Kulkarni <vishal@chelsio.com>
2019-05-03 21:43:43 +0000

    10910 Update cxgbe firmware version to 1.23.4.0 Reviewed by: Toomas Soome <tsoome@me.com> Reviewed by: Garrett D'Amore <garrett@damore.org> Approved by: Garrett D'Amore <garrett@damore.org>

--- usr/src/uts/common/io/cxgbe/firmware/t4_fw.h
+++ usr/src/uts/common/io/cxgbe/firmware/t4_fw.h
@@ -5,10 +5,10 @@
 #ifndef __T4FW_H
 #define __T4FW_H
 
-#define T4FW_VERSION_MAJOR 1
-#define T4FW_VERSION_MINOR 17
-#define T4FW_VERSION_MICRO 14
-#define T4FW_VERSION_BUILD 0
+#define FW_VERSION_MAJOR 1
+#define FW_VERSION_MINOR 23
+#define FW_VERSION_MICRO 4
+#define FW_VERSION_BUILD 0
 
 extern unsigned char t4fw_data[];
 extern int t4fw_size;
