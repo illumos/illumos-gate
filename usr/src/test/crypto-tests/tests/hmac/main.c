9a27e9327a9c5764a2fd7bc4f5f9ce7bddc512b9
Jason King <jason.king@joyent.com>
2019-07-10 04:55:35 +0000

    OS-7881 Add HMAC_SHA1 crypto tests Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Mike Gerdts <mike.gerdts@joyent.com> Approved by: Mike Gerdts <mike.gerdts@joyent.com>

---
+++ usr/src/test/crypto-tests/tests/hmac/main.c
@@ -0,0 +1,58 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2019 Joyent, Inc.
+ */
+
+#include <stdio.h>
+
+#include "cryptotest.h"
+
+extern char *mechname;
+extern uint8_t *KEY[];
+extern size_t KEYLEN[];
+extern uint8_t *DATA[];
+extern size_t DATALEN[];
+extern uint8_t *HMAC[];
+extern size_t hmac_len;
+extern size_t msgcount;
+
+int
+main(void)
+{
+	int errs = 0;
+	int i;
+	uint8_t N[1024];
+	cryptotest_t args = {
+		.out = N,
+		.outlen = sizeof (N),
+		.plen = 0,
+		.mechname = mechname,
+		.updatelen = 1
+	};
+
+	for (i = 0; i < msgcount; i++) {
+		args.key = KEY[i];
+		args.keylen = KEYLEN[i];
+
+		args.in = DATA[i];
+		args.inlen = DATALEN[i];
+
+		errs += run_test(&args, HMAC[i], hmac_len, MAC_FG);
+		(void) fprintf(stderr, "----------\n");
+	}
+	if (errs != 0)
+		(void) fprintf(stderr, "%d tests failed\n", errs);
+
+	return (errs);
+}
