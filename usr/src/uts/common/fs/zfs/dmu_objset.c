8bf394f116a79c011b8f9f3bd199e09b363742ef
Tom Caputi <tcaputi@datto.com>
2019-09-12 09:24:01 -0600

    11650 ZFS user accounting fixes Portions contributed by: Jerry Jelinek <jerry.jelinek@joyent.com> Portions contributed by: loli10K <ezomori.nozomu@gmail.com> Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov> Reviewed by: Alek Pinchuk <apinchuk@datto.com> Reviewed by: Igor Kozhukhov <igor@dilos.org> Reviewed by: Toomas Soome <tsoome@me.com> Reviewed by: Kody Kantor <kody.kantor@joyent.com> Approved by: Gordon Ross <gwr@nexenta.com>

--- usr/src/uts/common/fs/zfs/dmu_objset.c
+++ usr/src/uts/common/fs/zfs/dmu_objset.c
@@ -2051,6 +2051,9 @@ dmu_objset_do_userquota_updates(objset_t *os, dmu_tx_t *tx)
 	if (os->os_encrypted && dmu_objset_is_receiving(os))
 		return;
 
+	if (tx->tx_txg <= os->os_spa->spa_claim_max_txg)
+		return;
+
 	/* Allocate the user/group/project used objects if necessary. */
 	if (DMU_USERUSED_DNODE(os)->dn_type == DMU_OT_NONE) {
 		VERIFY0(zap_create_claim(os,
@@ -2390,7 +2393,8 @@ dmu_objset_projectquota_upgradable(objset_t *os)
 	return (dmu_objset_type(os) == DMU_OST_ZFS &&
 	    !dmu_objset_is_snapshot(os) &&
 	    dmu_objset_projectquota_enabled(os) &&
-	    !dmu_objset_projectquota_present(os));
+	    !dmu_objset_projectquota_present(os) &&
+	    spa_writeable(dmu_objset_spa(os)));
 }
 
 void
