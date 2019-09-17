f5700834e5d27ffa87ba2d9ec0fe5b4e38f5bb1c
Toomas Soome <tsoome@me.com>
2019-09-17 12:09:15 +0300

    11702 loader: zalloc cstyle cleanup Reviewed by: Andy Fiddaman <omnios@citrus-it.co.uk> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/boot/lib/libstand/zalloc_protos.h
+++ usr/src/boot/lib/libstand/zalloc_protos.h
@@ -1,5 +1,5 @@
 /*
- * This module derived from code donated to the FreeBSD Project by 
+ * This module derived from code donated to the FreeBSD Project by
  * Matthew Dillon <dillon@backplane.com>
  *
  * Copyright (c) 1998 The FreeBSD Project
@@ -25,11 +25,14 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
+
+#ifndef _ZALLOC_PROTOS_H
+#define	_ZALLOC_PROTOS_H
 
 Library void *znalloc(struct MemPool *mpool, uintptr_t bytes);
 Library void zfree(struct MemPool *mpool, void *ptr, uintptr_t bytes);
 Library void zextendPool(MemPool *mp, void *base, uintptr_t bytes);
 Library void zallocstats(struct MemPool *mp);
+
+#endif	/* _ZALLOC_PROTOS_H */
