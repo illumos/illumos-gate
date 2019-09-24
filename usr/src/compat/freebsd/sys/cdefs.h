3105c6ff4e5cab926dc4802a7e10eee1f4abbec4
Hans Rosenfeld <hans.rosenfeld@joyent.com>
2019-09-24 13:12:56 +0000

    OS-7992 use native linker sets for bhyve build Reviewed by: John Levon <john.levon@joyent.com> Reviewed by: Michael Zeller <mike.zeller@joyent.com> Approved by: John Levon <john.levon@joyent.com>

--- usr/src/compat/freebsd/sys/cdefs.h
+++ usr/src/compat/freebsd/sys/cdefs.h
@@ -53,30 +53,6 @@
 #define	__weak_symbol   __attribute__((__weak__))
 #endif
 
-/*
- * The __CONCAT macro is used to concatenate parts of symbol names, e.g.
- * with "#define OLD(foo) __CONCAT(old,foo)", OLD(foo) produces oldfoo.
- * The __CONCAT macro is a bit tricky to use if it must work in non-ANSI
- * mode -- there must be no spaces between its arguments, and for nested
- * __CONCAT's, all the __CONCAT's must be at the left.  __CONCAT can also
- * concatenate double-quoted strings produced by the __STRING macro, but
- * this only works with ANSI C.
- *
- * __XSTRING is like __STRING, but it expands any macros in its argument
- * first.  It is only available with ANSI C.
- */
-#if defined(__STDC__) || defined(__cplusplus)
-#define	__P(protos)	protos		/* full-blown ANSI C */
-#define	__CONCAT1(x,y)	x ## y
-#define	__CONCAT(x,y)	__CONCAT1(x,y)
-#define	__STRING(x)	#x		/* stringify without expanding x */
-#define	__XSTRING(x)	__STRING(x)	/* expand x, then stringify */
-#else	/* !(__STDC__ || __cplusplus) */
-#define	__P(protos)	()		/* traditional C preprocessor */
-#define	__CONCAT(x,y)	x/**/y
-#define	__STRING(x)	"x"
-#endif	/* !(__STDC__ || __cplusplus) */
-
 #if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 201112L || defined(lint)
 
 #if !__has_extension(c_static_assert)
