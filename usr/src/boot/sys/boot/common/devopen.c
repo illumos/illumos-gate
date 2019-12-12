adbb29bd77dbfbbf129b5e3bd6ccc5bd4074fd79
Toomas Soome <tsoome@me.com>
2019-12-12 17:33:08 +0200

    12073 loader: devopen dereference after free Reviewed by: Matthias Scheler <mscheler@tintri.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/boot/sys/boot/common/devopen.c
+++ usr/src/boot/sys/boot/common/devopen.c
@@ -1,4 +1,4 @@
-/*-
+/*
  * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
  * All rights reserved.
  *
@@ -25,7 +25,6 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 
 #include <stand.h>
 #include <string.h>
@@ -33,7 +32,7 @@ __FBSDID("$FreeBSD$");
 #include "bootstrap.h"
 
 int
-devopen(struct open_file *f, const char *fname, const char **file) 
+devopen(struct open_file *f, const char *fname, const char **file)
 {
 	struct devdesc *dev;
 	int result;
@@ -43,25 +42,22 @@ devopen(struct open_file *f, const char *fname, const char **file)
 		return (result);
 
 	/* point to device-specific data so that device open can use it */
+	f->f_dev = dev->d_dev;
 	f->f_devdata = dev;
 	result = dev->d_dev->dv_open(f, dev);
 	if (result != 0) {
 		f->f_devdata = NULL;
+		f->f_dev = NULL;
 		free(dev);
-		return (result);
 	}
 
-	/* reference the devsw entry from the open_file structure */
-	f->f_dev = dev->d_dev;
-	return (0);
+	return (result);
 }
 
 int
 devclose(struct open_file *f)
 {
 
-	if (f->f_devdata != NULL) {
-		free(f->f_devdata);
-	}
+	free(f->f_devdata);
 	return (0);
 }
