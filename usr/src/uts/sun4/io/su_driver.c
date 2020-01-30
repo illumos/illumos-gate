574493ffa2fa0218f9104c9978be64dd2d4f4b37
Toomas Soome <tsoome@me.com>
2020-01-30 14:06:15 +0200

    12562 su: NULL pointer errors Reviewed by: Yuri Pankov <ypankov@tintri.com> Reviewed by: Gergő Mihály Doma <domag02@gmail.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/uts/sun4/io/su_driver.c
+++ usr/src/uts/sun4/io/su_driver.c
@@ -247,7 +247,7 @@ static int asyprobe(dev_info_t *);
 static int asyattach(dev_info_t *, ddi_attach_cmd_t);
 static int asydetach(dev_info_t *, ddi_detach_cmd_t);
 
-static 	struct cb_ops cb_asy_ops = {
+static struct cb_ops cb_asy_ops = {
 	nodev,			/* cb_open */
 	nodev,			/* cb_close */
 	nodev,			/* cb_strategy */
@@ -363,8 +363,8 @@ asyprobe(dev_info_t *devi)
 
 	/*
 	 * Probe for the device:
-	 * 	Ser. int. uses bits 0,1,2; FIFO uses 3,6,7; 4,5 wired low.
-	 * 	If bit 4 or 5 appears on inb() ISR, board is not there.
+	 *	Ser. int. uses bits 0,1,2; FIFO uses 3,6,7; 4,5 wired low.
+	 *	If bit 4 or 5 appears on inb() ISR, board is not there.
 	 */
 	if (ddi_get8(handle, addr+ISR) & 0x30) {
 		ddi_regs_map_free(&handle);
@@ -709,8 +709,7 @@ asyattach(dev_info_t *devi, ddi_attach_cmd_t cmd)
 		 * create the minor device for this node.
 		 */
 		if (ddi_create_minor_node(devi, "ssp", S_IFCHR,
-		    asy->asy_unit | RSC_DEVICE, DDI_PSEUDO, NULL)
-		    == DDI_FAILURE) {
+		    asy->asy_unit | RSC_DEVICE, DDI_PSEUDO, 0) == DDI_FAILURE) {
 			cmn_err(CE_WARN,
 			    "%s%d: Failed to create node rsc-console",
 			    ddi_get_name(devi), ddi_get_instance(devi));
@@ -732,7 +731,7 @@ asyattach(dev_info_t *devi, ddi_attach_cmd_t cmd)
 		 * serial instances.
 		 */
 		if (ddi_create_minor_node(devi, "lom-console", S_IFCHR,
-		    instance, DDI_NT_SERIAL_LOMCON, NULL) == DDI_FAILURE) {
+		    instance, DDI_NT_SERIAL_LOMCON, 0) == DDI_FAILURE) {
 			cmn_err(CE_WARN,
 			    "%s%d: Failed to create node lom-console",
 			    ddi_get_name(devi), ddi_get_instance(devi));
@@ -750,8 +749,7 @@ asyattach(dev_info_t *devi, ddi_attach_cmd_t cmd)
 		 * create the minor device for this node.
 		 */
 		if (ddi_create_minor_node(devi, "sspctl", S_IFCHR,
-		    asy->asy_unit | RSC_DEVICE, DDI_PSEUDO, NULL)
-		    == DDI_FAILURE) {
+		    asy->asy_unit | RSC_DEVICE, DDI_PSEUDO, 0) == DDI_FAILURE) {
 			cmn_err(CE_WARN, "%s%d: Failed to create rsc-control",
 			    ddi_get_name(devi), ddi_get_instance(devi));
 			goto error;
@@ -775,7 +773,7 @@ asyattach(dev_info_t *devi, ddi_attach_cmd_t cmd)
 			goto error;
 		}
 		asy->asy_flags |= ASY_IGNORE_CD;	/* ignore cd */
-		asy->asy_device_type = ASY_KEYBOARD; 	/* Device type */
+		asy->asy_device_type = ASY_KEYBOARD;	/* Device type */
 	} else if (ddi_getprop(DDI_DEV_T_ANY, devi, DDI_PROP_DONTPASS,
 	    "mouse", 0)) {
 		/*
@@ -797,14 +795,14 @@ asyattach(dev_info_t *devi, ddi_attach_cmd_t cmd)
 		/* serial-port */
 		(void) sprintf(name, "%c", (instance+'a'));
 		if (ddi_create_minor_node(devi, name, S_IFCHR, instance,
-		    DDI_NT_SERIAL_MB, NULL) == DDI_FAILURE) {
+		    DDI_NT_SERIAL_MB, 0) == DDI_FAILURE) {
 			goto error;
 		}
 		state = MINORNODE;
 		/* serial-port:dailout */
 		(void) sprintf(name, "%c,cu", (instance+'a'));
 		if (ddi_create_minor_node(devi, name, S_IFCHR, instance|OUTLINE,
-		    DDI_NT_SERIAL_MB_DO, NULL) == DDI_FAILURE) {
+		    DDI_NT_SERIAL_MB_DO, 0) == DDI_FAILURE) {
 			goto error;
 		}
 		/* Property for ignoring DCD */
@@ -917,8 +915,8 @@ asyopen(queue_t *rq, dev_t *dev, int flag, int sflag, cred_t *cr)
 	struct asyncline *async;
 	int		mcr;
 	int		unit;
-	int 		len;
-	struct termios 	*termiosp;
+	int		len;
+	struct termios	*termiosp;
 
 #ifdef DEBUG
 	if (asydebug & ASY_DEBUG_CLOSE)
@@ -2640,7 +2638,7 @@ async_nstart(struct asyncline *async, int mode)
 				 * Therefore, the wait period is:
 				 *
 				 * (#TSR bits + #THR bits) *
-				 * 	1 MICROSEC / baud rate
+				 *	1 MICROSEC / baud rate
 				 */
 				(void) timeout(async_restart, async,
 				    drv_usectohz(16 * MICROSEC /
