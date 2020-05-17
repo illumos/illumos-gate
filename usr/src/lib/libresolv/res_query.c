f4fe62a3c26a7b15283589c57a3c10ed9695b529
Toomas Soome <tsoome@me.com>
2020-05-17 10:06:01 +0300

    13497 libresolv: symbol 'h_errno' is multiply-defined Reviewed by: Rich Lowe <richlowe@richlowe.net> Reviewed by: Marcel Telka <marcel@telka.sk> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/lib/libresolv/res_query.c
+++ usr/src/lib/libresolv/res_query.c
@@ -26,7 +26,7 @@
  */
 
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
-/*	  All Rights Reserved  	*/
+/*	  All Rights Reserved	*/
 
 /*
  * University Copyright- Copyright (c) 1982, 1986, 1988
@@ -57,8 +57,6 @@
 #else
 #define	MAXPACKET	1024
 #endif
-
-int h_errno;
 
 /*
  * Formulate a normal query, send, and await answer.
