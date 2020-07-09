f91a454727d8e1cd4bbbe2d4efd2754590298697
Gvozden Neskovic <neskovic@gmail.com>
2020-07-09 13:02:53 -0600

    12794 ZFS support for vectorized algorithms on x86 (HW support) Portions contributed by: Jerry Jelinek <jerry.jelinek@joyent.com> Reviewed by: Patrick Mooney <pmooney@pfmooney.com> Reviewed by: Toomas Soome <tsoome@me.com> Approved by: Robert Mustacchi <rm@fingolfin.org>

--- usr/src/uts/common/fs/zfs/sys/simd.h
+++ usr/src/uts/common/fs/zfs/sys/simd.h
@@ -18,13 +18,150 @@
 
 #if defined(__amd64__) || defined(__i386__)
 
-/* Temporararily disabled until subsequent work to turn this on. */
-#define	kfpu_allowed()		0
+
+#define	kfpu_allowed()		1
 #define	kfpu_initialize(tsk)	do {} while (0)
-#define	kfpu_begin()		do {} while (0)
-#define	kfpu_end()		do {} while (0)
 #define	kfpu_init()		(0)
 #define	kfpu_fini()		do {} while (0)
+
+#ifdef _KERNEL
+#include <sys/x86_archext.h>
+#include <sys/archsystm.h>
+#include <sys/kfpu.h>
+#include <sys/proc.h>
+#include <sys/disp.h>
+#include <sys/cpuvar.h>
+
+static inline void
+kfpu_begin(void)
+{
+	if (curthread->t_lwp != NULL && (curthread->t_procp->p_flag & SSYS)) {
+		kernel_fpu_begin(NULL, KFPU_USE_LWP);
+	} else {
+		kpreempt_disable();
+		kernel_fpu_begin(NULL, KFPU_NO_STATE);
+	}
+}
+
+static inline void
+kfpu_end(void)
+{
+	if (curthread->t_lwp != NULL && (curthread->t_procp->p_flag & SSYS)) {
+		kernel_fpu_end(NULL, KFPU_USE_LWP);
+	} else {
+		kernel_fpu_end(NULL, KFPU_NO_STATE);
+		kpreempt_enable();
+	}
+}
+
+/*
+ * Check if various vector instruction sets are available.
+ */
+
+static inline boolean_t
+zfs_sse_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_SSE));
+}
+
+static inline boolean_t
+zfs_sse2_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_SSE2));
+}
+
+static inline boolean_t
+zfs_sse3_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_SSE3));
+}
+
+static inline boolean_t
+zfs_ssse3_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_SSSE3));
+}
+
+static inline boolean_t
+zfs_avx_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_AVX));
+}
+
+static inline boolean_t
+zfs_avx2_available(void)
+{
+	return (is_x86_feature(x86_featureset, X86FSET_AVX2));
+}
+
+#else	/* ! _KERNEL */
+
+#include <sys/auxv.h>
+#include <sys/auxv_386.h>
+
+#define	kfpu_begin()		do {} while (0)
+#define	kfpu_end()		do {} while (0)
+
+/*
+ * User-level check if various vector instruction sets are available.
+ */
+
+static inline boolean_t
+zfs_sse_available(void)
+{
+	uint32_t u = 0;
+
+	(void) getisax(&u, 1);
+	return ((u & AV_386_SSE) != 0);
+}
+
+static inline boolean_t
+zfs_sse2_available(void)
+{
+	uint32_t u = 0;
+
+	(void) getisax(&u, 1);
+	return ((u & AV_386_SSE2) != 0);
+}
+
+static inline boolean_t
+zfs_sse3_available(void)
+{
+	uint32_t u = 0;
+
+	(void) getisax(&u, 1);
+	return ((u & AV_386_SSE3) != 0);
+}
+
+static inline boolean_t
+zfs_ssse3_available(void)
+{
+	uint32_t u = 0;
+
+	(void) getisax(&u, 1);
+	return ((u & AV_386_SSSE3) != 0);
+}
+
+static inline boolean_t
+zfs_avx_available(void)
+{
+	uint_t u = 0;
+
+	(void) getisax(&u, 1);
+	return ((u & AV_386_AVX) != 0);
+}
+
+static inline boolean_t
+zfs_avx2_available(void)
+{
+	uint32_t u[2] = { 0 };
+
+	(void) getisax((uint32_t *)&u, 2);
+	return ((u[1] & AV_386_2_AVX2) != 0);
+}
+
+#endif	/* _KERNEL */
+
 
 #else
 
