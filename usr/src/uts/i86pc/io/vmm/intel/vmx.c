91d438ebb667692f17584aae5640c22818cdab70
Jason King <jasonbking@users.noreply.github.com>
2020-09-14 13:05:23 -0500

    Fix merge (#329)

--- usr/src/uts/i86pc/io/vmm/intel/vmx.c
+++ usr/src/uts/i86pc/io/vmm/intel/vmx.c
@@ -227,7 +227,9 @@ static int pirvec = -1;
 SYSCTL_INT(_hw_vmm_vmx, OID_AUTO, posted_interrupt_vector, CTLFLAG_RD,
     &pirvec, 0, "APICv posted interrupt vector");
 
+#ifdef __FreeBSD__
 static struct unrhdr *vpid_unr;
+#endif /* __FreeBSD__*/
 static u_int vpid_alloc_failed;
 SYSCTL_UINT(_hw_vmm_vmx, OID_AUTO, vpid_alloc_failed, CTLFLAG_RD,
 	    &vpid_alloc_failed, 0, NULL);
