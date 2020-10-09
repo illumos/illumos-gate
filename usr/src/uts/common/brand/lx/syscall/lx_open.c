47660a03a1bef9bb5e314c4cb6a9a255a6cc95e6
Jerry Jelinek <jerry.jelinek@joyent.com>
2020-10-09 11:56:14 -0600

    OS-8236 Use new O_DIRECT flag in lx brand (#332)
    Reviewed by: Jason King <jbk@joyent.com>
    Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/uts/common/brand/lx/syscall/lx_open.c
+++ usr/src/uts/common/brand/lx/syscall/lx_open.c
@@ -23,6 +23,7 @@
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  * Copyright 2018 Joyent, Inc.
+ * Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
  */
 
 #include <sys/systm.h>
@@ -85,9 +86,9 @@ ltos_open_flags(int input)
 
 	/*
 	 * Linux uses the LX_O_DIRECT flag to do raw, synchronous I/O to the
-	 * device backing the fd in question.  Illumos doesn't have similar
-	 * functionality, but we can attempt to simulate it using the flags
-	 * (O_RSYNC|O_SYNC) and directio(3C).
+	 * device backing the fd in question.  illumos has O_DIRECT but
+	 * we additionally need O_RSYNC|O_SYNC to simulate the Linux
+	 * semantics as far as possible.
 	 *
 	 * The LX_O_DIRECT flag also requires that the transfer size and
 	 * alignment of I/O buffers be a multiple of the logical block size for
@@ -102,18 +103,18 @@ ltos_open_flags(int input)
 	 * system block size will be test suites.
 	 */
 	if (input & LX_O_DIRECT)
-		flags |= (O_RSYNC|O_SYNC);
+		flags |= (O_RSYNC|O_SYNC|O_DIRECT);
 
 	return (flags);
 }
 
-#define	LX_POSTPROCESS_OPTS	(LX_O_DIRECT | LX_O_ASYNC | LX_O_PATH)
+#define	LX_POSTPROCESS_OPTS	(LX_O_ASYNC | LX_O_PATH)
 
 static int
 lx_open_postprocess(int fd, int fmode)
 {
 	file_t *fp;
-	int rv, error = 0;
+	int error = 0;
 
 	if ((fmode & LX_POSTPROCESS_OPTS) == 0) {
 		/* Skip out early, if possible */
@@ -126,11 +127,6 @@ lx_open_postprocess(int fd, int fmode)
 		 * arrived here if some one is hammering away with close().
 		 */
 		return (EIO);
-	}
-
-	if (fmode & LX_O_DIRECT && error == 0) {
-		(void) VOP_IOCTL(fp->f_vnode, _FIODIRECTIO, DIRECTIO_ON,
-		    fp->f_flag, fp->f_cred, &rv, NULL);
 	}
 
 	if (fmode & LX_O_ASYNC && error == 0) {
