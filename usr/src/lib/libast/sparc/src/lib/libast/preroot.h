3e526b96e078451614b96e869ba7e7ff36f39030
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-12-30 17:47:37 +0000

    13405 ksh93 update to 2012-08-01: part 1: remove previous ast components
    Change-ID: Ia11e674d79cb9374d321116e794655b7d1957b0e

--- usr/src/lib/libast/sparc/src/lib/libast/preroot.h
+++
@@ -1,6 +0,0 @@
-/* : : generated from /home/gisburn/ksh93/ast_ksh_20100309/build_sparc_32bit/src/lib/libast/features/preroot.sh by iffe version 2009-12-04 : : */
-#ifndef _def_preroot_ast
-#define _def_preroot_ast	1
-#define _sys_types	1	/* #include <sys/types.h> ok */
-/* preroot not enabled */
-#endif
