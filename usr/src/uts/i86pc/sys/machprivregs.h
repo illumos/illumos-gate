72c7ebd9d162dc4cbe1844d12b6b120771c46960
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-07-19 16:10:00 +0000

    13958 kpti_trampolines.s fails to assemble with binutils 2.37 Reviewed by: Robert Mustacchi <rm@fingolfin.org> Reviewed by: Toomas Soome <tsoome@me.com> Reviewed by: Alex Wilson <alex@cooperi.net> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/uts/i86pc/sys/machprivregs.h
+++ usr/src/uts/i86pc/sys/machprivregs.h
@@ -78,6 +78,18 @@ extern "C" {
 #define	SYSRETL	sysretl
 #define	SWAPGS	swapgs
 
+/*
+ * As of GNU binutils 2.37, the assembler has split the 'sysexit' instruction
+ * into 'sysexitl' and 'sysexitq'. Using a plain 'sysexit' is interpreted as
+ * 'sysexitl' but comes with a warning about the assumption being made. Since
+ * all warnings are treated as errors in the kernel build, this results in a
+ * build failure. Unfortunately the desired 'sysexitl' cannot be used since
+ * older versions of the GNU assembler do not understand it.
+ * The following macro emits the correct byte sequence for 'sysexitl' on this
+ * platform.
+ */
+#define	SYSEXITL .byte 0x0f, 0x35
+
 #elif defined(__i386)
 
 #define	IRET	iret
