1b9d0d8668d6fb8bbf6fd07dfeac92665c32895e
Toomas Soome <tsoome@me.com>
2021-08-05 15:57:11 +0300

    14000 lorder: replace sequence a-z by [:lower:] Reviewed by: Andrew Stormont <andyjstormont@gmail.com> Reviewed by: Robert Mustacchi <rm+illumos@fingolfin.org>

--- usr/src/cmd/sgs/lorder/lorder.sh
+++ usr/src/cmd/sgs/lorder/lorder.sh
@@ -74,20 +74,20 @@ esac
 #	The last 2 sections of code are exactly alike but
 #	they handle different external symbols, namely the
 #	symbols that are defined in the text section, data section, bss
-#	section or common symbols and symbols 
+#	section or common symbols and symbols
 #	that are referenced but not defined in this file.
-#	A line containing the symbol (from the pattern space) and 
+#	A line containing the symbol (from the pattern space) and
 #	the file it is referenced in (from the hold space) is
 #	put into the pattern space.
-#	If its text, data, bss or common it is written out to the 
-#	symbol definition (symdef) file, otherwise it was referenced 
+#	If its text, data, bss or common it is written out to the
+#	symbol definition (symdef) file, otherwise it was referenced
 #	but not declared in this file so it is written out to the
 #	symbol referenced (symref) file.
 #
 #
 ${WHERE}/${PFX}nm -p $* 2>$TDIR/$$tmp | sed -e '/^[ 	]*$/d' -e '
 	/^[0-9]* R $/d
-	/ [a-zFLS] /d
+	/ [[:lower:]FLS] /d
 	/[^]]:$/{
 		s/://
 		h
