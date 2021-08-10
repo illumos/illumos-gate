bdc24928e6e072b3224ad4bcd1769e68dc91f970
Robert Mustacchi <rm@fingolfin.org>
2021-08-10 11:25:18 -0700

    14013 Update Zen 3 cpuid_subr.c info Reviewed by: Andy Fiddaman <andy@omnios.org> Reviewed by: Toomas Soome <tsoome@me.com> Approved by: Richard Lowe <richlowe@richlowe.net>

--- usr/src/uts/intel/sys/x86_archext.h
+++ usr/src/uts/intel/sys/x86_archext.h
@@ -979,6 +979,17 @@ extern "C" {
 #define	X86_CHIPREV_HYGON_18_DN_A1 \
 	_X86_CHIPREV_MKREV(X86_VENDOR_HYGON, 0x18, 0x0001)
 
+#define	X86_CHIPREV_AMD_19_GN_A0 \
+	_X86_CHIPREV_MKREV(X86_VENDOR_AMD, 0x19, 0x0000)
+#define	X86_CHIPREV_AMD_19_GN_B0 \
+	_X86_CHIPREV_MKREV(X86_VENDOR_AMD, 0x19, 0x0001)
+#define	X86_CHIPREV_AMD_19_GN_B1 \
+	_X86_CHIPREV_MKREV(X86_VENDOR_AMD, 0x19, 0x0002)
+#define	X86_CHIPREV_AMD_19_VMR_B0 \
+	_X86_CHIPREV_MKREV(X86_VENDOR_AMD, 0x19, 0x0003)
+#define	X86_CHIPREV_AMD_19_VMR_B1 \
+	_X86_CHIPREV_MKREV(X86_VENDOR_AMD, 0x19, 0x0004)
+
 /*
  * Various socket/package types, extended as the need to distinguish
  * a new type arises.  The top 8 byte identfies the vendor and the
