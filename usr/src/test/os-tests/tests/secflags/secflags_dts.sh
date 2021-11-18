0554d5ecd11d9644cbb915be31b5a0b7abb40122
Robert Mustacchi <rm@fingolfin.org>
2021-11-18 15:14:15 +0000

    14229 secflags_dts relies on compiler defaults Reviewed by: Rich Lowe <richlowe@richlowe.net> Reviewed by: Andy Fiddaman <andy@omnios.org> Reviewed by: Toomas Soome <tsoome@me.com> Approved by: Dan McDonald <danmcd@joyent.com>

--- usr/src/test/os-tests/tests/secflags/secflags_dts.sh
+++ usr/src/test/os-tests/tests/secflags/secflags_dts.sh
@@ -11,8 +11,11 @@
 # http://www.illumos.org/license/CDDL.
 #
 
+#
 # Copyright 2015, Richard Lowe.
 # Copyright 2019 Joyent, Inc.
+# Copyright 2021 Oxide Computer Company
+#
 
 tmpdir=/tmp/test.$$
 mkdir $tmpdir
@@ -37,12 +40,20 @@ main(int argc, char **argv)
 }
 EOF
 
-gcc -o tester-aslr tester.c -Wl,-z,aslr=enabled
-gcc -o tester-noaslr tester.c -Wl,-z,aslr=disabled
+gcc -m32 -o tester-aslr.32 tester.c -Wl,-z,aslr=enabled
+gcc -m32 -o tester-noaslr.32 tester.c -Wl,-z,aslr=disabled
+gcc -m64 -o tester-aslr.64 tester.c -Wl,-z,aslr=enabled
+gcc -m64 -o tester-noaslr.64 tester.c -Wl,-z,aslr=disabled
 
 # This is the easiest way I've found to get many many DTs, but it's gross
-gcc -o many-dts-aslr tester.c -Wl,-z,aslr=enabled $(for elt in /usr/lib/lib*.so; do echo -Wl,-N,$(basename $elt); done)
-gcc -o many-dts-noaslr tester.c -Wl,-z,aslr=disabled $(for elt in /usr/lib/lib*.so; do echo -Wl,-N,$(basename $elt); done)
+gcc -m32 -o many-dts-aslr.32 tester.c -Wl,-z,aslr=enabled \
+    $(for elt in /usr/lib/lib*.so; do echo -Wl,-N,$(basename $elt); done)
+gcc -m32 -o many-dts-noaslr.32 tester.c -Wl,-z,aslr=disabled \
+    $(for elt in /usr/lib/lib*.so; do echo -Wl,-N,$(basename $elt); done)
+gcc -m64 -o many-dts-aslr.64 tester.c -Wl,-z,aslr=enabled \
+    $(for elt in /usr/lib/64/lib*.so; do echo -Wl,-N,$(basename $elt); done)
+gcc -m64 -o many-dts-noaslr.64 tester.c -Wl,-z,aslr=disabled \
+    $(for elt in /usr/lib/64/lib*.so; do echo -Wl,-N,$(basename $elt); done)
 
 check() {
     bin=$1
@@ -54,7 +65,7 @@ check() {
     pid=$!
     sleep 1
     psecflags $pid | grep -q "${set}:.*aslr"
-    (( $? != $state )) && ret=1
+    (( $? != state )) && ret=1
     kill -9 $pid
     return $ret
 }
@@ -65,11 +76,21 @@ fail() {
 }
 
 psecflags -s none $$
-check ./tester-aslr 0 E || fail "DT_SUNW_ASLR 1 failed"
-check ./many-dts-aslr 0 E || fail "DT_SUNW_ASLR 1 with many DTs failed"
-check ./tester-aslr 1 I || fail "DT_SUNW_ASLR 1 incorrectly set the inheritable flag"
+check ./tester-aslr.32 0 E || fail "DT_SUNW_ASLR 1 failed (32-bit)"
+check ./many-dts-aslr.32 0 E || fail \
+    "DT_SUNW_ASLR 1 with many DTs failed (32-bit)"
+check ./tester-aslr.32 1 I || fail \
+    "DT_SUNW_ASLR 1 incorrectly set the inheritable flag (32-bit)"
+check ./tester-aslr.64 0 E || fail "DT_SUNW_ASLR 1 failed (64-bit)"
+check ./many-dts-aslr.64 0 E || fail \
+    "DT_SUNW_ASLR 1 with many DTs failed (64-bit)"
+check ./tester-aslr.64 1 I || fail \
+    "DT_SUNW_ASLR 1 incorrectly set the inheritable flag (64-bit)"
 
 psecflags -s aslr $$
-check ./tester-noaslr 1 E || fail "DT_SUNW_ASLR 0 failed"
-check ./many-dts-noaslr 1 E || fail "DT_SUNW_ASLR 0 with many DTs failed"
-
+check ./tester-noaslr.32 1 E || fail "DT_SUNW_ASLR 0 failed (32-bit)"
+check ./many-dts-noaslr.32 1 E || fail  \
+    "DT_SUNW_ASLR 0 with many DTs failed (32-bit)"
+check ./tester-noaslr.64 1 E || fail "DT_SUNW_ASLR 0 failed (64-bit)"
+check ./many-dts-noaslr.64 1 E || fail \
+    "DT_SUNW_ASLR 0 with many DTs failed (64-bit)"
