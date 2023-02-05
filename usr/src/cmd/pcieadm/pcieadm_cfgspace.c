8b0ccc49c4d0c1ac61668d047d9e8f14ca9ff6f3
Keith M Wesolowski <wesolows@oxide.computer>
2023-02-05 11:28:37 -0800

    15269 pcieadm misrepresents PCIE_AER_CTL as PCIE_AER_RE_CMD Reviewed by: Robert Mustacchi <rm+illumos@fingolfin.org> Reviewed by: Marco van Wieringen <mvw@planets.elm.net> Approved by: Dan McDonald <danmcd@mnx.io>

--- usr/src/cmd/pcieadm/pcieadm_cfgspace.c
+++ usr/src/cmd/pcieadm/pcieadm_cfgspace.c
@@ -10,7 +10,7 @@
  */
 
 /*
- * Copyright 2022 Oxide Computer Company
+ * Copyright 2023 Oxide Computer Company
  */
 
 /*
@@ -2083,7 +2083,7 @@ static const pcieadm_cfgspace_print_t pcieadm_cap_aer_v1[] = {
 	    pcieadm_cfgspace_print_hex },
 	{ PCIE_AER_HDR_LOG + 12, 4, "hl3", "Header Log 3",
 	    pcieadm_cfgspace_print_hex },
-	{ PCIE_AER_CTL, 4, "rootcmd", "Root Error Command",
+	{ PCIE_AER_RE_CMD, 4, "rootcmd", "Root Error Command",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootcom },
 	{ PCIE_AER_RE_STS, 4, "rootsts", "Root Error Status",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootsts },
@@ -2115,7 +2115,7 @@ static const pcieadm_cfgspace_print_t pcieadm_cap_aer_v2[] = {
 	    pcieadm_cfgspace_print_hex },
 	{ PCIE_AER_HDR_LOG + 12, 4, "hl3", "Header Log 3",
 	    pcieadm_cfgspace_print_hex },
-	{ PCIE_AER_CTL, 4, "rootcmd", "Root Error Command",
+	{ PCIE_AER_RE_CMD, 4, "rootcmd", "Root Error Command",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootcom },
 	{ PCIE_AER_RE_STS, 4, "rootsts", "Root Error Status",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootsts },
@@ -2153,7 +2153,7 @@ static const pcieadm_cfgspace_print_t pcieadm_cap_aer_bridge[] = {
 	    pcieadm_cfgspace_print_hex },
 	{ PCIE_AER_HDR_LOG + 12, 4, "hl3", "Header Log 3",
 	    pcieadm_cfgspace_print_hex },
-	{ PCIE_AER_CTL, 4, "rootcmd", "Root Error Command",
+	{ PCIE_AER_RE_CMD, 4, "rootcmd", "Root Error Command",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootcom },
 	{ PCIE_AER_RE_STS, 4, "rootsts", "Root Error Status",
 	    pcieadm_cfgspace_print_regdef, pcieadm_regdef_aer_rootsts },
