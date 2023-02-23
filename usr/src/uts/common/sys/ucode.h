e699c89cb88a367d501441939a61503d0e255ce5
Andy Fiddaman <illumos@fiddaman.net>
2023-02-23 22:35:39 +0000

    15414 Update intel CPU Microcode to 20230214 Reviewed by: Toomas Soome <tsoome@me.com> Approved by: Robert Mustacchi <rm@fingolfin.org>

--- usr/src/uts/common/sys/ucode.h
+++ usr/src/uts/common/sys/ucode.h
@@ -206,16 +206,16 @@ typedef union ucode_file {
 #define	UCODE_DEFAULT_BODY_SIZE		(UCODE_KB(2) - UCODE_HEADER_SIZE_INTEL)
 
 /*
- * For a single microcode file, the minimum size is 1K, maximum size is 384K.
- * Such limitations, while somewhat artificial, are not only to provide better
- * sanity checks, but also avoid wasting precious memory at startup time as the
- * microcode buffer for the first processor has to be statically allocated.
+ * For a single microcode file, the following minimum and maximum sizes are
+ * defined. Such limitations, while somewhat artificial, are not only to
+ * provide better sanity checks, but also avoid wasting precious memory at
+ * startup time as the microcode buffer for the first processor has to be
+ * statically allocated.
  *
- * For the concatenation of all the microcode binary files, the maximum size
- * is 16M.
+ * The last limit is for the concatenation of all the microcode binary files.
  */
 #define	UCODE_MIN_SIZE			UCODE_KB(1)
-#define	UCODE_MAX_SIZE			UCODE_KB(384)
+#define	UCODE_MAX_SIZE			UCODE_MB(2)
 #define	UCODE_MAX_COMBINED_SIZE		UCODE_MB(16)
 
 #define	UCODE_SIZE_CONVERT(size, default_size) \
