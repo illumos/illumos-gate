75859165f7f379d8d5c8ea22a2dcd4d21bd4e259
Toomas Soome <tsoome@me.com>
2023-07-07 13:01:31 +0300

    15783 loader: add stdint.h Reviewed by: Andy Fiddaman <illumos@fiddaman.net> Reviewed by: Igor Kozhukhov <igor@dilos.org> Reviewed by: Dan Cross <cross@oxidecomputer.com> Approved by: Dan McDonald <danmcd@mnx.io>

---
+++ usr/src/boot/include/stdint.h
@@ -0,0 +1,21 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright 2022 Toomas Soome <tsoome@me.com>
+ */
+
+#ifndef _STDINT_H
+#define	_STDINT_H
+
+#include <sys/stdint.h>
+
+#endif /* _STDINT_H */
