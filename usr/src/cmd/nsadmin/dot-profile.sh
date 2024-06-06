f52c5be01c2a4cde558845581175e6a5883c4174
Jonathan Perkin <jonathan@perkin.org.uk>
2024-06-06 20:44:20 +0100

    OS-8550 Support mozilla-rootcerts-openssl
    Co-authored-by: Dan McDonald <danmcd@mnx.io>
    Reviewed by: Brianna Bennett <brbennett@mnx.io>
    Approved by: Brianna Bennett <brbennett@mnx.io>

--- usr/src/cmd/nsadmin/dot-profile.sh
+++ usr/src/cmd/nsadmin/dot-profile.sh
@@ -6,14 +6,18 @@
 
 #
 # Copyright 2019 Joyent, Inc.
+# Copyright 2024 MNX Cloud, Inc.
 #
 
 PATH=/usr/bin:/usr/sbin:/smartdc/bin:/opt/smartdc/bin:/opt/local/bin:/opt/local/sbin:/opt/tools/bin:/opt/tools/sbin:/opt/smartdc/agents/bin
 MANPATH=/usr/share/man:/smartdc/man:/opt/smartdc/man:/opt/local/man:/opt/tools/man
 PAGER=less
-# If pkgsrc-tools is set up and the mozilla-rootcerts package is installed
-# configure the platform curl to use it.
+# If pkgsrc-tools is set up and either the mozilla-rootcerts-openssl or
+# mozilla-rootcerts packages are installed, configure the platform curl to
+# use the provided CA bundle.
 if [[ -f /opt/tools/share/mozilla-rootcerts/cacert.pem ]]; then
 	CURL_CA_BUNDLE=/opt/tools/share/mozilla-rootcerts/cacert.pem
+elif [[ -f /opt/tools/etc/openssl/certs/ca-certificates.crt ]]; then
+	CURL_CA_BUNDLE=/opt/tools/etc/openssl/certs/ca-certificates.crt
 fi
 export PATH MANPATH PAGER CURL_CA_BUNDLE
