6faf52448e142b151fa3deade474be359e7c8698
Toomas Soome <tsoome@me.com>
2024-11-29 17:55:58 +0200

    16948 want __cacheline_aligned Reviewed by: Patrick Mooney <pmooney@pfmooney.com> Approved by: Dan McDonald <danmcd@mnx.io>

--- usr/src/compat/bhyve/amd64/machine/param.h
+++ usr/src/compat/bhyve/amd64/machine/param.h
@@ -36,6 +36,4 @@
 /* Size of the level 4 page-map level-4 table units */
 #define	NPML4EPG	(PAGE_SIZE/(sizeof (pml4_entry_t)))
 
-#define	CACHE_LINE_SIZE	64
-
 #endif	/* _COMPAT_FREEBSD_AMD64_MACHINE_PARAM_H_ */
