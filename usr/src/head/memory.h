10936bbef9b4a6b108e670b5e53ff9def41839cc
Gordon Ross <gordon.w.ross@gmail.com>
2026-01-20 12:10:58 -0500

    17832 memory.h has C++ namespace problems Reviewed by: Robert Mustacchi <rm+illumos@fingolfin.org> Approved by: Dan McDonald <danmcd@edgecast.io>

--- usr/src/head/memory.h
+++ usr/src/head/memory.h
@@ -20,6 +20,7 @@
  * CDDL HEADER END
  */
 /*
+ * Copyright 2026 Gordon Ross <gordon.w.ross@gmail.com>
  * Copyright 2014 Garrett D'Amore <garrett@damore.org>
  */
 /*	Copyright (c) 1988 AT&T	*/
@@ -35,28 +36,46 @@ extern "C" {
 #endif
 
 extern void *memccpy(void *, const void *, int, size_t);
+
 #if __cplusplus >= 199711L
 namespace std {
+#endif
+
+extern int memcmp(const void *, const void *, size_t);
+extern void *memcpy(void *_RESTRICT_KYWD, const void *_RESTRICT_KYWD, size_t);
+extern void *memset(void *, int, size_t);
+
+/* See similar in string_iso.h */
+#if __cplusplus >= 199711L
 extern const void *memchr(const void *, int, size_t);
-#ifndef _MEMCHR_INLINE
+#ifndef	_MEMCHR_INLINE
 #define	_MEMCHR_INLINE
 extern "C++" {
 	inline void *memchr(void * __s, int __c, size_t __n) {
-		return (void*)memchr((const void *) __s, __c, __n);
+		return (void *)memchr((const void *)__s, __c, __n);
 	}
 }
-#endif /* _MEMCHR_INLINE */
-} /* end of namespace std */
-using std::memchr;
-#else
+#endif	/* _MEMCHR_INLINE */
+
+#else	/* __cplusplus >= 199711L */
+
 extern void *memchr(const void *, int, size_t);
-#endif
-extern void *memcpy(void *, const void *, size_t);
-extern void *memset(void *, int, size_t);
-extern int memcmp(const void *, const void *, size_t);
+
+#endif	/* __cplusplus >= 199711L */
+
+#if __cplusplus >= 199711L
+}	/* end of namespace std */
+#endif	/* __cplusplus >= 199711L */
 
 #ifdef	__cplusplus
 }
 #endif
+
+#if __cplusplus >= 199711L
+using std::memcmp;
+using std::memcpy;
+using std::memset;
+using std::memchr;
+#endif	/* __cplusplus >= 199711L */
 
 #endif	/* _MEMORY_H */
