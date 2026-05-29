b32229105ff0363d3ca16ede5eeaa6affdba6615
Toomas Soome <tsoome@me.com>
2026-05-29 16:44:39 +0300

    18136 loader: hexdump() pager does ignore quit Reviewed by: Andy Fiddaman <illumos@fiddaman.net> Reviewed by: Bill Sommerfeld <sommerfeld@hamachi.org> Approved by: Gordon Ross <gordon.w.ross@gmail.com>

--- usr/src/boot/common/misc.c
+++ usr/src/boot/common/misc.c
@@ -40,25 +40,27 @@
 char *
 unargv(int argc, char *argv[])
 {
-    size_t	hlong;
-    int		i;
-    char	*cp;
+	size_t	hlong;
+	int	i;
+	char	*cp;
 
-    for (i = 0, hlong = 0; i < argc; i++)
-	hlong += strlen(argv[i]) + 2;
+	for (i = 0, hlong = 0; i < argc; i++)
+		hlong += strlen(argv[i]) + 2;
 
-    if(hlong == 0)
-	return(NULL);
+	if (hlong == 0)
+		return (NULL);
 
-    cp = malloc(hlong);
-    cp[0] = 0;
-    for (i = 0; i < argc; i++) {
-	strcat(cp, argv[i]);
-	if (i < (argc - 1))
-	  strcat(cp, " ");
-    }
+	cp = malloc(hlong);
+	if (cp != NULL) {
+		cp[0] = 0;
+		for (i = 0; i < argc; i++) {
+			strcat(cp, argv[i]);
+			if (i < (argc - 1))
+				strcat(cp, " ");
+		}
+	}
 
-    return(cp);
+	return (cp);
 }
 
 /*
@@ -67,15 +69,15 @@ unargv(int argc, char *argv[])
 size_t
 strlenout(vm_offset_t src)
 {
-    char	c;
-    size_t	len;
+	char	c;
+	size_t	len;
 
-    for (len = 0; ; len++) {
-	archsw.arch_copyout(src++, &c, 1);
-	if (c == 0)
-	    break;
-    }
-    return(len);
+	for (len = 0; ; len++) {
+		archsw.arch_copyout(src++, &c, 1);
+		if (c == 0)
+			break;
+	}
+	return (len);
 }
 
 /*
@@ -84,15 +86,15 @@ strlenout(vm_offset_t src)
 char *
 strdupout(vm_offset_t str)
 {
-    char	*result, *cp;
+	char	*result, *cp;
 
-    result = malloc(strlenout(str) + 1);
-    for (cp = result; ;cp++) {
-	archsw.arch_copyout(str++, cp, 1);
-	if (*cp == 0)
-	    break;
-    }
-    return(result);
+	result = malloc(strlenout(str) + 1);
+	for (cp = result; ; cp++) {
+		archsw.arch_copyout(str++, cp, 1);
+		if (*cp == 0)
+			break;
+	}
+	return (result);
 }
 
 /* Zero a region in kernel space. */
@@ -102,10 +104,10 @@ kern_bzero(vm_offset_t dest, size_t len)
 	char buf[256];
 	size_t chunk, resid;
 
-	bzero(buf, sizeof(buf));
+	bzero(buf, sizeof (buf));
 	resid = len;
 	while (resid > 0) {
-		chunk = min(sizeof(buf), resid);
+		chunk = min(sizeof (buf), resid);
 		archsw.arch_copyin(buf, dest, chunk);
 		resid -= chunk;
 		dest += chunk;
@@ -175,49 +177,55 @@ alloc_pread(int fd, off_t off, size_t len)
 void
 hexdump(caddr_t region, size_t len)
 {
-    caddr_t	line;
-    int		x, c;
-    char	lbuf[80];
-#define emit(fmt, args...)	{sprintf(lbuf, fmt , ## args); pager_output(lbuf);}
-
-    pager_open();
-    for (line = region; line < (region + len); line += 16) {
-	emit("%08lx  ", (long) line);
-
-	for (x = 0; x < 16; x++) {
-	    if ((line + x) < (region + len)) {
-		emit("%02x ", *(u_int8_t *)(line + x));
-	    } else {
-		emit("-- ");
-	    }
-	    if (x == 7)
-		emit(" ");
+	caddr_t	line;
+	int	x, c;
+	char	lbuf[80];
+#define	emit(fmt, args...)						\
+	{								\
+		(void) snprintf(lbuf, sizeof (lbuf), fmt, ## args);	\
+		if (pager_output(lbuf))					\
+			goto out;					\
 	}
-	emit(" |");
-	for (x = 0; x < 16; x++) {
-	    if ((line + x) < (region + len)) {
-		c = *(u_int8_t *)(line + x);
-		if ((c < ' ') || (c > '~'))	/* !isprint(c) */
-		    c = '.';
-		emit("%c", c);
-	    } else {
-		emit(" ");
-	    }
+
+	pager_open();
+	for (line = region; line < (region + len); line += 16) {
+		emit("%08lx  ", (long)line);
+
+		for (x = 0; x < 16; x++) {
+			if ((line + x) < (region + len)) {
+				emit("%02x ", *(uint8_t *)(line + x));
+			} else {
+				emit("-- ");
+			}
+			if (x == 7)
+				emit(" ");
+		}
+		emit(" |");
+		for (x = 0; x < 16; x++) {
+			if ((line + x) < (region + len)) {
+				c = *(uint8_t *)(line + x);
+				if ((c < ' ') || (c > '~')) /* !isprint(c) */
+					c = '.';
+				emit("%c", c);
+			} else {
+				emit(" ");
+			}
+		}
+		emit("|\n");
 	}
-	emit("|\n");
-    }
-    pager_close();
+out:
+	pager_close();
 }
 
 void
 dev_cleanup(void)
 {
-    int		i;
+	int		i;
 
-    /* Call cleanup routines */
-    for (i = 0; devsw[i] != NULL; ++i)
-	if (devsw[i]->dv_cleanup != NULL)
-	    (devsw[i]->dv_cleanup)();
+	/* Call cleanup routines */
+	for (i = 0; devsw[i] != NULL; ++i)
+		if (devsw[i]->dv_cleanup != NULL)
+			(devsw[i]->dv_cleanup)();
 }
 
 #ifndef BOOT2
