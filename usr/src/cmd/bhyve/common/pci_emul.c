5dc8ec7c48787979eb30bd9f15c702bb7391a892
Roman Bogorodskiy <novel@FreeBSD.org>
2026-09-20 14:41:55 +0000

    18460 bhyve: fix boot device ordering Reviewed by: Bill Sommerfeld <sommerfeld@hamachi.org> Reviewed by: Andy Fiddaman <illumos@fiddaman.net> Approved by: Gordon Ross <gordon.w.ross@gmail.com>

--- usr/src/cmd/bhyve/common/pci_emul.c
+++ usr/src/cmd/bhyve/common/pci_emul.c
@@ -1477,7 +1477,7 @@ init_bootorder(void)
 	}
 	fclose(fp);
 
-	return (qemu_fwcfg_add_file("bootorder", bootorder_len, bootorder));
+	return (qemu_fwcfg_add_file("bootorder", bootorder_len + 1, bootorder));
 }
 
 #define	BUSIO_ROUNDUP		32
